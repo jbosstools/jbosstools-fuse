ba2e1ad97772b985ece56ed6a730ab2555fc61c1
James Strachan <james.strachan@gmail.com>
2012-11-08 09:12:22 +0000

    force authentication when downloading archetypes

--- ide-buildtools/src/main/scala/org/fusesource/ide/buildtools/DownloadFuseArchetypes.scala
+++ ide-buildtools/src/main/scala/org/fusesource/ide/buildtools/DownloadFuseArchetypes.scala
@@ -35,7 +35,7 @@ class DownloadFuseArchetypes(outputDir: File = new File("fuse-ide-archetypes"), 
   override def toString = "Download latest archetypes to: " + outputDir
 
   def run(): Unit = {
-    aether.authorized = true
+    Aether.authorised = true
     if (!outputDir.exists()) {
       throw new IllegalArgumentException("No outputDir exists! " + outputDir)
     }
