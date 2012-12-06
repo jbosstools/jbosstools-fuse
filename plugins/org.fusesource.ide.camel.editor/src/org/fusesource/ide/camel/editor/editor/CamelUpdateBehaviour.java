9c0ede53245aa8d30e8247414943a085d61d5683
Lars Heinemann <lhein@apache.org>
2012-12-06 11:10:12 +0100

    fixes for graphiti upgrade

--- plugins/org.fusesource.ide.camel.editor/src/org/fusesource/ide/camel/editor/editor/CamelUpdateBehaviour.java
+++ plugins/org.fusesource.ide.camel.editor/src/org/fusesource/ide/camel/editor/editor/CamelUpdateBehaviour.java
@@ -17,6 +17,7 @@ public class CamelUpdateBehaviour extends DefaultUpdateBehavior {
 	 */
 	public CamelUpdateBehaviour(DiagramEditor diagramEditor) {
 		super(diagramEditor);
+		createEditingDomain();
 	}
 
 	/* (non-Javadoc)
