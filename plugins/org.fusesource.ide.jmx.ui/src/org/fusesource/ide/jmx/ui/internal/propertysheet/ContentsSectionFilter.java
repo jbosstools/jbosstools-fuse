e3f8c9aae2d11fd02e14a59366796d0deea9e3e7
Lars Heinemann <lhein@apache.org>
2013-03-26 22:28:09 +0100

    removed the properties tab "Contents" for some node types so it looks like the old properties view

--- plugins/org.fusesource.ide.jmx.ui/src/org/fusesource/ide/jmx/ui/internal/propertysheet/ContentsSectionFilter.java
+++ plugins/org.fusesource.ide.jmx.ui/src/org/fusesource/ide/jmx/ui/internal/propertysheet/ContentsSectionFilter.java
@@ -11,7 +11,7 @@
  ******************************************************************************/
 package org.fusesource.ide.jmx.ui.internal.propertysheet;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import org.eclipse.jface.viewers.IFilter;
 import org.fusesource.ide.commons.tree.RefreshableCollectionNode;
@@ -21,11 +21,27 @@ import org.fusesource.ide.commons.tree.RefreshableCollectionNode;
  */
 public class ContentsSectionFilter implements IFilter {
 
+	private static final ArrayList<String> noContentsTab = new ArrayList<String>();
+	
+	// we want to skip the contents tab in properties view for the following node types
+	static {
+		noContentsTab.add("org.fusesource.ide.fabric.camel.navigator.RoutesNode");
+		noContentsTab.add("org.fusesource.ide.fabric.navigator.osgi.BundlesNode");
+		noContentsTab.add("org.fusesource.ide.fabric.camel.navigator.RouteNode");
+		noContentsTab.add("org.fusesource.ide.fabric.camel.navigator.ProcessorNode");
+		noContentsTab.add("org.fusesource.ide.fabric.navigator.Fabric");
+		noContentsTab.add("org.fusesource.ide.fabric.navigator.ContainersNode");
+		noContentsTab.add("org.fusesource.ide.fabric.navigator.ProfileNode");
+		noContentsTab.add("org.fusesource.ide.fabric.navigator.ContainerNode");
+	}
+	
     @Override
     public boolean select(Object toTest) {
+    	if (noContentsTab.contains(toTest.getClass().getName())) return false;
+    	
         if (toTest instanceof RefreshableCollectionNode) {
-            List<?> propertySources = ((RefreshableCollectionNode) toTest).getPropertySourceList();
-            return propertySources != null && propertySources.size() > 0;
+            //List<?> propertySources = ((RefreshableCollectionNode) toTest).getPropertySourceList();
+            return true; //propertySources != null && propertySources.size() > 0;
         }
         return false;
     }
