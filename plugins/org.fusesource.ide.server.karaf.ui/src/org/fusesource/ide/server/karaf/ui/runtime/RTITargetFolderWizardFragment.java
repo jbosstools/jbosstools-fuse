f8695d38bd374935448e7f560aa3d6441ea758a7
Lars Heinemann <lhein@apache.org>
2014-03-21 10:13:17 +0100

    fix for ECLIPSE-1055 and ECLIPSE-1059

---
+++ plugins/org.fusesource.ide.server.karaf.ui/src/org/fusesource/ide/server/karaf/ui/runtime/RTITargetFolderWizardFragment.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Create a WST wizard fragment associated with a composite to handle the 
+ * user-specification of a JBoss Fuse Runtime installation directory.
+ * 
+ * Copyright (c) 2014 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.fusesource.ide.server.karaf.ui.runtime;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.wst.server.ui.wizard.IWizardHandle;
+import org.eclipse.wst.server.ui.wizard.WizardFragment;
+import org.fusesource.ide.server.karaf.ui.Messages;
+
+public class RTITargetFolderWizardFragment extends WizardFragment {
+	public static final String FUSE_RT_LOC = "fuseRTLoc";
+
+	protected RTIComposite comp;
+
+	public RTITargetFolderWizardFragment() {
+		// do nothing
+	}
+
+	public boolean hasComposite() {
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.wst.server.ui.internal.task.WizardTask#getWizardPage()
+	 */
+	public Composite createComposite(Composite parent, IWizardHandle wizard) {
+		comp = new RTIComposite(parent, getTaskModel(), wizard);
+		wizard.setTitle(Messages.AbstractKarafRuntimeComposite_jboss_fuse_rt_label);
+		wizard.setDescription(Messages.AbstractKarafRuntimeComposite_selectInstallDir);
+		return comp;
+	}
+
+	public boolean isComplete() {
+		return this.comp.isComplete();
+	}
+}
