856b758a2446a4bc6583c27b5883036c001de8b0
Lars Heinemann <lhein@apache.org>
2014-04-03 20:54:07 +0200

    camel context folder is now only created on projects with the correct nature (see JBIDE-16968)

---
+++ plugins/org.fusesource.ide.project/src/org/fusesource/ide/project/CamelNatureTester.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.fusesource.ide.project;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.runtime.CoreException;
+import org.fusesource.ide.commons.logging.RiderLogFacade;
+
+/**
+ * @author lhein
+ */
+public class CamelNatureTester extends PropertyTester {
+
+	@Override
+	public boolean test(Object receiver, String property, Object[] args,
+			Object expectedValue) {
+
+		if (receiver instanceof IProject) {
+			IProject project = (IProject) receiver;
+			boolean enabled = isCamelNatureDefined(project);
+			if (property.equals("camelNatureEnabled")) {
+				return enabled == true;
+			} else if (property.equals("camelNatureDisabled")) {
+				return enabled == false;
+			}
+		}
+		return false;
+	}
+
+	private boolean isCamelNatureDefined(IProject project) {
+		try {
+			IProjectDescription description = project.getDescription();
+			String[] natures = description.getNatureIds();
+
+			for (int i = 0; i < natures.length; ++i) {
+				if (RiderProjectNature.NATURE_ID.equals(natures[i])) {
+					return true;
+				}
+			}
+		} catch (CoreException e) {
+			RiderLogFacade.getLog(Activator.getDefault().getLog()).error(e);
+		}
+		return false;
+	}
+}
