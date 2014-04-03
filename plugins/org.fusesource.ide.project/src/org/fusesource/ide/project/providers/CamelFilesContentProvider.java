852dc06ce880cbd089c4515bf7bd309cee7f57e5
Lars Heinemann <lhein@apache.org>
2014-04-03 20:54:07 +0200

    camel context folder is now only created on projects with the correct nature (see JBIDE-16968)

--- plugins/org.fusesource.ide.project/src/org/fusesource/ide/project/providers/CamelFilesContentProvider.java
+++ plugins/org.fusesource.ide.project/src/org/fusesource/ide/project/providers/CamelFilesContentProvider.java
@@ -11,15 +11,12 @@
 
 package org.fusesource.ide.project.providers;
 
+import java.util.HashMap;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
-import org.fusesource.ide.branding.Activator;
 
 /**
  * @author lhein
@@ -27,65 +24,37 @@ import org.fusesource.ide.branding.Activator;
  */
 public class CamelFilesContentProvider implements ITreeContentProvider {
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getChildren(java.lang.Object)
-	 */
+	private HashMap<IProject, CamelVirtualFolder> cache = new HashMap<IProject, CamelVirtualFolder>();
+
 	@Override
 	public Object[] getChildren(Object parentElement) {
 		if (parentElement instanceof IProject) {
 			IProject project = (IProject) parentElement;
 
 			if (!project.isAccessible()) {
-			    return new Object[0];
+				return new Object[0];
 			}
 
-			CamelVirtualFolder cvf = new CamelVirtualFolder(project);
-			cvf.populateChildren();
-
-			boolean validNature = false;
-			try {
-				IProjectDescription description = project.getDescription();
-				String[] natures = description.getNatureIds();
-				for (int i = 0; i < natures.length; ++i) {
-					if (Activator.CAMEL_NATURE_ID.equals(natures[i])) {
-						validNature = true;
-					}
-				}
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-
-			if (!validNature && cvf.getCamelFiles().size()>0) {
-				// add camel nature
-				try {
-					addCamelNature(project, null);
-				} catch (CoreException ex) {
-					Activator.getLogger().warning("Unable to set Camel Nature on the project...");
-				} finally {
-					validNature = true;
-				}
-			}
-
-			if (validNature) {
-				CamelVirtualFolder[] resVal = new CamelVirtualFolder[1];
-				resVal[0] = cvf;
-				return resVal;
-			}
-			return new Object[0];
-		} else if (parentElement instanceof CamelVirtualFolder) {
-			CamelVirtualFolder cvf = (CamelVirtualFolder)parentElement;
-			if (cvf.getCamelFiles().size()<1) {
+			CamelVirtualFolder cvf = cache.get(project);
+			if (cvf == null) {
+				cvf = new CamelVirtualFolder(project);
 				cvf.populateChildren();
+				cache.put(project, cvf);
 			}
-			return cvf.getCamelFiles().toArray(new IFile[cvf.getCamelFiles().size()]);
+
+			CamelVirtualFolder[] resVal = new CamelVirtualFolder[1];
+			resVal[0] = cvf;
+			return resVal;
+		} else if (parentElement instanceof CamelVirtualFolder) {
+			CamelVirtualFolder cvf = (CamelVirtualFolder) parentElement;
+			return cvf.getCamelFiles().toArray(
+					new IResource[cvf.getCamelFiles().size()]);
 		}
 		return new Object[] {};
 	}
 
 	@Override
 	public Object getParent(Object element) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -105,24 +74,5 @@ public class CamelFilesContentProvider implements ITreeContentProvider {
 
 	@Override
 	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/**
-	 * adds the Camel nature to the project
-	 * 
-	 * @param project
-	 * @param monitor
-	 * @throws CoreException
-	 */
-	private void addCamelNature(IProject project, IProgressMonitor monitor) throws CoreException {
-		IProjectDescription projectDescription = project.getDescription();
-		String[] ids = projectDescription.getNatureIds();
-		String[] newIds = new String[ids.length + 1];
-		System.arraycopy(ids,0,newIds,0,ids.length);
-		newIds[ids.length] = Activator.CAMEL_NATURE_ID;
-		projectDescription.setNatureIds(newIds);
-		project.setDescription(projectDescription, monitor);
 	}
 }
