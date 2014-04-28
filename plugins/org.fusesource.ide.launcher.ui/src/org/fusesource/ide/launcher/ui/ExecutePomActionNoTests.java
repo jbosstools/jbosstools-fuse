ff996d18ae88b0d0947dda86abf5c5d262176842
Lars Heinemann <lhein@apache.org>
2014-04-28 15:07:38 +0200

    fixed issue ECLIPSE-1082

--- plugins/org.fusesource.ide.launcher.ui/src/org/fusesource/ide/launcher/ui/ExecutePomActionNoTests.java
+++ plugins/org.fusesource.ide.launcher.ui/src/org/fusesource/ide/launcher/ui/ExecutePomActionNoTests.java
@@ -10,6 +10,13 @@
  ******************************************************************************/
 package org.fusesource.ide.launcher.ui;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.ui.ISelectionService;
+import org.eclipse.ui.PlatformUI;
 import org.fusesource.ide.launcher.CamelContextLaunchConfigConstants;
 
 /**
@@ -23,5 +30,27 @@ public class ExecutePomActionNoTests extends ExecutePomActionSupport {
 				"org.fusesource.ide.launcher.ui.launchConfigurationTabGroup.camelContext",
 				CamelContextLaunchConfigConstants.CAMEL_CONTEXT_NO_TESTS_LAUNCH_CONFIG_TYPE_ID,
 				CamelContextLaunchConfigConstants.DEFAULT_MAVEN_GOALS + " -Dmaven.test.skip=true");
+	}
+	
+	protected void appendAttributes(IContainer basedir,
+			ILaunchConfigurationWorkingCopy workingCopy, String goal) {
+
+		String path = getSelectedFilePath();
+		workingCopy.setAttribute(CamelContextLaunchConfigConstants.ATTR_FILE,
+				path == null ? "" : path); // basedir.getLocation().toOSString()
+	}
+
+	protected String getSelectedFilePath() {
+		ISelectionService selService = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService();
+		ISelection isel = selService.getSelection();
+		if (isel != null && isel instanceof StructuredSelection) {
+			StructuredSelection ssel = (StructuredSelection)isel;
+			Object elem = ssel.getFirstElement();
+			if (elem != null && elem instanceof IFile) {
+				IFile f = (IFile)elem;
+				return f.getLocationURI().toString();
+			}
+		}
+		return null;
 	}
 }
