f9093d94c1766e3d19789ccf98f7d33f866b0802
lhein <lhein@apache.org>
2014-07-07 10:21:10 +0200

    removing old modules from the codebase

--- old/org.fusesource.ide.jvmmonitor.ui/src/org/fusesource/ide/jvmmonitor/internal/ui/IJvmFacade.java
+++
@@ -1,9 +0,0 @@
-package org.fusesource.ide.jvmmonitor.internal.ui;
-
-import org.fusesource.ide.jvmmonitor.core.IActiveJvm;
-
-public interface IJvmFacade {
-
-	IActiveJvm getActiveJvm();
-
-}
--- old/rcp_build/org.fusesource.ide.rcp.feature/rootFiles/win/README.txt
+++
@@ -1,7 +0,0 @@
-Fuse IDE  
--------------------
-
-To start Fuse IDE, run 'FuseIDE.exe'.
-
-You can find documentation online at:
-https://access.redhat.com/site/documentation/JBoss_Fuse/
