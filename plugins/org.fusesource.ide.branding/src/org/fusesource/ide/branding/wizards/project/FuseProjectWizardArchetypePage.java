d47eadb0544d30ea4603d435b53234f3f60f9a3d
lhein <lhein@apache.org>
2014-07-14 12:02:55 +0200

    fixed too early Finish button availability in new Project Wizard (see FUSETOOLS-1149)

--- plugins/org.fusesource.ide.branding/src/org/fusesource/ide/branding/wizards/project/FuseProjectWizardArchetypePage.java
+++ plugins/org.fusesource.ide.branding/src/org/fusesource/ide/branding/wizards/project/FuseProjectWizardArchetypePage.java
@@ -30,7 +30,6 @@ import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.jface.viewers.IOpenListener;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.OpenEvent;
@@ -69,12 +68,9 @@ import org.fusesource.camel.tooling.util.Archetypes;
 import org.fusesource.ide.branding.Activator;
 import org.fusesource.ide.branding.RiderHelpContextIds;
 import org.fusesource.ide.branding.wizards.WizardMessages;
-import org.fusesource.ide.commons.Viewers;
+import org.fusesource.ide.commons.ui.Selections;
 import org.fusesource.ide.commons.util.Strings;
 import org.osgi.framework.Bundle;
-
-
-// import com.ibm.icu.lang.UCharacter;
 
 /**
  * Maven Archetype selection wizard page presents the user with a list of
@@ -93,21 +89,7 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 	Text descriptionText;
 
 	/** the list of available archetypes */
-
-	/**
-	 * a flag indicating if the archetype selection is actually used in the
-	 * wizard
-	 */
-	private boolean isUsed = true;
-
 	volatile Collection<ArchetypeDetails> archetypes;
-
-	/*
-	 * 
-	 * Collection<Archetype> lastVersionArchetypes;
-	 * 
-	 * ArchetypeCatalogFactory catalogFactory = null;
-	 */
 
 	public static final String DEFAULT_VERSION = "1.0.0-SNAPSHOT"; //$NON-NLS-1$
 
@@ -177,10 +159,6 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 
 		createViewer(composite);
 
-		// TODO
-		// createAdvancedSettings(composite, new GridData(SWT.FILL, SWT.TOP,
-		// true, false, 3, 1));
-
 		setControl(composite);
 	}
 
@@ -192,11 +170,6 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 		.setText(WizardMessages.MavenProjectWizardArchetypePage_lblFilter);
 
 		QuickViewerFilter quickViewerFilter = new QuickViewerFilter();
-		/*
-		 * LastVersionFilter versionFilter = new LastVersionFilter();
-		 * IncludeSnapshotsFilter snapshotsFilter = new
-		 * IncludeSnapshotsFilter();
-		 */
 
 		filterText = new Text(parent, SWT.BORDER | SWT.SEARCH);
 		filterText
@@ -222,11 +195,6 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 		final ToolItem clearToolItem = new ToolItem(toolBar, SWT.PUSH);
 		clearToolItem.setEnabled(false);
 
-		// TODO
-		/*
-		 * clearToolItem.setImage(MavenImages.IMG_CLEAR);
-		 * clearToolItem.setDisabledImage(MavenImages.IMG_CLEAR_DISABLED);
-		 */
 		clearToolItem.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -314,13 +282,25 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 			}
 		});
 
+		viewer.addOpenListener(new IOpenListener() {
+			@Override
+			public void open(OpenEvent openevent) {
+				if (canFlipToNextPage()) {
+					getContainer().showPage(getNextPage());
+				}
+			}
+		});
+
+		List<ArchetypeDetails> list = getArchetypes();
+		viewer.setInput(list);
+
 		viewer.addSelectionChangedListener(new ISelectionChangedListener() {
 			@Override
 			public void selectionChanged(SelectionChangedEvent event) {
 				if (descriptionText == null) {
 					return;
 				}
-				ArchetypeDetails archetype = getArchetype();
+				archetype = (ArchetypeDetails)Selections.getFirstSelection(event.getSelection());
 				if (archetype != null) {
 					String repositoryUrl = archetype.getRepository();
 					String description = archetype.getDescription();
@@ -345,19 +325,7 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 				validate();
 			}
 		});
-
-		viewer.addOpenListener(new IOpenListener() {
-			@Override
-			public void open(OpenEvent openevent) {
-				if (canFlipToNextPage()) {
-					getContainer().showPage(getNextPage());
-				}
-			}
-		});
-
-		List<ArchetypeDetails> list = getArchetypes();
-		viewer.setInput(list);
-
+		
 		Composite composite2 = new Composite(sashForm, SWT.NONE);
 		GridLayout gridLayout2 = new GridLayout();
 		gridLayout2.marginHeight = 0;
@@ -394,54 +362,6 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 		gridLayout.numColumns = 3;
 		buttonComposite.setLayout(gridLayout);
 
-		/*
-		 * showLastVersionButton = new Button(buttonComposite, SWT.CHECK);
-		 * showLastVersionButton.setLayoutData(new GridData(SWT.LEFT,
-		 * SWT.CENTER, true, false));
-		 * showLastVersionButton.setText(WizardMessages
-		 * .MavenProjectWizardArchetypePage_btnLast);
-		 * showLastVersionButton.setSelection(true);
-		 * showLastVersionButton.addSelectionListener(versionFilter);
-		 * 
-		 * includeShapshotsButton = new Button(buttonComposite, SWT.CHECK);
-		 * GridData buttonData = new GridData(SWT.LEFT, SWT.CENTER, true,
-		 * false); buttonData.horizontalIndent = 25;
-		 * includeShapshotsButton.setLayoutData(buttonData);
-		 * includeShapshotsButton
-		 * .setText(WizardMessages.MavenProjectWizardArchetypePage_btnSnapshots
-		 * ); includeShapshotsButton.setSelection(false);
-		 * includeShapshotsButton.addSelectionListener(snapshotsFilter);
-		 * 
-		 * addArchetypeButton = new Button(buttonComposite, SWT.NONE);
-		 * addArchetypeButton
-		 * .setText(WizardMessages.MavenProjectWizardArchetypePage_btnAdd);
-		 * addArchetypeButton.setData("name", "addArchetypeButton");
-		 * //$NON-NLS-1$ //$NON-NLS-2$ buttonData = new GridData(SWT.RIGHT,
-		 * SWT.CENTER, true, false); buttonData.horizontalIndent = 35;
-		 * addArchetypeButton.setLayoutData(buttonData);
-		 * 
-		 * addArchetypeButton.addSelectionListener(new SelectionAdapter() {
-		 * public void widgetSelected(SelectionEvent e) { CustomArchetypeDialog
-		 * dialog = new CustomArchetypeDialog(getShell(),
-		 * WizardMessages.MavenProjectWizardArchetypePage_add_title);
-		 * if(dialog.open() == Window.OK) { String archetypeGroupId =
-		 * dialog.getArchetypeGroupId(); String archetypeArtifactId =
-		 * dialog.getArchetypeArtifactId(); String archetypeVersion =
-		 * dialog.getArchetypeVersion(); String repositoryUrl =
-		 * dialog.getRepositoryUrl(); downloadArchetype(archetypeGroupId,
-		 * archetypeArtifactId, archetypeVersion, repositoryUrl); } } });
-		 */
-
-		if (!list.isEmpty()) {
-			viewer.setSelection(new StructuredSelection(list.get(0)));
-			Viewers.async(new Runnable() {
-
-				@Override
-				public void run() {
-					userChangedArtifactId = false;
-				}});
-		}
-
 		if (Strings.isBlank(getGroupId())) {
 			// lets default to the last one
 			int itemCount = groupIdCombo.getItemCount();
@@ -454,12 +374,6 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 	}
 
 	private void createArtifactGroup(Composite parent) {
-		// Composite artifactGroup = new Composite(parent, SWT.NONE);
-		// GridData gd_artifactGroup = new GridData( SWT.FILL, SWT.FILL, true,
-		// false );
-		// artifactGroup.setLayoutData(gd_artifactGroup);
-		// artifactGroup.setLayout(new GridLayout(2, false));
-
 		Label groupIdlabel = new Label(parent, SWT.NONE);
 		groupIdlabel.setText(WizardMessages.artifactComponentGroupId);
 
@@ -568,18 +482,6 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 						answer.add(details);
 					}
 				}
-				/*
-				Enumeration entryPaths = bundle.getEntryPaths("/archetypes");
-				while (entryPaths.hasMoreElements()) {
-					Object next = entryPaths.nextElement();
-					String name = next.toString();
-					if (name.endsWith(".jar")) {
-						URL resource = bundle.getResource(name);
-						String id = name.replaceFirst("archetypes/", "");
-						answer.add(new ArchetypeDetails(id, resource));
-					}
-				}
-				 */
 			}
 			return answer;
 
@@ -587,66 +489,6 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 			setErrorMessage(WizardMessages.MavenProjectWizardArchetypePage_error_read);
 			return null;
 		}
-	}
-
-	/** Loads the available archetypes. */
-	/*
-	 * void loadArchetypes(final String groupId, final String artifactId, final
-	 * String version) { Job job = new
-	 * Job(WizardMessages.wizardProjectPageArchetypeRetrievingArchetypes) {
-	 * protected IStatus run(IProgressMonitor monitor) { try { List<Archetype>
-	 * catalogArchetypes = getArchetypesForCatalog();
-	 * 
-	 * if(catalogArchetypes == null || catalogArchetypes.size() == 0) {
-	 * Display.getDefault().asyncExec(new Runnable() { public void run() {
-	 * if(catalogFactory != null &&
-	 * "Nexus Indexer".equals(catalogFactory.getDescription())) { //$NON-NLS-1$
-	 * setErrorMessage(WizardMessages.MavenProjectWizardArchetypePage_error_no);
-	 * } } }); } else { Display.getDefault().asyncExec(new Runnable() { public
-	 * void run() { setErrorMessage(null); } }); } if(catalogArchetypes == null)
-	 * { return Status.CANCEL_STATUS; } TreeSet<Archetype> archs = new
-	 * TreeSet<Archetype>(ARCHETYPE_COMPARATOR);
-	 * archs.addAll(catalogArchetypes); archetypes = archs;
-	 * 
-	 * Display.getDefault().asyncExec(new Runnable() { public void run() {
-	 * updateViewer(groupId, artifactId, version); } }); } catch(Exception e) {
-	 * monitor.done(); return Status.CANCEL_STATUS; }
-	 * 
-	 * return Status.OK_STATUS; } }; job.schedule(); }
-	 * 
-	 * public Set<Archetype> filterVersions(Collection<Archetype> archetypes) {
-	 * HashMap<String, Archetype> filteredArchetypes = new HashMap<String,
-	 * Archetype>();
-	 * 
-	 * for(Archetype currentArchetype : archetypes) { String key =
-	 * getArchetypeKey(currentArchetype); Archetype archetype =
-	 * filteredArchetypes.get(key); if(archetype == null) {
-	 * filteredArchetypes.put(key, currentArchetype); } else {
-	 * DefaultArtifactVersion currentVersion = new
-	 * DefaultArtifactVersion(currentArchetype.getVersion());
-	 * DefaultArtifactVersion version = new
-	 * DefaultArtifactVersion(archetype.getVersion());
-	 * if(currentVersion.compareTo(version) > 0) { filteredArchetypes.put(key,
-	 * currentArchetype); } } }
-	 * 
-	 * TreeSet<Archetype> result = new TreeSet<Archetype>(new
-	 * Comparator<Archetype>() { public int compare(Archetype a1, Archetype a2)
-	 * { String k1 = a1.getGroupId() + ":" + a1.getArtifactId() + ":" +
-	 * a1.getVersion(); //$NON-NLS-1$ //$NON-NLS-2$ String k2 = a2.getGroupId()
-	 * + ":" + a2.getArtifactId() + ":" + a2.getVersion(); //$NON-NLS-1$
-	 * //$NON-NLS-2$ return k1.compareTo(k2); } });
-	 * result.addAll(filteredArchetypes.values()); return result; }
-	 * 
-	 * private String getArchetypeKey(Archetype archetype) { return
-	 * archetype.getGroupId() + ":" + archetype.getArtifactId(); //$NON-NLS-1$ }
-	 * 
-	 * ArchetypeCatalog getArchetypeCatalog() throws CoreException { return
-	 * catalogFactory == null ? null : catalogFactory.getArchetypeCatalog(); }
-	 */
-
-	/** Sets the flag that the archetype selection is used in the wizard. */
-	public void setUsed(boolean isUsed) {
-		this.isUsed = isUsed;
 	}
 
 	/** Overrides the default to return "true" if the page is not used. */
@@ -662,16 +504,6 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 		super.setVisible(visible);
 
 		if (visible) {
-			/*
-			 * ArchetypeManager archetypeManager =
-			 * MavenPlugin.getDefault().getArchetypeManager(); String catalogId
-			 * = dialogSettings.get(KEY_CATALOG); catalogFactory = null;
-			 * if(catalogId != null) { catalogFactory =
-			 * archetypeManager.getArchetypeCatalogFactory(catalogId); }
-			 * catalogsComboViewer.setSelection(new
-			 * StructuredSelection(catalogFactory == null ? ALL_CATALOGS :
-			 * catalogFactory));
-			 */
 			if (defaultFocusOnFilter ) {
 				filterText.setFocus();
 			} else {
@@ -689,133 +521,8 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 
 	/** Returns the selected archetype. */
 	public ArchetypeDetails getArchetype() {
-		return (ArchetypeDetails) ((IStructuredSelection) viewer.getSelection())
-				.getFirstElement();
+		return this.archetype;
 	}
-
-	/*
-	 * void updateViewer(String groupId, String artifactId, String version) {
-	 * lastVersionArchetypes = filterVersions(archetypes);
-	 * 
-	 * viewer.setInput(archetypes);
-	 * 
-	 * selectArchetype(groupId, artifactId, version);
-	 * 
-	 * Table table = viewer.getTable(); int columnCount =
-	 * table.getColumnCount(); int width = 0; for(int i = 0; i < columnCount;
-	 * i++ ) { TableColumn column = table.getColumn(i); column.pack(); width +=
-	 * column.getWidth(); } GridData tableData = (GridData)
-	 * table.getLayoutData(); int oldHint = tableData.widthHint; if(width >
-	 * oldHint) { tableData.widthHint = width; } getShell().pack(true);
-	 * tableData.widthHint = oldHint; }
-	 * 
-	 * protected void selectArchetype(String groupId, String artifactId, String
-	 * version) { Archetype archetype = findArchetype(groupId, artifactId,
-	 * version);
-	 * 
-	 * Table table = viewer.getTable(); if(archetype != null) {
-	 * viewer.setSelection(new StructuredSelection(archetype), true);
-	 * 
-	 * int n = table.getSelectionIndex(); table.setSelection(n); } }
-	 */
-
-	/** Locates an archetype with given ids. */
-	/*
-	 * protected Archetype findArchetype(String groupId, String artifactId,
-	 * String version) { for(Archetype archetype : archetypes) {
-	 * if(archetype.getGroupId().equals(groupId) &&
-	 * archetype.getArtifactId().equals(artifactId)) { if(version == null ||
-	 * version.equals(archetype.getVersion())) { return archetype; } } }
-	 * 
-	 * return version == null ? null : findArchetype(groupId, artifactId, null);
-	 * }
-	 * 
-	 * protected void downloadArchetype(final String archetypeGroupId, final
-	 * String archetypeArtifactId, final String archetypeVersion, final String
-	 * repositoryUrl) { final String archetypeName = archetypeGroupId + ":" +
-	 * archetypeArtifactId + ":" + archetypeVersion; //$NON-NLS-1$ //$NON-NLS-2$
-	 * 
-	 * try { getContainer().run(true, true, new IRunnableWithProgress() { public
-	 * void run(IProgressMonitor monitor) throws InterruptedException {
-	 * monitor.beginTask
-	 * (WizardMessages.MavenProjectWizardArchetypePage_task_downloading +
-	 * archetypeName, IProgressMonitor.UNKNOWN);
-	 * 
-	 * try { final IMaven maven = MavenPlugin.getDefault().getMaven();
-	 * 
-	 * final MavenPlugin plugin = MavenPlugin.getDefault();
-	 * 
-	 * final List<ArtifactRepository> remoteRepositories;
-	 * if(repositoryUrl.length() == 0) { remoteRepositories =
-	 * maven.getArtifactRepositories(); // XXX should use
-	 * ArchetypeManager.getArhetypeRepositories() } else { ArtifactRepository
-	 * repository = new DefaultArtifactRepository( // "archetype",
-	 * repositoryUrl, new DefaultRepositoryLayout(), null, null); //$NON-NLS-1$
-	 * remoteRepositories = Collections.singletonList(repository); }
-	 * 
-	 * monitor.subTask(WizardMessages.MavenProjectWizardArchetypePage_task_resolving
-	 * ); Artifact pomArtifact = maven.resolve(archetypeGroupId,
-	 * archetypeArtifactId, archetypeVersion, "pom", null, remoteRepositories,
-	 * monitor); //$NON-NLS-1$ monitor.worked(1); if(monitor.isCanceled()) {
-	 * throw new InterruptedException(); }
-	 * 
-	 * File pomFile = pomArtifact.getFile(); if(pomFile.exists()) {
-	 * monitor.subTask
-	 * (WizardMessages.MavenProjectWizardArchetypePage_task_resolving2);
-	 * Artifact jarArtifact = maven.resolve(archetypeGroupId,
-	 * archetypeArtifactId, archetypeVersion, "jar", null, remoteRepositories,
-	 * monitor); //$NON-NLS-1$ monitor.worked(1); if(monitor.isCanceled()) {
-	 * throw new InterruptedException(); }
-	 * 
-	 * File jarFile = jarArtifact.getFile();
-	 * 
-	 * monitor.subTask(WizardMessages.MavenProjectWizardArchetypePage_task_reading
-	 * ); monitor.worked(1); if(monitor.isCanceled()) { throw new
-	 * InterruptedException(); }
-	 * 
-	 * monitor.subTask(WizardMessages.MavenProjectWizardArchetypePage_task_indexing
-	 * ); IndexManager indexManager = plugin.getIndexManager(); IMutableIndex
-	 * localIndex = indexManager.getLocalIndex();
-	 * localIndex.addArtifact(jarFile, new ArtifactKey(pomArtifact));
-	 * 
-	 * //save out the archetype //TODO move this logig out of UI code! Archetype
-	 * archetype = new Archetype(); archetype.setGroupId(archetypeGroupId);
-	 * archetype.setArtifactId(archetypeArtifactId);
-	 * archetype.setVersion(archetypeVersion);
-	 * archetype.setRepository(repositoryUrl);
-	 * org.apache.maven.archetype.Archetype archetyper =
-	 * MavenPlugin.getDefault().getArchetype();
-	 * archetyper.updateLocalCatalog(archetype);
-	 * 
-	 * loadArchetypes(archetypeGroupId, archetypeArtifactId, archetypeVersion);
-	 * } else { final Artifact pom = pomArtifact; //the user tried to add an
-	 * archetype that couldn't be resolved on the server
-	 * getShell().getDisplay().asyncExec(new Runnable() { public void run() {
-	 * setErrorMessage(NLS.bind(
-	 * WizardMessages.MavenProjectWizardArchetypePage_error_resolve,
-	 * pom.toString())); } }); }
-	 * 
-	 * } catch(InterruptedException ex) { throw ex;
-	 * 
-	 * } catch(final Exception ex) { final String msg = NLS.bind(
-	 * WizardMessages.MavenProjectWizardArchetypePage_error_resolve2,
-	 * archetypeName); log.error(msg, ex); getShell().getDisplay().asyncExec(new
-	 * Runnable() { public void run() { setErrorMessage(msg + "\n" +
-	 * ex.toString()); //$NON-NLS-1$ } });
-	 * 
-	 * } finally { monitor.done();
-	 * 
-	 * } } });
-	 * 
-	 * } catch(InterruptedException ex) { // ignore
-	 * 
-	 * } catch(InvocationTargetException ex) { String msg =
-	 * NLS.bind(WizardMessages.MavenProjectWizardArchetypePage_error_resolve2,
-	 * archetypeName); log.error(msg, ex); setErrorMessage(msg + "\n" +
-	 * ex.toString()); //$NON-NLS-1$
-	 * 
-	 * } }
-	 */
 
 	/**
 	 * ArchetypeLabelProvider
@@ -871,23 +578,6 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 		}
 	}
 
-	// reload the table when index updating finishes
-	// try to preserve selection in case this is a rebuild
-	protected void reloadViewer() {
-		/*
-		 * Display.getDefault().asyncExec(new Runnable() { public void run() {
-		 * if(isCurrentPage()) { StructuredSelection sel = (StructuredSelection)
-		 * viewer.getSelection(); Archetype selArchetype = null; if(sel != null
-		 * && sel.getFirstElement() != null) { selArchetype = (Archetype)
-		 * sel.getFirstElement(); } if(selArchetype != null) {
-		 * loadArchetypes(selArchetype.getGroupId(),
-		 * selArchetype.getArtifactId(), selArchetype.getVersion()); } else {
-		 * loadArchetypes("org.apache.maven.archetypes",
-		 * "maven-archetype-quickstart", "1.0"); //$NON-NLS-1$ //$NON-NLS-2$
-		 * //$NON-NLS-3$ } } } });
-		 */
-	}
-
 	/**
 	 * Validates the contents of this wizard page.
 	 * <p>
@@ -937,7 +627,6 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 			return WizardMessages.wizardProjectPageMaven2ValidatorVersion;
 		}
 
-		// TODO: check validity of version?
 		String packageName = packageCombo.getText();
 		if (packageName.trim().length() != 0) {
 			if (!Pattern
@@ -1070,20 +759,6 @@ public class FuseProjectWizardArchetypePage extends AbstractFuseWizardPage {
 
 	public String getProjectName() {
 		return getArtifactId();
-		/*
-		 * // XXX should use resolved MavenProject or Model
-		 * if(projectNameTemplate.length() == 0) { return getArtifactId(); }
-		 * 
-		 * String artifactId = getArtifactId(); String groupId = getGroupId();
-		 * if(groupId == null && getParent() != null) { groupId =
-		 * getParent().getGroupId(); } String version = getVersion(); if(version
-		 * == null && getParent() != null) { version = getParent().getVersion();
-		 * }
-		 * 
-		 * return projectNameTemplate.replaceAll(GROUP_ID,
-		 * groupId).replaceAll(ARTIFACT_ID, artifactId).replaceAll(VERSION,
-		 * version == null ? "" : version); //$NON-NLS-1$
-		 */
 	}
 
 	public IStatus validateProjectName() {
