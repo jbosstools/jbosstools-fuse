1418ecf00b36f080eab4f93733b3cb6f0dc204f7
Lars Heinemann <lhein@apache.org>
2015-01-30 15:46:59 +0100

    additional refactoring for FUSETOOLS-1076

--- servers/plugins/org.fusesource.ide.server.karaf.ui/src/org/fusesource/ide/server/karaf/ui/runtime/v2x/KarafRuntimeFragment.java
+++ servers/plugins/org.fusesource.ide.server.karaf.ui/src/org/fusesource/ide/server/karaf/ui/runtime/v2x/KarafRuntimeFragment.java
@@ -26,6 +26,7 @@ import org.eclipse.wst.server.core.IRuntime;
 import org.eclipse.wst.server.core.IRuntimeWorkingCopy;
 import org.eclipse.wst.server.core.ServerCore;
 import org.eclipse.wst.server.core.TaskModel;
+import org.eclipse.wst.server.ui.wizard.IWizardHandle;
 import org.fusesource.ide.server.karaf.core.runtime.IKarafRuntime;
 import org.fusesource.ide.server.karaf.core.runtime.IKarafRuntimeWorkingCopy;
 import org.fusesource.ide.server.karaf.core.runtime.KarafRuntimeDelegate;
@@ -42,6 +43,35 @@ import org.jboss.tools.as.runtimes.integration.ui.composites.DownloadRuntimeHome
  */
 public class KarafRuntimeFragment extends RuntimeWizardFragment {
 
+	// TODO: remove with next version of jbt server tools
+	private Composite compositeStored = null;
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.ide.eclipse.as.wtp.ui.wizard.RuntimeWizardFragment#createComposite(org.eclipse.swt.widgets.Composite, org.eclipse.wst.server.ui.wizard.IWizardHandle)
+	 */
+	// TODO: remove with next version of jbt server tools
+	@Override
+	public Composite createComposite(Composite parent, IWizardHandle handle) {
+		this.compositeStored = super.createComposite(parent, handle);
+		return this.compositeStored;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.ide.eclipse.as.wtp.ui.wizard.RuntimeWizardFragment#isComplete()
+	 */
+	// TODO: remove with next version of jbt server tools
+	@Override
+	public boolean isComplete() {
+	   return super.isComplete() && compositeStored != null && !compositeStored.isDisposed();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.ide.eclipse.as.wtp.ui.wizard.RuntimeWizardFragment#updateWizardHandle(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
 	protected void updateWizardHandle(Composite parent) {
 		// make modifications to parent
 		IRuntime r = getRuntimeFromTaskModel();
@@ -57,15 +87,84 @@ public class KarafRuntimeFragment extends RuntimeWizardFragment {
 		return KarafSharedImages.getImageDescriptor(imageKey);
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.jboss.ide.eclipse.as.wtp.ui.wizard.RuntimeWizardFragment#exit()
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.ide.eclipse.as.wtp.ui.wizard.RuntimeWizardFragment#createJRECompositeWidget(org.eclipse.swt.widgets.Composite)
 	 */
 	@Override
-	public void exit() {
-		super.exit();
-		beenEntered = false;
+	protected AbstractJREComposite createJRECompositeWidget(Composite main) {
+		// Create our composite
+		return new KarafJREComposite(main, SWT.NONE, getTaskModel());
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.ide.eclipse.as.wtp.ui.wizard.RuntimeWizardFragment#getExplanationText()
+	 */
+	@Override
+	protected String getExplanationText() {
+		return "Please point to a Karaf installation.";
 	}
 
+	protected void initiateHelp(Composite parent) {
+		PlatformUI.getWorkbench().getHelpSystem().setHelp(parent, "org.jboss.ide.eclipse.as.doc.user.new_server_runtime"); //$NON-NLS-1$		
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.ide.eclipse.as.wtp.ui.wizard.RuntimeWizardFragment#getHomeVersionWarning()
+	 */
+	@Override
+	protected String getHomeVersionWarning() {
+		String homeDir = homeDirComposite.getHomeDirectory();
+		File loc = new File(homeDir);
+		String serverId = new ServerBeanLoader(loc).getServerAdapterId();
+		String rtId = serverId == null ? null : 
+				ServerCore.findServerType(serverId).getRuntimeType().getId();
+		IRuntime adapterRt = getRuntimeFromTaskModel();
+		String adapterRuntimeId = adapterRt.getRuntimeType().getId();
+		if( !adapterRuntimeId.equals(rtId)) {
+			return NLS.bind("Incorrect Version Error {0} {1}", 
+					adapterRt.getRuntimeType().getVersion(), 
+					getVersionString(loc));
+		}
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.ide.eclipse.as.wtp.ui.wizard.RuntimeWizardFragment#saveJreInRuntime(org.eclipse.wst.server.core.IRuntimeWorkingCopy)
+	 */
+	@Override
+	protected void saveJreInRuntime(IRuntimeWorkingCopy wc) {
+		IKarafRuntimeWorkingCopy srt = (IKarafRuntimeWorkingCopy) wc.loadAdapter(
+				IKarafRuntimeWorkingCopy.class, new NullProgressMonitor());
+		if( srt != null ) {
+			IExecutionEnvironment selectedEnv = jreComposite.getSelectedExecutionEnvironment();
+			IVMInstall selectedVM = jreComposite.getSelectedVM();
+			srt.setVM(selectedVM);
+			srt.setExecutionEnvironment(selectedEnv);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.ide.eclipse.as.wtp.ui.wizard.RuntimeWizardFragment#createHomeCompositeWidget(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	protected RuntimeHomeComposite createHomeCompositeWidget(Composite main) {
+		return new DownloadRuntimeHomeComposite(main, SWT.NONE, handle, getTaskModel());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.ide.eclipse.as.wtp.ui.wizard.RuntimeWizardFragment#saveRuntimeLocationInPreferences(org.eclipse.wst.server.core.IRuntime)
+	 */
+	@Override
+	protected void saveRuntimeLocationInPreferences(IRuntime runtime) {
+		// Do nothing
+	}
+	
 	protected class KarafJREComposite extends AbstractJREComposite {
 
 		public KarafJREComposite(Composite parent, int style, TaskModel tm) {
@@ -115,56 +214,5 @@ public class KarafRuntimeFragment extends RuntimeWizardFragment {
 			IKarafRuntime jbsrt = (IKarafRuntime)r.loadAdapter(IKarafRuntime.class, null);
 			return jbsrt.getExecutionEnvironment();
 		}
-	}
-	
-	protected AbstractJREComposite createJRECompositeWidget(Composite main) {
-		// Create our composite
-		return new KarafJREComposite(main, SWT.NONE, getTaskModel());
-	}
-	
-	protected String getExplanationText() {
-		return "Please point to a Karaf installation.";
-	}
-
-	protected void initiateHelp(Composite parent) {
-		PlatformUI.getWorkbench().getHelpSystem().setHelp(parent, "org.jboss.ide.eclipse.as.doc.user.new_server_runtime"); //$NON-NLS-1$		
-	}
-	
-	protected String getHomeVersionWarning() {
-		String homeDir = homeDirComposite.getHomeDirectory();
-		File loc = new File(homeDir);
-		String serverId = new ServerBeanLoader(loc).getServerAdapterId();
-		String rtId = serverId == null ? null : 
-				ServerCore.findServerType(serverId).getRuntimeType().getId();
-		IRuntime adapterRt = getRuntimeFromTaskModel();
-		String adapterRuntimeId = adapterRt.getRuntimeType().getId();
-		if( !adapterRuntimeId.equals(rtId)) {
-			return NLS.bind("Incorrect Version Error {0} {1}", 
-					adapterRt.getRuntimeType().getVersion(), 
-					getVersionString(loc));
-		}
-		return null;
-	}
-
-	@Override
-	protected void saveJreInRuntime(IRuntimeWorkingCopy wc) {
-		IKarafRuntimeWorkingCopy srt = (IKarafRuntimeWorkingCopy) wc.loadAdapter(
-				IKarafRuntimeWorkingCopy.class, new NullProgressMonitor());
-		if( srt != null ) {
-			IExecutionEnvironment selectedEnv = jreComposite.getSelectedExecutionEnvironment();
-			IVMInstall selectedVM = jreComposite.getSelectedVM();
-			srt.setVM(selectedVM);
-			srt.setExecutionEnvironment(selectedEnv);
-		}
-	}
-
-	@Override
-	protected RuntimeHomeComposite createHomeCompositeWidget(Composite main) {
-		return new DownloadRuntimeHomeComposite(main, SWT.NONE, handle, getTaskModel());
-	}
-
-	@Override
-	protected void saveRuntimeLocationInPreferences(IRuntime runtime) {
-		// Do nothing
 	}
 }
