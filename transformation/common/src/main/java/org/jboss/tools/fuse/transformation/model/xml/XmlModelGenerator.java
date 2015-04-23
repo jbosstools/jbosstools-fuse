8cadae1dce13b77074b89d9241d58668d38c7f15
Keith Babo <kbabo@redhat.com>
2015-04-22 20:17:49 -0400

    laying groundwork in common for issue #201

--- transformation/common/src/main/java/org/jboss/tools/fuse/transformation/model/xml/XmlModelGenerator.java
+++ transformation/common/src/main/java/org/jboss/tools/fuse/transformation/model/xml/XmlModelGenerator.java
@@ -15,15 +15,33 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.PrintStream;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.apache.xmlbeans.XmlObject;
 import org.apache.xmlbeans.XmlOptions;
 import org.apache.xmlbeans.impl.inst2xsd.Inst2Xsd;
 import org.apache.xmlbeans.impl.inst2xsd.Inst2XsdOptions;
 import org.apache.xmlbeans.impl.xb.xsdschema.SchemaDocument;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 
+import com.sun.codemodel.JAnnotationUse;
+import com.sun.codemodel.JAnnotationValue;
 import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JFormatter;
+import com.sun.codemodel.JPackage;
+import com.sun.tools.xjc.api.Mapping;
 import com.sun.tools.xjc.api.S2JJAXBModel;
 import com.sun.tools.xjc.api.SchemaCompiler;
 import com.sun.tools.xjc.api.XJC;
@@ -46,18 +64,13 @@ public class XmlModelGenerator {
     public JCodeModel generateFromSchema(final File schemaFile, final String packageName,
             final File targetPath) throws Exception {
 
-        final SchemaCompiler sc = XJC.createSchemaCompiler();
-        final FileInputStream schemaStream = new FileInputStream(schemaFile);
-        final InputSource is = new InputSource(schemaStream);
-
-        // to work around windows platform issue
-        String id = schemaFile.getAbsolutePath().replace('\\', '/');
-        is.setSystemId(id);
-
-        sc.parseSchema(is);
+        final SchemaCompiler sc = createSchemaCompiler(schemaFile);
         sc.forcePackageName(packageName);
 
         final S2JJAXBModel s2 = sc.bind();
+        if (s2 == null) {
+            throw new Exception("Failed to parse schema into JAXB Model");
+        }
         final JCodeModel jcm = s2.generateCode(null, null);
         try (PrintStream status = new PrintStream(new ByteArrayOutputStream())) {
             jcm.build(targetPath, status);
@@ -89,5 +102,86 @@ public class XmlModelGenerator {
 
         // Step 2 - call generateFromSchema with generated schema
         return generateFromSchema(schemaFile, packageName, targetPath);
+    }
+    
+    /**
+     * Returns a list of top-level element definitions for a given schema. 
+     * @param schemaFile the schema to check for top-level elements
+     * @return list of elements 
+     * @throws Exception
+     */
+    public List<QName> getElementsFromSchema(final File schemaFile) throws Exception {
+        List<QName> elements = new LinkedList<QName>();
+        SchemaCompiler sc = createSchemaCompiler(schemaFile);
+        final S2JJAXBModel s2 = sc.bind();
+        if (s2 == null) {
+            throw new Exception("Failed to parse schema into JAXB Model");
+        }
+        for (Mapping mapping : s2.getMappings()) {
+            elements.add(mapping.getElement());
+        }
+        
+        return elements;
+    }
+    
+    /**
+     * Returns the qualified name of the root element in the specified XML instance
+     * document.
+     * @param instanceFile XML instance doc
+     * @return root element name
+     * @throws Exception failed to parse instance document
+     */
+    public QName getRootElementName(final File instanceFile) throws Exception {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        Document doc = dbf.newDocumentBuilder().parse(instanceFile);
+        Element root = doc.getDocumentElement();
+        if (root == null) {
+            throw new Exception("Invalid instance document : no root element");
+        }
+        return new QName(root.getNamespaceURI(), root.getNodeName());
+    }
+    
+    /**
+     * Returns a mapping of element names to generated Java classes for a given 
+     * JCodeModel.  Note that the element names are not qualfied, they are all the
+     * local name of the element.
+     * @param model generated model from a schema or instance document
+     * @return map with element names as keys and class names as values
+     */
+    public Map<String, String> elementToClassMapping(JCodeModel model) {
+        Map<String, String> mappings = new HashMap<String, String>();
+        Iterator<JPackage> packageIt = model.packages();
+        // We need to search through all generated classes of generated packages
+        while (packageIt.hasNext()) {
+            Iterator<JDefinedClass> classIt = packageIt.next().classes();
+            while (classIt.hasNext()) {
+                JDefinedClass jClass = classIt.next();
+                // Top-level elements will result in classes with the @XmlRootElement annotation
+                // which is all we care about.
+                for (JAnnotationUse ann : jClass.annotations()) {
+                    if (ann.getAnnotationClass().fullName().equals(XmlRootElement.class.getName())) {
+                        JAnnotationValue jVal = ann.getAnnotationMembers().get("name");
+                        StringWriter sw = new StringWriter();
+                        jVal.generate(new JFormatter(sw));
+                        mappings.put(sw.toString().replaceAll("\"", ""), jClass.fullName());
+                    }
+                }
+            }
+        }
+        return mappings;
+    }
+    
+    private SchemaCompiler createSchemaCompiler(final File schemaFile) throws Exception {
+        final SchemaCompiler sc = XJC.createSchemaCompiler();
+        final FileInputStream schemaStream = new FileInputStream(schemaFile);
+        final InputSource is = new InputSource(schemaStream);
+
+        // to work around windows platform issue
+        String id = schemaFile.getAbsolutePath().replace('\\', '/');
+        is.setSystemId(id);
+        
+        sc.parseSchema(is);
+        return sc;
     }
 }
--- transformation/common/src/test/java/org/jboss/tools/fuse/transformation/model/xml/XmlModelGeneratorTest.java
+++ transformation/common/src/test/java/org/jboss/tools/fuse/transformation/model/xml/XmlModelGeneratorTest.java
@@ -14,15 +14,25 @@
 package org.jboss.tools.fuse.transformation.model.xml;
 
 import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
 
+import com.sun.codemodel.JCodeModel;
+
 public class XmlModelGeneratorTest {
     
     private static String XML_INST_PATH =
             "src/test/resources/org/jboss/tools/fuse/transformation/model/xml/abc-order.xml";
+    private static String XML_SCHEMA_PATH =
+            "src/test/resources/org/jboss/tools/fuse/transformation/model/xml/multi-element.xsd";
+    private static String XML_SCHEMA_PATH_2 =
+            "src/test/resources/org/jboss/tools/fuse/transformation/model/xml/abc-order.xsd";
     private static String XML_SCHEMA_GEN_PATH = "target/abc-order.xsd";
 
     @Test
@@ -40,5 +50,48 @@ public class XmlModelGeneratorTest {
         File genDir = new File("target/test/generateFromInstance");
         Assert.assertTrue(new File(genDir, "ObjectFactory.java").exists());
         Assert.assertTrue(new File(genDir, "ABCOrder.java").exists());
+    }
+    
+    @Test
+    public void listElementsFromSchema() throws Exception {
+        XmlModelGenerator modelGen = new XmlModelGenerator();
+        List<QName> elements = modelGen.getElementsFromSchema(new File(XML_SCHEMA_PATH));
+        Assert.assertEquals(3, elements.size());
+        Assert.assertTrue(elements.contains(new QName("urn:abc", "order-items")));
+        Assert.assertTrue(elements.contains(new QName("urn:abc", "ZABCOrder")));
+        Assert.assertTrue(elements.contains(new QName("urn:abc", "header")));
+    }
+    
+    @Test
+    public void getRootElement() throws Exception {
+        XmlModelGenerator modelGen = new XmlModelGenerator();
+        QName rootName = modelGen.getRootElementName(new File(XML_INST_PATH));
+        Assert.assertEquals(new QName("ABCOrder"), rootName);
+    }
+    
+    @Test
+    public void getGeneratedElementsMultipleElements() throws Exception {
+        File xmlSchema = new File(XML_SCHEMA_PATH);
+        XmlModelGenerator modelGen = new XmlModelGenerator();
+        JCodeModel codeModel = modelGen.generateFromSchema(
+                xmlSchema, "test.getGeneratedElementsMultipleElements", new File("target"));
+        Map<String, String> mappings = modelGen.elementToClassMapping(codeModel);
+        Assert.assertEquals(3, mappings.size());
+        Assert.assertEquals("test.getGeneratedElementsMultipleElements.Header", mappings.get("header"));
+        Assert.assertEquals("test.getGeneratedElementsMultipleElements.OrderItems", mappings.get("order-items"));
+        Assert.assertEquals("test.getGeneratedElementsMultipleElements.ZABCOrder", mappings.get("ZABCOrder"));
+        
+    }
+    
+    @Test
+    public void getGeneratedElementsSingleElement() throws Exception {
+        File xmlSchema = new File(XML_SCHEMA_PATH_2);
+        XmlModelGenerator modelGen = new XmlModelGenerator();
+        JCodeModel codeModel = modelGen.generateFromSchema(
+                xmlSchema, "test.getGeneratedElementsSingleElement", new File("target"));
+        Map<String, String> mappings = modelGen.elementToClassMapping(codeModel);
+        Assert.assertEquals(1, mappings.size());
+        Assert.assertEquals("test.getGeneratedElementsSingleElement.ABCOrder", mappings.get("ABCOrder"));
+        
     }
 }
