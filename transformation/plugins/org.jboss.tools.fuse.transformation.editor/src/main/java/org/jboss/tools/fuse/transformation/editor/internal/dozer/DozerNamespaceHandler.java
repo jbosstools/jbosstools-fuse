53bfcb1244b26f5e071effdcd9ed18e02b151782
Brian Fitzpatrick <bfitzpat@redhat.com>
2015-07-07 14:50:24 -0600

    FUSETOOLS-1330 - Adding support to select dozer files vs. all xml files FUSETOOLS-1331 - Fixed issue with errant Finish button enablement

---
+++ transformation/plugins/org.jboss.tools.fuse.transformation.editor/src/main/java/org/jboss/tools/fuse/transformation/editor/internal/dozer/DozerNamespaceHandler.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
+package org.jboss.tools.fuse.transformation.editor.internal.dozer;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import org.fusesource.ide.commons.contenttype.FindNamespaceHandlerSupport;
+
+/**
+ * @author bfitzpat
+ */
+public class DozerNamespaceHandler extends FindNamespaceHandlerSupport {
+	
+	private static String[] dozerNamespaces = new String[]{ "http://dozer.sourceforge.net" };
+	public static Set<String> dzrNamespaces = Collections.unmodifiableSet(new HashSet<String>(Arrays.asList(dozerNamespaces)));
+	
+	public DozerNamespaceHandler() {
+		super(dzrNamespaces);
+	}
+}
---
+++ transformation/plugins/org.jboss.tools.fuse.transformation.editor/src/main/java/org/jboss/tools/fuse/transformation/editor/internal/dozer/DozerXmlMatchingStrategy.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
+package org.jboss.tools.fuse.transformation.editor.internal.dozer;
+
+import org.fusesource.ide.commons.contenttype.XmlMatchingStrategySupport;
+import org.fusesource.ide.commons.contenttype.FindNamespaceHandlerSupport;
+
+/**
+ * @author bfitzpat
+ */
+public class DozerXmlMatchingStrategy extends XmlMatchingStrategySupport {
+
+	/* (non-Javadoc)
+	 * @see org.fusesource.ide.commons.contenttype.XmlMatchingStrategySupport#createNamespaceFinder()
+	 */
+	@Override
+	protected FindNamespaceHandlerSupport createNamespaceFinder() {
+		return new DozerNamespaceHandler();
+	}
+}
