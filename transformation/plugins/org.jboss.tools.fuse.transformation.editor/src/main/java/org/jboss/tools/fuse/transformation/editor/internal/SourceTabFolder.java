c0126be4abdcb38588c15c174438a8fcf5002dac
John Verhaeg <john.verhaeg@gmail.com>
2015-08-26 15:10:38 -0500

    FUSETOOLS-1334: Allow to create a transformation via drag&drop from target to source

--- transformation/plugins/org.jboss.tools.fuse.transformation.editor/src/main/java/org/jboss/tools/fuse/transformation/editor/internal/SourceTabFolder.java
+++ transformation/plugins/org.jboss.tools.fuse.transformation.editor/src/main/java/org/jboss/tools/fuse/transformation/editor/internal/SourceTabFolder.java
@@ -14,13 +14,24 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.List;
 
+import org.eclipse.jface.util.LocalSelectionTransfer;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.ViewerDropAdapter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.dnd.TransferData;
 import org.eclipse.swt.widgets.Composite;
+import org.jboss.tools.fuse.transformation.FieldMapping;
 import org.jboss.tools.fuse.transformation.Variable;
+import org.jboss.tools.fuse.transformation.editor.Activator;
+import org.jboss.tools.fuse.transformation.editor.internal.util.CanceledDialogException;
 import org.jboss.tools.fuse.transformation.editor.internal.util.TransformationConfig;
+import org.jboss.tools.fuse.transformation.editor.internal.util.Util;
 import org.jboss.tools.fuse.transformation.editor.internal.util.Util.Images;
+import org.jboss.tools.fuse.transformation.model.Model;
 
 public final class SourceTabFolder extends ModelTabFolder {
 
@@ -46,14 +57,80 @@ public final class SourceTabFolder extends ModelTabFolder {
      * @see org.jboss.tools.fuse.transformation.editor.internal.ModelTabFolder#constructModelViewer(org.jboss.tools.fuse.transformation.editor.internal.util.TransformationConfig, java.util.List)
      */
     @Override
-    ModelViewer constructModelViewer(TransformationConfig config,
-            List<PotentialDropTarget> potentialDropTargets) {
+    ModelViewer constructModelViewer(final TransformationConfig config,
+                                     List<PotentialDropTarget> potentialDropTargets) {
         final ModelViewer viewer = super.constructModelViewer(config, potentialDropTargets);
+        viewer.treeViewer.addDropSupport(DND.DROP_MOVE,
+                new Transfer[] {LocalSelectionTransfer.getTransfer()},
+                new ViewerDropAdapter(viewer.treeViewer) {
+
+            @Override
+            public boolean performDrop(final Object data) {
+                try {
+                    final Object target =
+                        ((IStructuredSelection) LocalSelectionTransfer.getTransfer()
+                                                                      .getSelection())
+                                                                      .getFirstElement();
+                    FieldMapping mapping = null;
+                    final Model sourceModel = (Model)getCurrentTarget();
+                    final Model targetModel = (Model)target;
+                    final boolean sourceIsOrInCollection = Util.isOrInCollection(sourceModel);
+                    final boolean targetIsOrInCollection = Util.isOrInCollection(targetModel);
+
+                    if (sourceIsOrInCollection == targetIsOrInCollection) {
+                        mapping = config.mapField(sourceModel, targetModel, null, null);
+                    } else try {
+                        final List<Integer> sourceIndexes = sourceIsOrInCollection
+                                                            ? Util.indexes(getShell(),
+                                                                           sourceModel, true)
+                                                            : null;
+                        final List<Integer> targetIndexes = targetIsOrInCollection
+                                                            ? Util.indexes(getShell(),
+                                                                           targetModel, false)
+                                                            : null;
+                        mapping =
+                            config.mapField(sourceModel, targetModel, sourceIndexes, targetIndexes);
+                    } catch (CanceledDialogException e) {
+                        return false;
+                    }
+
+                    if (mapping != null
+                        && Util.modelsNeedDateFormat(mapping.getSource(),
+                                                     mapping.getTarget(),
+                                                     true)) {
+                        Util.updateDateFormat(getShell(), mapping);
+                    }
+                    config.save();
+                    return true;
+                } catch (final Exception e) {
+                    Activator.error(e);
+                    return false;
+                }
+            }
+
+            @Override
+            public boolean validateDrop(final Object source,
+                                        final int operation,
+                                        final TransferData transferType) {
+                return getCurrentLocation() == ViewerDropAdapter.LOCATION_ON
+                                               && Util.draggingFromValidTarget(config)
+                                               && Util.validSourceAndTarget(source,
+                                                                            Util.draggedObject(),
+                                                                            config);
+            }
+        });
+        potentialDropTargets.add(new PotentialDropTarget(viewer.treeViewer.getTree()) {
+
+            @Override
+            public boolean valid() {
+                return Util.draggingFromValidTarget(config);
+            }
+        });
         config.addListener(new PropertyChangeListener() {
 
             @Override
             public void propertyChange(final PropertyChangeEvent event) {
-                if (event.getPropertyName().equals(TransformationConfig.MAPPING_TARGET)) {
+                if (event.getPropertyName().equals(TransformationConfig.MAPPING_SOURCE)) {
                     if (!viewer.treeViewer.getControl().isDisposed()) {
                         viewer.treeViewer.refresh();
                     }
