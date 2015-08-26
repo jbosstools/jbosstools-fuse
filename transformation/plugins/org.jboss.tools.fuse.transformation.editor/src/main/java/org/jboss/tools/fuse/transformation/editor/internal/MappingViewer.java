999a9999f7b8d2bdb891c12fe931199f69e46c3d
John Verhaeg <john.verhaeg@gmail.com>
2015-08-26 11:12:02 -0500

    FUSETOOLS-1387: NPE changing a variable value

--- transformation/plugins/org.jboss.tools.fuse.transformation.editor/src/main/java/org/jboss/tools/fuse/transformation/editor/internal/MappingViewer.java
+++ transformation/plugins/org.jboss.tools.fuse.transformation.editor/src/main/java/org/jboss/tools/fuse/transformation/editor/internal/MappingViewer.java
@@ -275,7 +275,7 @@ abstract class MappingViewer {
     }
 
     void variableValueUpdated(final Variable variable) {
-        if (variable.equals(mapping.getSource())) {
+        if (mapping != null && variable.equals(mapping.getSource())) {
             sourceText.setToolTipText(variableToolTip(variable));
         }
     }
