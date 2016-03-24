4f4776b1e0af693230ea36f4702fbb3653128b9c
Aurelien Pupier <apupier@redhat.com>
2016-03-24 15:47:53 +0100

    FUSETOOLS-1728 - reactivate roundtrip test for camel files
    and add a check of xml equivalence with the initial file

--- editor/tests/org.fusesource.ide.camel.editor.tests.integration/src/main/java/org/fusesource/ide/camel/editor/integration/XmlMarshalIT.java
+++ editor/tests/org.fusesource.ide.camel.editor.tests.integration/src/main/java/org/fusesource/ide/camel/editor/integration/XmlMarshalIT.java
@@ -20,32 +20,45 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
 import java.util.Collection;
 
+import org.assertj.core.api.Assertions;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
 import org.fusesource.ide.camel.model.service.core.io.CamelIOHandler;
 import org.fusesource.ide.camel.model.service.core.model.CamelFile;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class XmlMarshalIT {
 	
 	CamelIOHandler marshaller = new CamelIOHandler();
 	
 	@Rule
 	public TemporaryFolder testFolder = new TemporaryFolder();
+
+	private String fileNameToTest;
+
+	public XmlMarshalIT(final String fileNameToTest) {
+		this.fileNameToTest = fileNameToTest;
+	}
+
+	@Parameters
+	public static Collection<String> params() {
+		return Arrays.asList("sample.xml", "filterSample.xml", "filterSampleBlueprint.xml", "cbrSample.xml");
+	}
 
 	private IProject project;
 	
@@ -72,15 +85,19 @@ public class XmlMarshalIT {
 	}
 	
 	@Test
-	@Ignore("Test is nto passing since major refactor, need to investigate why.")
 	public void testLoadAndSaveOfSimpleModel() throws Exception {
-		assertModelRoundTrip("sample.xml", 1);
+		assertModelRoundTrip(fileNameToTest, 1);
 	}
 	
+
 	protected CamelFile assertModelRoundTrip(String name, int outputCount) throws IOException, CoreException {
 		
 		InputStream inputStream = XmlMarshalIT.class.getClassLoader().getResourceAsStream("/"+name);
 		
+		File baseFile = File.createTempFile("baseFile" + name, "xml");
+		Files.copy(inputStream, baseFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+		inputStream = XmlMarshalIT.class.getClassLoader().getResourceAsStream("/" + name);
 		IFile fileInProject = project.getFile(name);
 		fileInProject.create(inputStream, true, new NullProgressMonitor());
 
@@ -94,6 +111,7 @@ public class XmlMarshalIT {
 		String model1String = model1.getDocumentAsXML();
 		String model2String = model2.getDocumentAsXML();
 		
+		Assertions.assertThat(model1String).isXmlEqualToContentOf(baseFile);
 		assertEquals("Should have the same content", model1String, model2String);
 		
 		return model2;
