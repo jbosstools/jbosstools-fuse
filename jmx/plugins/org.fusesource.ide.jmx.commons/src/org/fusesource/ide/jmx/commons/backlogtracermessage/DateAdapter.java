435bb6ba1bfe8776032cf556f0bbf9a4cf743540
Aurelien Pupier <apupier@redhat.com>
2016-06-03 15:00:03 +0200

    FUSETOOLS-1827 - Support new Date format from JMX to display in Messages (#521)
    view

---
+++ jmx/plugins/org.fusesource.ide.jmx.commons/src/org/fusesource/ide/jmx/commons/backlogtracermessage/DateAdapter.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.fusesource.ide.jmx.commons.backlogtracermessage;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * @author Aurelien Pupier
+ *
+ */
+public class DateAdapter extends XmlAdapter<String, Date> {
+
+	protected static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+	private SimpleDateFormat dateFormat;
+
+	public DateAdapter() {
+		this.dateFormat = new SimpleDateFormat(DATE_FORMAT);
+	}
+
+	@Override
+	public Date unmarshal(String v) throws Exception {
+		return dateFormat.parse(v);
+	}
+
+	@Override
+	public String marshal(Date v) throws Exception {
+		return dateFormat.format(v);
+	}
+
+
+}
--- jmx/tests/org.fusesource.ide.jmx.commons.tests/src/test/java/org/fusesource/ide/jmx/commons/backlogtracermessage/BacklogTracerEventMessageParserTest.java
+++ jmx/tests/org.fusesource.ide.jmx.commons.tests/src/test/java/org/fusesource/ide/jmx/commons/backlogtracermessage/BacklogTracerEventMessageParserTest.java
@@ -10,7 +10,10 @@
  ******************************************************************************/
 package org.fusesource.ide.jmx.commons.backlogtracermessage;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.junit.Test;
 
@@ -19,14 +22,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BacklogTracerEventMessageParserTest {
 
 	@Test
-	public void testGetBacklogTracerEventMessage() throws Exception {
-		InputStream stream = this.getClass().getResourceAsStream("backlogTracerEventMessage.xml");
-		String xmlDump = org.apache.commons.io.IOUtils.toString(stream);
+	public void testGetBacklogTracerEventMessage_messsageParsed() throws Exception {
+		String xmlDump = backlogTracerEventMessageDump();
 
 		BacklogTracerEventMessage backlogTracerEventMessage = new BacklogTracerEventMessageParser().getBacklogTracerEventMessage(xmlDump);
 
 		assertThat(backlogTracerEventMessage.getMessage()).isNotNull();
 	}
+
+	@Test
+	public void testGetBacklogTracerEventMessage_timestampParsed() throws Exception {
+		String xmlDump = backlogTracerEventMessageDump();
+
+		BacklogTracerEventMessage backlogTracerEventMessage = new BacklogTracerEventMessageParser().getBacklogTracerEventMessage(xmlDump);
+
+		final Date dateToCheck = new SimpleDateFormat(DateAdapter.DATE_FORMAT).parse("2016-04-01T17:09:44.635+0200");
+		assertThat(backlogTracerEventMessage.getTimestamp()).hasSameTimeAs(dateToCheck);
+	}
+
+	@Test
+	public void testGetBacklogTracerEventMessage_uidParsed() throws Exception {
+		String xmlDump = backlogTracerEventMessageDump();
+
+		BacklogTracerEventMessage backlogTracerEventMessage = new BacklogTracerEventMessageParser().getBacklogTracerEventMessage(xmlDump);
+
+		assertThat(backlogTracerEventMessage.getUid()).isEqualTo(5l);
+	}
+
+	@Test
+	public void testGetBacklogTracerEventMessage_exchangeIdParsed() throws Exception {
+		String xmlDump = backlogTracerEventMessageDump();
+
+		BacklogTracerEventMessage backlogTracerEventMessage = new BacklogTracerEventMessageParser().getBacklogTracerEventMessage(xmlDump);
+
+		assertThat(backlogTracerEventMessage.getExchangeId()).isEqualTo("ID-DESKTOP-9NT300B-61151-1459523299086-0-40");
+	}
+
+	/**
+	 * @return
+	 * @throws IOException
+	 */
+	private String backlogTracerEventMessageDump() throws IOException {
+		InputStream stream = this.getClass().getResourceAsStream("backlogTracerEventMessage.xml");
+		return org.apache.commons.io.IOUtils.toString(stream);
+	}
+
 
 	@Test
 	public void testGetBacklogTracerEventMessages() throws Exception {
