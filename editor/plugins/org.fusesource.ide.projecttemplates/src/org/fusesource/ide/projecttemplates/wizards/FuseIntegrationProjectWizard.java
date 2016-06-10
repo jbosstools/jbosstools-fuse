dab9202ce2976da07e21a687d357bbc180c3305b
Lars Heinemann <lhein@apache.org>
2016-06-10 08:42:10 +0200

    FUSETOOLS-1888 - automatically switch to fuse perspective and open context file after project is created

--- editor/plugins/org.fusesource.ide.projecttemplates/src/org/fusesource/ide/projecttemplates/wizards/FuseIntegrationProjectWizard.java
+++ editor/plugins/org.fusesource.ide.projecttemplates/src/org/fusesource/ide/projecttemplates/wizards/FuseIntegrationProjectWizard.java
@@ -13,15 +13,35 @@ package org.fusesource.ide.projecttemplates.wizards;
 
 import java.lang.reflect.InvocationTargetException;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialogWithToggle;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.util.OpenStrategy;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferenceConstants;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.WorkbenchException;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.internal.ide.IDEInternalPreferences;
+import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.eclipse.ui.internal.util.PrefUtil;
+import org.eclipse.ui.internal.wizards.newresource.ResourceMessages;
 import org.fusesource.ide.projecttemplates.adopters.AbstractProjectTemplate;
 import org.fusesource.ide.projecttemplates.impl.simple.EmptyProjectTemplate;
 import org.fusesource.ide.projecttemplates.internal.Messages;
@@ -37,6 +57,8 @@ import org.fusesource.ide.projecttemplates.wizards.pages.FuseIntegrationProjectW
  */
 public class FuseIntegrationProjectWizard extends Wizard implements INewWizard {
 
+	public static final String FUSE_PERSPECTIVE_ID = "org.fusesource.ide.branding.perspective";
+	
 	protected IStructuredSelection selection;
 	
 	protected FuseIntegrationProjectWizardLocationPage locationPage;
@@ -108,11 +130,24 @@ public class FuseIntegrationProjectWizard extends Wizard implements INewWizard {
 							ProjectTemplatesActivator.pluginLog().logError("Unable to create project...", ex);
 						}
 					}
+
+					// switch perspective if needed
+					IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+					IPerspectiveDescriptor finalPersp = PlatformUI.getWorkbench().getPerspectiveRegistry().findPerspectiveWithId(FUSE_PERSPECTIVE_ID);
+					IPerspectiveDescriptor currentPersp = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getPerspective();
+					final boolean switchPerspective = currentPersp.getId().equals(finalPersp.getId()) ? false : confirmPerspectiveSwitch(workbenchWindow, finalPersp);
+					if (switchPerspective) {
+						// switch to Fuse perspective if necessary.
+						switchToFusePerspective(workbenchWindow);
+					}
+					// refresh
 					try {
 						c.getProject().refreshLocal(IProject.DEPTH_INFINITE, new NullProgressMonitor());
 					} catch (CoreException ex) {
 						ProjectTemplatesActivator.pluginLog().logError(ex);
 					}
+					// finally open the camel context file
+					openCamelContextFile(c.getProject());
 					monitor.done();
 				}
 			});
@@ -125,6 +160,122 @@ public class FuseIntegrationProjectWizard extends Wizard implements INewWizard {
 		}		
 		return true;
 	}
+	
+	/**
+	 * Switches, if necessary, the perspective of active workbench window to Fuse perspective. 
+	 *
+	 * @param workbenchWindow
+	 */
+	private void switchToFusePerspective(final IWorkbenchWindow workbenchWindow) {
+		IPerspectiveDescriptor activePerspective = workbenchWindow.getActivePage().getPerspective();
+		if (activePerspective == null || !activePerspective.getId().equals(FUSE_PERSPECTIVE_ID)) {
+			workbenchWindow.getShell().getDisplay().syncExec(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						workbenchWindow.getWorkbench().showPerspective(FUSE_PERSPECTIVE_ID, workbenchWindow);
+					} catch (WorkbenchException e) {
+						ProjectTemplatesActivator.pluginLog().logError(e);
+					}
+				}
+			});
+		}
+	}
+	
+	/**
+	 * Prompts the user for whether to switch perspectives.
+	 * 
+	 * @param window
+	 *            The workbench window in which to switch perspectives; must not
+	 *            be <code>null</code>
+	 * @param finalPersp
+	 *            The perspective to switch to; must not be <code>null</code>.
+	 * 
+	 * @return <code>true</code> if it's OK to switch, <code>false</code>
+	 *         otherwise
+	 */
+	private boolean confirmPerspectiveSwitch(IWorkbenchWindow window,
+			IPerspectiveDescriptor finalPersp) {
+		
+		IPreferenceStore store = IDEWorkbenchPlugin.getDefault().getPreferenceStore();
+		String pspm = store
+				.getString(IDEInternalPreferences.PROJECT_SWITCH_PERSP_MODE);
+		if (!IDEInternalPreferences.PSPM_PROMPT.equals(pspm)) {
+			// Return whether or not we should always switch
+			return IDEInternalPreferences.PSPM_ALWAYS.equals(pspm);
+		}
+
+		String desc = finalPersp.getDescription();
+		String message;
+		if (desc == null || desc.length() == 0)
+			message = NLS.bind(ResourceMessages.NewProject_perspSwitchMessage, finalPersp.getLabel());
+		else
+			message = NLS.bind(ResourceMessages.NewProject_perspSwitchMessageWithDesc, new String[] { finalPersp.getLabel(), desc });
+
+		MessageDialogWithToggle dialog = MessageDialogWithToggle
+				.openYesNoQuestion(window.getShell(),
+						ResourceMessages.NewProject_perspSwitchTitle, message,
+						null /* use the default message for the toggle */,
+						false /* toggle is initially unchecked */, store,
+						IDEInternalPreferences.PROJECT_SWITCH_PERSP_MODE);
+		int result = dialog.getReturnCode();
+		
+		// If we are not going to prompt anymore propagate the choice.
+		if (dialog.getToggleState()) {
+			String preferenceValue;
+			if (result == IDialogConstants.YES_ID) {
+				// Doesn't matter if it is replace or new window
+				// as we are going to use the open perspective setting
+				preferenceValue = IWorkbenchPreferenceConstants.OPEN_PERSPECTIVE_REPLACE;
+			} else {
+				preferenceValue = IWorkbenchPreferenceConstants.NO_NEW_PERSPECTIVE;
+			}
+
+			// update PROJECT_OPEN_NEW_PERSPECTIVE to correspond
+			PrefUtil.getAPIPreferenceStore().setValue(IDE.Preferences.PROJECT_OPEN_NEW_PERSPECTIVE,	preferenceValue);
+		}
+		return result == IDialogConstants.YES_ID;
+	}
+
+	/**
+	 * Open the first detected camel context file in the editor
+	 * @param project
+	 */
+	private void openCamelContextFile(IProject project){
+		if(project!=null){
+			final IFile[] holder = new IFile[1];
+			try {
+				project.accept(new IResourceVisitor() {	//look for camel content types in the project			
+					@Override
+					public boolean visit(IResource resource) throws CoreException {
+						if(resource instanceof IFile){
+							IFile file = (IFile)resource;
+							if (file.getContentDescription() != null
+									&& "org.fusesource.ide.camel.editor.camelContentType"
+											.equals(file.getContentDescription().getContentType().getId())) {
+								holder[0]=file;
+							}
+						}
+						return holder[0]==null;//keep looking if we haven't found one yet 
+					}
+				});
+			} catch (CoreException e1) {
+				ProjectTemplatesActivator.pluginLog().logError(e1);
+			}
+			if(holder[0]!=null){
+				Display.getDefault().asyncExec(new Runnable() {
+					public void run() {
+						try {
+							IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(),
+									holder[0], OpenStrategy.activateOnOpen());
+						} catch (PartInitException e) {
+							ProjectTemplatesActivator.pluginLog().logError("Cannot open camel context file in editor", e);
+						}
+					}
+				});
+			}
+		}
+	}	
 	
 	/* (non-Javadoc)
 	 * @see org.eclipse.jface.wizard.Wizard#addPages()
