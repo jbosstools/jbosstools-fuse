63b63d2e3abbae28acd8b00b26199db4216088ae
Lars Heinemann <lhein@apache.org>
2016-06-14 15:30:27 +0200

    move handler to add/remove camel nature into configure context menu

---
+++ editor/plugins/org.fusesource.ide.project/src/org/fusesource/ide/project/AddCamelNatureAction.java
@@ -0,0 +1,71 @@
+/******************************************************************************* 
+ * Copyright (c) 2016 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.fusesource.ide.project;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.fusesource.ide.foundation.ui.logging.RiderLogFacade;
+import org.fusesource.ide.foundation.ui.util.Selections;
+
+/**
+ * @author lhein
+ */
+public class AddCamelNatureAction implements IObjectActionDelegate {
+
+	/** The current selection (a project). */
+	private ISelection selection = null;
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+	 */
+	@Override
+	public void run(IAction action) {
+		if (selection instanceof IStructuredSelection) {
+			IProject project = (IProject) Selections.getFirstSelection(selection);
+			try {
+				IProjectDescription description = project.getDescription();
+				String[] natures = description.getNatureIds();
+
+				// Add the nature
+				String[] newNatures = new String[natures.length + 1];
+				System.arraycopy(natures, 0, newNatures, 0, natures.length);
+				newNatures[natures.length] = RiderProjectNature.NATURE_ID;
+				description.setNatureIds(newNatures);
+				project.setDescription(description, null);
+			} catch (CoreException e) {
+				RiderLogFacade.getLog(Activator.getDefault().getLog()).error(e);
+			}
+		} else {
+			RiderLogFacade.getLog(Activator.getDefault().getLog()).error("Cannot add Camel support on selection of type " + selection.getClass().getName());
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+	 */
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		this.selection = selection;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IObjectActionDelegate#setActivePart(org.eclipse.jface.action.IAction, org.eclipse.ui.IWorkbenchPart)
+	 */
+	@Override
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	}
+}
