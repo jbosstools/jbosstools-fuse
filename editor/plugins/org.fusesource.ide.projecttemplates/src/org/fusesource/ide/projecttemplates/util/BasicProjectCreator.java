206bd1a2dc27df05de8c2b45951d8289a773d742
Aurélien Pupier <apupier@redhat.com>
2016-06-21 17:20:25 +0200

    FUSETOOLS-1890 - Support creation of project outside of workspace (#550)
    - also add improved validation when using a specific folder inside the
    workspace

--- editor/plugins/org.fusesource.ide.projecttemplates/src/org/fusesource/ide/projecttemplates/util/BasicProjectCreator.java
+++ editor/plugins/org.fusesource.ide.projecttemplates/src/org/fusesource/ide/projecttemplates/util/BasicProjectCreator.java
@@ -1,43 +1,42 @@
-/******************************************************************************* 
- * Copyright (c) 2016 Red Hat, Inc. 
- * Distributed under license by Red Hat, Inc. All rights reserved. 
- * This program is made available under the terms of the 
- * Eclipse Public License v1.0 which accompanies this distribution, 
- * and is available at http://www.eclipse.org/legal/epl-v10.html 
- * 
- * Contributors: 
- * Red Hat, Inc. - initial API and implementation 
- ******************************************************************************/ 
+/*******************************************************************************
+ * Copyright (c) 2016 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.fusesource.ide.projecttemplates.util;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.fusesource.ide.projecttemplates.internal.ProjectTemplatesActivator;
 
 /**
  * a basic project creator which only creates the project folder
  * and some basic configuration
- * 
+ *
  * @author lhein
  */
 public class BasicProjectCreator {
 
 	private IProject project;
 	private NewProjectMetaData metadata;
-	
-	/**
-	 * 
-	 */
+
 	public BasicProjectCreator(NewProjectMetaData metadata) {
 		this.metadata = metadata;
 	}
 
 	/**
 	 * creates the project
-	 * 
+	 *
 	 * @param monitor
 	 * @return	true on success
 	 */
@@ -46,18 +45,25 @@ public class BasicProjectCreator {
 			// first create the project
 			IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 			project = root.getProject(metadata.getProjectName());
-			project.create(monitor);
+			final IPath specificLocationPath = metadata.getLocationPath();
+			if (specificLocationPath != null) {
+				IProjectDescription description = ResourcesPlugin.getWorkspace().newProjectDescription(metadata.getProjectName());
+				description.setLocation(specificLocationPath);
+				project.create(description, monitor);
+			} else {
+				project.create(monitor);
+			}
 			project.open(monitor);
 		} catch (CoreException ex) {
 			ProjectTemplatesActivator.pluginLog().logError(ex);
 			return false;
-		} 
+		}
 		return true;
-	}	
-	
+	}
+
 	/**
 	 * returns the created project
-	 * 
+	 *
 	 * @return
 	 */
 	public IProject getProject() {
--- editor/tests/org.fusesource.ide.projecttemplates.tests.integration/src/org/fusesource/ide/projecttemplates/tests/integration/wizards/FuseIntegrationProjectCreatorRunnableIT.java
+++ editor/tests/org.fusesource.ide.projecttemplates.tests.integration/src/org/fusesource/ide/projecttemplates/tests/integration/wizards/FuseIntegrationProjectCreatorRunnableIT.java
@@ -10,6 +10,8 @@
  ******************************************************************************/
 package org.fusesource.ide.projecttemplates.tests.integration.wizards;
 
+import java.io.File;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,6 +24,7 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
@@ -41,7 +44,9 @@ import org.fusesource.ide.projecttemplates.wizards.FuseIntegrationProjectCreator
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -50,6 +55,9 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  */
 public class FuseIntegrationProjectCreatorRunnableIT {
+
+	@Rule
+	public TemporaryFolder tmpFolder = new TemporaryFolder();
 
 	private IProject project = null;
 	boolean deploymentFinished = false;
@@ -71,34 +79,39 @@ public class FuseIntegrationProjectCreatorRunnableIT {
 
 	@Test
 	public void testEmptyBlueprintProjectCreation() throws Exception {
-		testEmptyProjectCreation("-SimpleBlueprintProject", CamelDSLType.BLUEPRINT, "src/main/resources/OSGI-INF/blueprint/blueprint.xml");
+		testEmptyProjectCreation("-SimpleBlueprintProject", CamelDSLType.BLUEPRINT, "src/main/resources/OSGI-INF/blueprint/blueprint.xml", null);
 	}
 
 	@Test
 	public void testEmptySpringProjectCreation() throws Exception {
-		testEmptyProjectCreation("-SimpleSpringProject", CamelDSLType.SPRING, "src/main/resources/META-INF/spring/camel-context.xml");
+		testEmptyProjectCreation("-SimpleSpringProject", CamelDSLType.SPRING, "src/main/resources/META-INF/spring/camel-context.xml", null);
+	}
+
+	@Test
+	public void testEmptySpringProjectCreationOnLocationOutsideWorkspace() throws Exception {
+		NewProjectMetaData metadata = createDefaultNewProjectMetadata(CamelDSLType.SPRING,
+				FuseIntegrationProjectCreatorRunnableIT.class.getSimpleName() + "-SimpleSpringProject_outsideProject");
+		File folderForprojectOutsiddeWorkspaceLocation = tmpFolder.newFolder("folderForProjectOutsideWorkspaceLocation");
+		final Path locationPath = new Path(folderForprojectOutsiddeWorkspaceLocation.getAbsolutePath());
+		metadata.setLocationPath(locationPath);
+		testEmptyProjectCreation("-SimpleSpringProject_outsideProject", CamelDSLType.SPRING, "src/main/resources/META-INF/spring/camel-context.xml", metadata);
+
+		assertThat(Files.isSameFile(project.getLocation().toFile().toPath(), locationPath.toFile().toPath())).isTrue();
 	}
 
 	@Test
 	@Ignore("Deactivate Java DSL test to move forward - failing randomly on CI only")
 	public void testEmptyJavaProjectCreation() throws Exception {
-		testEmptyProjectCreation("-SimpleJavaProject", CamelDSLType.JAVA, "src/main/java/com/mycompany/camel/CamelRoute.java");
+		testEmptyProjectCreation("-SimpleJavaProject", CamelDSLType.JAVA, "src/main/java/com/mycompany/camel/CamelRoute.java", null);
 	}
 
-	private void testEmptyProjectCreation(String projectNameSuffix, CamelDSLType dsl, String camelFilePath) throws Exception {
+	private void testEmptyProjectCreation(String projectNameSuffix, CamelDSLType dsl, String camelFilePath, NewProjectMetaData metadata) throws Exception {
 		final String projectName = FuseIntegrationProjectCreatorRunnableIT.class.getSimpleName() + projectNameSuffix;
 		assertThat(ResourcesPlugin.getWorkspace().getRoot().getProject(projectName).exists()).isFalse();
 
-		NewProjectMetaData metadata = new NewProjectMetaData();
-		metadata.setProjectName(projectName);
-		metadata.setLocationPath(null);
-		// TODO use latest version, or a parameterized test to test all versions
-		// available CamelModelFactory.getLatestCamelVersion()
-		metadata.setCamelVersion("2.15.1.redhat-621084");
-		metadata.setTargetRuntime(null);
-		metadata.setDslType(dsl);
-		metadata.setBlankProject(true);
-		metadata.setTemplate(null);
+		if (metadata == null) {
+			metadata = createDefaultNewProjectMetadata(dsl, projectName);
+		}
 
 		new ProgressMonitorDialog(Display.getDefault().getActiveShell()).run(false, true, new FuseIntegrationProjectCreatorRunnable(metadata));
 
@@ -118,6 +131,26 @@ public class FuseIntegrationProjectCreatorRunnableIT {
 		// TODO: currently we generate completely project which are not valid so
 		// cannot be launched
 		// launchDebug(project);
+	}
+
+	/**
+	 * @param dsl
+	 * @param projectName
+	 * @return
+	 */
+	private NewProjectMetaData createDefaultNewProjectMetadata(CamelDSLType dsl, final String projectName) {
+		NewProjectMetaData metadata;
+		metadata = new NewProjectMetaData();
+		metadata.setProjectName(projectName);
+		metadata.setLocationPath(null);
+		// TODO use latest version, or a parameterized test to test all versions
+		// available CamelModelFactory.getLatestCamelVersion()
+		metadata.setCamelVersion("2.15.1.redhat-621084");
+		metadata.setTargetRuntime(null);
+		metadata.setDslType(dsl);
+		metadata.setBlankProject(true);
+		metadata.setTemplate(null);
+		return metadata;
 	}
 
 	private void waitJob() throws OperationCanceledException, InterruptedException {
