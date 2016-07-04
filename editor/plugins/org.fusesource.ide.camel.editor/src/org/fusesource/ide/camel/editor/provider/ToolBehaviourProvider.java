fe8ca6159ebedb7ccf3b51f444296bf896af2b7a
Lars Heinemann <lhein@apache.org>
2016-07-04 20:59:54 +0200

    fixed broken integration test

--- editor/plugins/org.fusesource.ide.camel.editor/src/org/fusesource/ide/camel/editor/provider/ToolBehaviourProvider.java
+++ editor/plugins/org.fusesource.ide.camel.editor/src/org/fusesource/ide/camel/editor/provider/ToolBehaviourProvider.java
@@ -742,8 +742,13 @@ public class ToolBehaviourProvider extends DefaultToolBehaviorProvider {
 		ArrayList<IToolEntry> entries = new ArrayList<IToolEntry>();
 
 		// inject palette entries generated out of the component model file
-		CamelDesignEditor editor = (CamelDesignEditor)getDiagramTypeProvider().getDiagramBehavior().getDiagramContainer();
-		String camelVersion = CamelUtils.getCurrentProjectCamelVersion(editor);
+		CamelDesignEditor editor = null;
+		if (getDiagramTypeProvider() != null && 
+			getDiagramTypeProvider().getDiagramBehavior() != null &&
+			getDiagramTypeProvider().getDiagramBehavior().getDiagramContainer() != null) {
+			editor = (CamelDesignEditor)getDiagramTypeProvider().getDiagramBehavior().getDiagramContainer();
+		}
+		String camelVersion = editor != null ? CamelUtils.getCurrentProjectCamelVersion(editor) : CamelUtils.getCurrentProjectCamelVersion();
 
 		ComponentModel componentModel = CamelModelFactory.getModelForVersion(camelVersion).getComponentModel();
 		for (Component component : componentModel.getSupportedComponents()) {
