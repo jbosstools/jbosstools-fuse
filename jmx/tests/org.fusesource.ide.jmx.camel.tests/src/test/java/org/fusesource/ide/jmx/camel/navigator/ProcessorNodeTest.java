f58eb4de3129cc70210121f2330e44537b2bc3ef
Aurelien Pupier <apupier@redhat.com>
2016-07-11 17:43:17 +0200

    FUSETOOLS-1909 - Support display of element with same id in 2 routes

---
+++ jmx/tests/org.fusesource.ide.jmx.camel.tests/src/test/java/org/fusesource/ide/jmx/camel/navigator/ProcessorNodeTest.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.fusesource.ide.jmx.camel.navigator;
+
+
+import org.fusesource.ide.camel.model.service.core.model.CamelBasicModelElement;
+import org.fusesource.ide.camel.model.service.core.model.CamelRouteElement;
+import org.jboss.tools.jmx.core.IConnectionProvider;
+import org.jboss.tools.jmx.core.IConnectionWrapper;
+import org.jboss.tools.jmx.core.tree.Root;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.mockito.Mockito.doReturn;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProcessorNodeTest {
+	
+	@Mock
+	IConnectionWrapper connectionWrapper;
+	
+	@Mock
+	IConnectionProvider connectionProvider;
+	
+	@Before
+	public void setup(){
+		doReturn(connectionProvider).when(connectionWrapper).getProvider();
+	}
+	
+	@Test
+	public void testHashCodeDifferentForSameIdInDifferentRoute() throws Exception {
+		CamelRouteElement route1 = new CamelRouteElement(null, null);
+		route1.setId("Route1");
+		CamelBasicModelElement basicElement = new CamelBasicModelElement(null, null);
+		basicElement.setId("sameId");
+		RouteNode routeNode1 = new RouteNode(new RoutesNode(new CamelContextNode(new CamelContextsNode(new Root(connectionWrapper), null), null, null)), route1);
+		ProcessorNode processorNode1 = new ProcessorNode(routeNode1, routeNode1, basicElement);
+		
+		
+		CamelRouteElement route2 = new CamelRouteElement(null, null);
+		route2.setId("Route2");
+		CamelBasicModelElement basicElement2 = new CamelBasicModelElement(null, null);
+		basicElement2.setId("sameId");
+		RouteNode routeNode2 = new RouteNode(new RoutesNode(new CamelContextNode(new CamelContextsNode(new Root(connectionWrapper), null), null, null)), route2);
+		ProcessorNode processorNode2 = new ProcessorNode(routeNode2, routeNode2, basicElement2);
+
+		assertThat(processorNode2.hashCode()).isNotEqualTo(processorNode1.hashCode());
+	}
+
+}
