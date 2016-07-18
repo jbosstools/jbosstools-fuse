79a5dcc5395e3be8150ce88043d7326900d3168e
Brian Fitzpatrick <bfitzpat@redhat.com>
2016-07-18 16:19:08 -0600

    FUSETOOLS-1956 - adding java dsl CXF contract first (#574)

--- templates/medium/cxf-contract-first/medium-fuse-cxf-contract-first-java/src/test/java/com/mycompany/camel/OrderTest.java
+++ templates/medium/cxf-contract-first/medium-fuse-cxf-contract-first-java/src/test/java/com/mycompany/camel/OrderTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ws.camel.mycompany.com;
+package com.mycompany.camel;
 
 import org.junit.Assert;
 import org.junit.Test;
