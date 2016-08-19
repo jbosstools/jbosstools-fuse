69e8a44bba58f4eadd76f406c5f823897db91af9
Aurelien Pupier <apupier@redhat.com>
2016-08-19 16:56:25 +0200

    FUSETOOLS-2019 - Ensure Launch shortcuts available on source editor
    - fix regression introduced by commit
    687e901ddfbf85a37470ed1c55283279bf9dd630 which is hiding Launch
    shortcuts for local camel context in case of missing camel-maven-plugin

--- editor/tests/org.fusesource.ide.launcher.ui.tests/src/test/java/org/fusesource/ide/launcher/ui/propertytester/CamelLocalLaunchPropertyTesterTest.java
+++ editor/tests/org.fusesource.ide.launcher.ui.tests/src/test/java/org/fusesource/ide/launcher/ui/propertytester/CamelLocalLaunchPropertyTesterTest.java
@@ -17,14 +17,14 @@ import static org.mockito.Mockito.spy;
 import org.apache.maven.model.Build;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.Plugin;
-import org.assertj.core.api.Assertions;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.fusesource.ide.foundation.ui.io.CamelXMLEditorInput;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -51,6 +51,7 @@ public class CamelLocalLaunchPropertyTesterTest {
 		build.getPlugins().add(camelPlugin);
 		mavenModel.setBuild(build);
 		doReturn(mavenModel ).when(camelLocalLaunchPropertyTester).retrieveMavenModel(project);
+		doReturn(project).when(project).getAdapter(IResource.class);
 		
 		boolean res = camelLocalLaunchPropertyTester.test(project, CamelLocalLaunchPropertyTester.IS_LOCAL_LAUNCH_AVAILABLE, null, null);
 		
@@ -79,8 +80,27 @@ public class CamelLocalLaunchPropertyTesterTest {
 		build.getPlugins().add(camelPlugin);
 		mavenModel.setBuild(build);
 		doReturn(mavenModel).when(camelLocalLaunchPropertyTester).retrieveMavenModel(file);
+		doReturn(file).when(file).getAdapter(IResource.class);
 		
 		boolean res = camelLocalLaunchPropertyTester.test(file, CamelLocalLaunchPropertyTester.IS_LOCAL_LAUNCH_AVAILABLE, null, null);
+		
+		assertThat(res).isTrue();
+	}
+	
+	@Test
+	public void test_ReturnTrue_ForCamelXMLEditorInput() throws Exception {
+		CamelLocalLaunchPropertyTester camelLocalLaunchPropertyTester = spy(new CamelLocalLaunchPropertyTester());
+		Model mavenModel = new Model();
+		Build build = new Build();
+		Plugin camelPlugin = new Plugin();
+		camelPlugin.setGroupId("org.apache.camel");
+		camelPlugin.setArtifactId("camel-maven-plugin");
+		build.getPlugins().add(camelPlugin);
+		mavenModel.setBuild(build);
+		doReturn(mavenModel).when(camelLocalLaunchPropertyTester).retrieveMavenModel(file);
+		doReturn(file).when(file).getAdapter(IResource.class);
+		
+		boolean res = camelLocalLaunchPropertyTester.test(new CamelXMLEditorInput(file, null), CamelLocalLaunchPropertyTester.IS_LOCAL_LAUNCH_AVAILABLE, null, null);
 		
 		assertThat(res).isTrue();
 	}

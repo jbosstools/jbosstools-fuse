jbosstools/jbosstools-fuse

commit 687e901ddfbf85a37470ed1c55283279bf9dd630

FUSETOOLS-1977 - Enable local launch only when camel-maven-plugin (#636)
available

editor/plugins/org.fusesource.ide.launcher.ui/plugin.xml
@@ -238,12 +238,13 @@
       </image>
    </extension>
  
-  <extension
+    <extension
        point="org.eclipse.core.expressions.definitions">
     	<definition id="org.fusesource.ide.launcher.ui.launchCamelEnable">
                  <with variable="selection">
                      <count value="1"/>
                      <iterate>
+                       <and>
                        <or>
                          <adapt
                                type="org.eclipse.core.resources.IFile">
@@ -255,11 +256,27 @@
 						 <and>
 							<instanceof value="org.eclipse.core.resources.IProject" />
           					<test property="org.eclipse.core.resources.projectNature" value="org.fusesource.ide.project.RiderProjectNature" />
-						 </and>                         
-                       </or>  
+						 </and>
+                       </or>
+                       <test
+                             forcePluginActivation="true"
+                             property="org.fusesource.ide.launcher.ui.isLocalLaunchAvailable">
+                       </test>
+                        
+						 </and>                 
                      </iterate>
                  </with>
     	</definition>
-   </extension>   
+   </extension>
+  <extension
+        point="org.eclipse.core.expressions.propertyTesters">
+     <propertyTester
+           class="org.fusesource.ide.launcher.ui.propertytester.CamelLocalLaunchPropertyTester"
+           id="org.fusesource.ide.launcher.ui.localLaunchPropertyTester"
+           namespace="org.fusesource.ide.launcher.ui"
+           properties="isLocalLaunchAvailable"
+           type="org.eclipse.core.resources.IResource">
+     </propertyTester>
+  </extension>  
    
 </plugin>

editor/plugins/org.fusesource.ide.launcher.ui/src/org/fusesource/ide/launcher/ui/propertytester/CamelLocalLaunchPropertyTester.java
@@ -1,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.fusesource.ide.launcher.ui.propertytester;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Plugin;
+import org.apache.maven.model.PluginManagement;
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.m2e.core.MavenPlugin;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.fusesource.ide.launcher.ui.Activator;
+
+public class CamelLocalLaunchPropertyTester extends PropertyTester {
+
+	private static final String CAMEL_MAVEN_PLUGIN = "camel-maven-plugin";
+	private static final String ORG_APACHE_CAMEL = "org.apache.camel";
+	static final String IS_LOCAL_LAUNCH_AVAILABLE = "isLocalLaunchAvailable";
+
+	public CamelLocalLaunchPropertyTester() {
+		// Keep for reflection instantiation
+	}
+
+	@Override
+	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+		if(receiver instanceof IResource && IS_LOCAL_LAUNCH_AVAILABLE.equals(property)){
+			Model mavenModel = retrieveMavenModel((IResource) receiver);
+			if(mavenModel != null){
+				List<Plugin> plugins = retrievePlugins(mavenModel);
+				return containsCamelMavenPlugin(plugins);
+			}
+		}
+		return false;
+	}
+
+	Model retrieveMavenModel(IResource receiver) {
+		IProject project = receiver.getProject();
+		IMavenProjectFacade facade = MavenPlugin.getMavenProjectRegistry().getProject(project);
+		try {
+			return facade.getMavenProject(new NullProgressMonitor()).getModel();
+		} catch (CoreException e) {
+			Activator.getLogger().error(e);
+		}
+		return null;
+	}
+
+	private List<Plugin> retrievePlugins(Model mavenModel) {
+		Build build = mavenModel.getBuild();
+		List<Plugin> plugins = new ArrayList<>();
+		if(build != null){
+			plugins.addAll(build.getPlugins());
+			PluginManagement pluginManagement = build.getPluginManagement();
+			if(pluginManagement != null){
+				plugins.addAll(pluginManagement.getPlugins());
+			}
+		}
+		return plugins;
+	}
+
+	private boolean containsCamelMavenPlugin(List<Plugin> plugins) {
+		return plugins.parallelStream().filter(this::isCamelMavenPlugin).findFirst().isPresent();
+	}
+	
+	private boolean isCamelMavenPlugin(Plugin plugin) {
+		return ORG_APACHE_CAMEL.equals(plugin.getGroupId()) && CAMEL_MAVEN_PLUGIN.equals(plugin.getArtifactId());
+	}
+
+}

editor/tests/org.fusesource.ide.launcher.ui.tests/src/test/java/org/fusesource/ide/launcher/ui/propertytester/CamelLocalLaunchPropertyTesterTest.java
@@ -1,0 +1,97 @@
+/******************************************************************************* 
+ * Copyright (c) 2016 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.fusesource.ide.launcher.ui.propertytester;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Plugin;
+import org.assertj.core.api.Assertions;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CamelLocalLaunchPropertyTesterTest {
+	
+	@Mock
+	private IProject project;
+	@Mock
+	private IFile file;
+	
+	@Before
+	public void setup(){
+		doReturn(project).when(file).getProject();
+	}
+
+	@Test
+	public void test_ReturnTrue_ForProject() throws Exception {
+		CamelLocalLaunchPropertyTester camelLocalLaunchPropertyTester = spy(new CamelLocalLaunchPropertyTester());
+		Model mavenModel = new Model();
+		Build build = new Build();
+		Plugin camelPlugin = new Plugin();
+		camelPlugin.setGroupId("org.apache.camel");
+		camelPlugin.setArtifactId("camel-maven-plugin");
+		build.getPlugins().add(camelPlugin);
+		mavenModel.setBuild(build);
+		doReturn(mavenModel ).when(camelLocalLaunchPropertyTester).retrieveMavenModel(project);
+		
+		boolean res = camelLocalLaunchPropertyTester.test(project, CamelLocalLaunchPropertyTester.IS_LOCAL_LAUNCH_AVAILABLE, null, null);
+		
+		assertThat(res).isTrue();
+	}
+	
+	@Test
+	public void test_ReturnFalse_WhenNoBuildInMavenModel() throws Exception {
+		CamelLocalLaunchPropertyTester camelLocalLaunchPropertyTester = spy(new CamelLocalLaunchPropertyTester());
+		Model mavenModel = new Model();
+		doReturn(mavenModel ).when(camelLocalLaunchPropertyTester).retrieveMavenModel(project);
+		
+		boolean res = camelLocalLaunchPropertyTester.test(project, CamelLocalLaunchPropertyTester.IS_LOCAL_LAUNCH_AVAILABLE, null, null);
+		
+		assertThat(res).isFalse();
+	}
+	
+	@Test
+	public void test_ReturnTrue_ForFile() throws Exception {
+		CamelLocalLaunchPropertyTester camelLocalLaunchPropertyTester = spy(new CamelLocalLaunchPropertyTester());
+		Model mavenModel = new Model();
+		Build build = new Build();
+		Plugin camelPlugin = new Plugin();
+		camelPlugin.setGroupId("org.apache.camel");
+		camelPlugin.setArtifactId("camel-maven-plugin");
+		build.getPlugins().add(camelPlugin);
+		mavenModel.setBuild(build);
+		doReturn(mavenModel).when(camelLocalLaunchPropertyTester).retrieveMavenModel(file);
+		
+		boolean res = camelLocalLaunchPropertyTester.test(file, CamelLocalLaunchPropertyTester.IS_LOCAL_LAUNCH_AVAILABLE, null, null);
+		
+		assertThat(res).isTrue();
+	}
+	
+	@Test
+	public void test_ReturnFalse_ForNotMavenProject() throws Exception {
+		CamelLocalLaunchPropertyTester camelLocalLaunchPropertyTester = spy(new CamelLocalLaunchPropertyTester());
+		doReturn(null).when(camelLocalLaunchPropertyTester).retrieveMavenModel(project);
+		
+		boolean res = camelLocalLaunchPropertyTester.test(project, CamelLocalLaunchPropertyTester.IS_LOCAL_LAUNCH_AVAILABLE, null, null);
+		
+		assertThat(res).isFalse();
+	}
+}

editor/plugins/org.fusesource.ide.launcher.ui/META-INF/MANIFEST.MF
@@ -6,6 +6,7 @@ Bundle-Version: 8.0.0.qualifier
 Bundle-Vendor: %Bundle-Vendor
 Bundle-RequiredExecutionEnvironment: JavaSE-1.7
 Require-Bundle: org.eclipse.ui,
+ org.eclipse.core.expressions;bundle-version="3.5.0",
  org.eclipse.core.resources,
  org.eclipse.core.runtime,
  org.eclipse.ui.ide,
@@ -18,6 +19,7 @@ Require-Bundle: org.eclipse.ui,
  org.eclipse.ui.externaltools,
  org.eclipse.m2e.archetype.common,
  org.eclipse.m2e.core,
+ org.eclipse.m2e.maven.runtime;bundle-version="1.6.3",
  org.eclipse.m2e.core.ui,
  org.eclipse.m2e.discovery,
  org.eclipse.m2e.editor,

commit 69e8a44bba58f4eadd76f406c5f823897db91af9

FUSETOOLS-2019 - Ensure Launch shortcuts available on source editor
- fix regression introduced by commit
687e901ddfbf85a37470ed1c55283279bf9dd630 which is hiding Launch
shortcuts for local camel context in case of missing camel-maven-plugin

editor/plugins/org.fusesource.ide.launcher.ui/plugin.xml
@@ -275,7 +275,7 @@
            id="org.fusesource.ide.launcher.ui.localLaunchPropertyTester"
            namespace="org.fusesource.ide.launcher.ui"
            properties="isLocalLaunchAvailable"
-           type="org.eclipse.core.resources.IResource">
+           type="org.eclipse.core.runtime.IAdaptable">
      </propertyTester>
   </extension>  
    

editor/plugins/org.fusesource.ide.launcher.ui/src/org/fusesource/ide/launcher/ui/propertytester/CamelLocalLaunchPropertyTester.java
@@ -21,6 +21,7 @@ import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.m2e.core.MavenPlugin;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
@@ -38,11 +39,14 @@ public class CamelLocalLaunchPropertyTester extends PropertyTester {
 
 	@Override
 	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-		if(receiver instanceof IResource && IS_LOCAL_LAUNCH_AVAILABLE.equals(property)){
-			Model mavenModel = retrieveMavenModel((IResource) receiver);
-			if(mavenModel != null){
-				List<Plugin> plugins = retrievePlugins(mavenModel);
-				return containsCamelMavenPlugin(plugins);
+		if(receiver instanceof IAdaptable){
+			IResource resource = ((IAdaptable) receiver).getAdapter(IResource.class);
+			if(resource != null && IS_LOCAL_LAUNCH_AVAILABLE.equals(property)){
+				Model mavenModel = retrieveMavenModel(resource);
+				if(mavenModel != null){
+					List<Plugin> plugins = retrievePlugins(mavenModel);
+					return containsCamelMavenPlugin(plugins);
+				}
 			}
 		}
 		return false;

editor/tests/org.fusesource.ide.launcher.ui.tests/src/test/java/org/fusesource/ide/launcher/ui/propertytester/CamelLocalLaunchPropertyTesterTest.java
@@ -17,14 +17,14 @@ import static org.mockito.Mockito.spy;
 import org.apache.maven.model.Build;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.Plugin;
-import org.assertj.core.api.Assertions;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.fusesource.ide.foundation.ui.io.CamelXMLEditorInput;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -51,6 +51,7 @@ public class CamelLocalLaunchPropertyTesterTest {
 		build.getPlugins().add(camelPlugin);
 		mavenModel.setBuild(build);
 		doReturn(mavenModel ).when(camelLocalLaunchPropertyTester).retrieveMavenModel(project);
+		doReturn(project).when(project).getAdapter(IResource.class);
 		
 		boolean res = camelLocalLaunchPropertyTester.test(project, CamelLocalLaunchPropertyTester.IS_LOCAL_LAUNCH_AVAILABLE, null, null);
 		
@@ -79,8 +80,27 @@ public class CamelLocalLaunchPropertyTesterTest {
 		build.getPlugins().add(camelPlugin);
 		mavenModel.setBuild(build);
 		doReturn(mavenModel).when(camelLocalLaunchPropertyTester).retrieveMavenModel(file);
+		doReturn(file).when(file).getAdapter(IResource.class);
 		
 		boolean res = camelLocalLaunchPropertyTester.test(file, CamelLocalLaunchPropertyTester.IS_LOCAL_LAUNCH_AVAILABLE, null, null);
+		
+		assertThat(res).isTrue();
+	}
+	
+	@Test
+	public void test_ReturnTrue_ForCamelXMLEditorInput() throws Exception {
+		CamelLocalLaunchPropertyTester camelLocalLaunchPropertyTester = spy(new CamelLocalLaunchPropertyTester());
+		Model mavenModel = new Model();
+		Build build = new Build();
+		Plugin camelPlugin = new Plugin();
+		camelPlugin.setGroupId("org.apache.camel");
+		camelPlugin.setArtifactId("camel-maven-plugin");
+		build.getPlugins().add(camelPlugin);
+		mavenModel.setBuild(build);
+		doReturn(mavenModel).when(camelLocalLaunchPropertyTester).retrieveMavenModel(file);
+		doReturn(file).when(file).getAdapter(IResource.class);
+		
+		boolean res = camelLocalLaunchPropertyTester.test(new CamelXMLEditorInput(file, null), CamelLocalLaunchPropertyTester.IS_LOCAL_LAUNCH_AVAILABLE, null, null);
 		
 		assertThat(res).isTrue();
 	}