4d3e29cdb25dae14b5de1df2feb335fdce9b11cf
Lars Heinemann <lhein@apache.org>
2016-08-25 12:02:03 +0200

    fixed amq dependencymanager

--- editor/plugins/org.fusesource.ide.camel.editor/src/org/fusesource/ide/camel/editor/provider/ActiveMQPaletteEntryDependenciesManager.java
+++ editor/plugins/org.fusesource.ide.camel.editor/src/org/fusesource/ide/camel/editor/provider/ActiveMQPaletteEntryDependenciesManager.java
@@ -10,16 +10,30 @@
  ******************************************************************************/ 
 package org.fusesource.ide.camel.editor.provider;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Plugin;
 import org.fusesource.ide.camel.editor.provider.ext.IDependenciesManager;
+import org.fusesource.ide.foundation.core.util.Strings;
 
 public class ActiveMQPaletteEntryDependenciesManager implements IDependenciesManager {
 
-	public static final String ACTIVEMQ_VERSION_FOR_CAMEL_2_17 = "5.11.0.redhat-630077";
-	public static final String ACTIVEMQ_VERSION_FOR_CAMEL_2_15 = "5.11.0.redhat-621084";
+	public static final String RED_HAT_SUFFIX = ".redhat-";
+	public static final Map<String, String> camelToAMQVersionMapping;
+	// TODO: change me after each release
+	public static final String LATEST_AMQ_VERSION = "5.11.0";
+	
+	static {
+		camelToAMQVersionMapping = new HashMap<>();
+		camelToAMQVersionMapping.put("2.15.1", "5.11.0");
+		camelToAMQVersionMapping.put("2.17.0", "5.11.0");
+		camelToAMQVersionMapping.put("2.17.3", "5.11.0");
+		// TODO: update me with every new release of camel and amq supported in tooling
+	}
+	
 	static final String ACTIVEMQ_CAMEL = "activemq-camel";
 	static final String ORG_APACHE_ACTIVEMQ = "org.apache.activemq";
 
@@ -47,12 +61,35 @@ public class ActiveMQPaletteEntryDependenciesManager implements IDependenciesMan
 	}
 
 	String getActiveMQVersion(String camelVersion) {
-		if(camelVersion.startsWith("2.15")){
-			return ACTIVEMQ_VERSION_FOR_CAMEL_2_15;
+		boolean productizedVersion = camelVersion.indexOf(RED_HAT_SUFFIX) != -1;		
+		String key = getVersionWithoutIdentifier(camelVersion);
+		String amqVersion = camelToAMQVersionMapping.get(key);
+		if(amqVersion != null) {
+			if (productizedVersion) {
+				return String.format("%s%s%s", amqVersion, RED_HAT_SUFFIX, getBuildNumberFromVersion(camelVersion));
+			} else {
+				return amqVersion;
+			}
 		} else {
 			//use latest version
-			return ACTIVEMQ_VERSION_FOR_CAMEL_2_17;
+			return LATEST_AMQ_VERSION;
 		}
 	}
 
+	String getBuildNumberFromVersion(String camelVersion) {
+		if (camelVersion.split("\\.").length>3) {
+			return camelVersion.substring(camelVersion.lastIndexOf('-')+1);
+		}
+		throw new IllegalArgumentException("Camel version " + camelVersion + " has no valid format for retrieving the build number.");
+	}
+	
+	String getVersionWithoutIdentifier(String camelVersion) {
+		if (!Strings.isEmpty(camelVersion)) {
+			String[] versionParts = camelVersion.split("\\.");
+			if (versionParts.length>2) {
+				return String.format("%s.%s.%s", versionParts[0], versionParts[1], versionParts[2]);
+			} 
+		}
+		throw new IllegalArgumentException("Given Camel Version " + camelVersion + " doesn't contain a valid value");
+	}
 }
