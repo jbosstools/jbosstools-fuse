08edd76559840542016688bf6deecf03a263e1cd
Aurelien Pupier <apupier@redhat.com>
2016-09-29 08:57:28 +0200

    FUSETOOLS-2131 - provide preferences to use staging repositories
    - one option to enable/disable
    - one option to customize the list of staging repositories
    - by default , it is disabled
    - enable it in tests for not yet publicly released version

---
+++ editor/plugins/org.fusesource.ide.projecttemplates/src/org/fusesource/ide/projecttemplates/preferences/initializer/StagingRepositoriesPreferenceInitializer.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.fusesource.ide.projecttemplates.preferences.initializer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.fusesource.ide.projecttemplates.internal.ProjectTemplatesActivator;
+
+public class StagingRepositoriesPreferenceInitializer extends AbstractPreferenceInitializer {
+	
+	private static final String NAME_URL_SEPARATOR = ",";
+	private static final String REPO_SEPARATOR = ";";
+	private static final String ENABLE_STAGING_REPOSITORIES = "enableStagingRepositories";
+	static final String STAGING_REPOSITORIES = "stagingRepositories";
+	private static final String PRODUCT_STAGING_REPO_URI = "fuse-internal"+NAME_URL_SEPARATOR+"http://download.eng.brq.redhat.com/brewroot/repos/jb-fuse-6.2-build/latest/maven";
+	private static final String THIRD_PARTY_STAGING_REPO_URI = "redhat-ea"+NAME_URL_SEPARATOR+"https://maven.repository.redhat.com/earlyaccess/all";
+
+	public StagingRepositoriesPreferenceInitializer() {
+		// Keep for reflection initialization
+	}
+
+	@Override
+	public void initializeDefaultPreferences() {
+		IPreferenceStore preferenceStore = getPreferenceStore();
+		preferenceStore.setDefault(ENABLE_STAGING_REPOSITORIES, false);
+		preferenceStore.setDefault(STAGING_REPOSITORIES, PRODUCT_STAGING_REPO_URI + REPO_SEPARATOR + THIRD_PARTY_STAGING_REPO_URI);
+	}
+
+	IPreferenceStore getPreferenceStore() {
+		return ProjectTemplatesActivator.getDefault().getPreferenceStore();
+	}
+	
+	public boolean isStagingRepositoriesEnabled(){
+		return getPreferenceStore().getBoolean(ENABLE_STAGING_REPOSITORIES);
+	}
+	
+	public void setStagingRepositoriesEnablement(boolean enable){
+		getPreferenceStore().setValue(ENABLE_STAGING_REPOSITORIES, enable);
+	}
+	
+	/**
+	 * @return A list of pair in List for Name/URl of repositories
+	 */
+	public List<List<String>> getStagingRepositories(){
+		String storedValue = getPreferenceStore().getString(STAGING_REPOSITORIES);
+		return Arrays.asList(storedValue.split(REPO_SEPARATOR))
+				.stream()
+				.map(repoName -> Arrays.asList(repoName.split(NAME_URL_SEPARATOR)))
+				.collect(Collectors.toList());
+	}
+
+}
