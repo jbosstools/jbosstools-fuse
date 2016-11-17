5071f26e7a90546f06bbf6fe131331dae4d9050a
Aurelien Pupier <apupier@redhat.com>
2016-11-17 14:29:19 +0100

    FUSETOOLS-2194 - Provide message warning when Camel version not detected

---
+++ editor/plugins/org.fusesource.ide.projecttemplates/src/org/fusesource/ide/projecttemplates/wizards/pages/RuntimeCamelVersionFinder.java
@@ -0,0 +1,93 @@
+/******************************************************************************* 
+ * Copyright (c) 2016 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.fusesource.ide.projecttemplates.wizards.pages;
+
+import java.io.File;
+
+import org.eclipse.wst.server.core.IRuntime;
+
+public class RuntimeCamelVersionFinder {
+	
+	static final String FUSE_RUNTIME_PREFIX = "org.fusesource.ide.fuseesb.runtime.";
+	static final String EAP_RUNTIME_PREFIX = "org.jboss.ide.eclipse.as.runtime.eap.";
+	private static final String CAMEL_CORE_LIB_PREFIX = "camel-core-";
+	private static final String CAMEL_CORE_LIB_SUFFIX = ".jar";
+
+	public String getVersion(IRuntime runtime) {
+		if (runtime != null) {
+			if (isJBossFuseRuntime(runtime)) {
+				return getVersionForJbossFuseRuntime(runtime);
+			} else if (isFuseOnEAPRuntime(runtime)) {
+				return getVersionForEAPRuntime(runtime);
+			}
+		}
+		return FuseIntegrationProjectWizardRuntimeAndCamelPage.UNKNOWN_CAMEL_VERSION;
+	}
+
+	private String getVersionForEAPRuntime(IRuntime runtime) {
+		File camelFolder = getCamelFolderOnEAP(runtime);
+		if(camelFolder.exists()){
+			String[] versions = camelFolder.list((dir, name) -> {
+				String lowerCaseName = name.toLowerCase();
+				return lowerCaseName.startsWith(CAMEL_CORE_LIB_PREFIX) && lowerCaseName.endsWith(CAMEL_CORE_LIB_SUFFIX);
+			});
+			if (versions.length==1) {
+				String jarName = versions[0];
+				return jarName.substring(jarName.indexOf(CAMEL_CORE_LIB_PREFIX)+CAMEL_CORE_LIB_PREFIX.length(), jarName.indexOf(CAMEL_CORE_LIB_SUFFIX));
+			}
+		}
+		return FuseIntegrationProjectWizardRuntimeAndCamelPage.UNKNOWN_CAMEL_VERSION;
+	}
+
+	private File getCamelFolderOnEAP(IRuntime runtime) {
+		return runtime.getLocation().append("modules").append("system").append("layers").append("fuse").append("org").append("apache").append("camel").append("core").append("main").toFile();
+	}
+
+	private String getVersionForJbossFuseRuntime(IRuntime runtime) {
+		File camelFolder = getCamelFolderOnFuse(runtime);
+		if(camelFolder.exists()){
+			String[] versions = camelFolder.list();
+			if (versions.length==1) {
+				return versions[0];
+			}
+		}
+		return FuseIntegrationProjectWizardRuntimeAndCamelPage.UNKNOWN_CAMEL_VERSION;
+	}
+
+	private File getCamelFolderOnFuse(IRuntime runtime) {
+		return runtime.getLocation().append("system").append("org").append("apache").append("camel").append("camel-core").toFile();
+	}
+	
+	/**
+	 * checks whether the given runtime is an EAP runtime
+	 * 
+	 * @param runtime	the runtime to check
+	 * @return	true if the runtime is an EAP runtime
+	 */
+	private boolean isFuseOnEAPRuntime(IRuntime runtime) {
+		return isRuntimeOfType(runtime, EAP_RUNTIME_PREFIX);
+	}
+	
+	/**
+	 * checks whether the given runtime is a fuse runtime
+	 * 
+	 * @param runtime	the runtime to check
+	 * @return	true if the runtime is a jboss fuse runtime
+	 */
+	private boolean isJBossFuseRuntime(IRuntime runtime) {
+		return isRuntimeOfType(runtime, FUSE_RUNTIME_PREFIX);
+	}
+	
+	private boolean isRuntimeOfType(IRuntime runtime, String runtimeType){
+		return runtime.getRuntimeType().getId().startsWith(runtimeType);
+	}
+
+}
---
+++ editor/tests/org.fusesource.ide.projecttemplates.tests.integration/src/main/java/org/fusesource/ide/projecttemplates/wizards/pages/FuseIntegrationProjectWizardRuntimeAndCamelPageIT.java
@@ -0,0 +1,90 @@
+/******************************************************************************* 
+ * Copyright (c) 2016 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.fusesource.ide.projecttemplates.wizards.pages;
+
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.wst.server.core.IRuntime;
+import org.fusesource.ide.projecttemplates.internal.Messages;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FuseIntegrationProjectWizardRuntimeAndCamelPageIT {
+	
+	@Mock
+	FuseIntegrationProjectWizardRuntimeAndCamelPage page;
+	@Mock(answer = Answers.RETURNS_DEEP_STUBS)
+	private IRuntime runtime;
+	@Mock
+	private StyledText camelInfoText;
+	@Rule
+	public  TemporaryFolder tmpFolder = new TemporaryFolder();
+	private File fakeEAPCamelFolder;
+	
+	@Before
+	public void setup() throws IOException{
+		fakeEAPCamelFolder = tmpFolder.newFolder("fakeEAPCamelFolder");
+		new File(fakeEAPCamelFolder, "camel-core-2.17.3.jar").createNewFile();
+		doCallRealMethod().when(page).validate();
+		doCallRealMethod().when(page).determineRuntimeCamelVersion(runtime);
+		doCallRealMethod().when(page).setCamelInfoText(Mockito.any());
+		doReturn("2.17.3").when(page).getSelectedCamelVersion();
+	}
+	
+	@Test
+	public void testValidationMessageForRuntimeWithoutCamel() throws Exception {
+		doReturn(runtime).when(page).getSelectedRuntime();
+		when(runtime.getRuntimeType().getId()).thenReturn(RuntimeCamelVersionFinder.FUSE_RUNTIME_PREFIX);
+		page.setCamelInfoText(camelInfoText);
+		
+		page.validate();
+		
+		verify(camelInfoText).setText(Messages.FuseIntegrationProjectWizardRuntimeAndCamelPage_WarningMessageWhenCamelVersionCannotBeDeterminedInRuntime);
+	}
+	
+	@Test
+	public void testValidationMessageClearedWhenNoRuntimeCalled() throws Exception {
+		doReturn(null).when(page).getSelectedRuntime();
+		page.setCamelInfoText(camelInfoText);
+		
+		page.validate();
+		
+		verify(camelInfoText).setText("");
+	}
+	
+	@Test
+	public void testValidationMessageClearedWhenRuntimeWithCorrectCamel() throws Exception {
+		doReturn(runtime).when(page).getSelectedRuntime();
+		when(runtime.getRuntimeType().getId()).thenReturn(RuntimeCamelVersionFinder.EAP_RUNTIME_PREFIX);
+		when(runtime.getLocation().append("modules").append("system").append("layers").append("fuse").append("org").append("apache").append("camel").append("core").append("main").toFile()).thenReturn(fakeEAPCamelFolder);
+		page.setCamelInfoText(camelInfoText);
+		
+		page.validate();
+		
+		verify(camelInfoText).setText("");
+	}
+
+}
