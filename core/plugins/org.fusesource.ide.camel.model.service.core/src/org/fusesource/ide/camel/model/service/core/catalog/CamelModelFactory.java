62968bfb885e54a207e564d58d2fce944c641a67
Aurelien Pupier <apupier@redhat.com>
2017-01-11 13:29:26 +0100

    FUSETOOLS-2227 - Add support for FUSE 6.3.0.R1
    - corresponds to Camel 2.17.0.630224

--- core/plugins/org.fusesource.ide.camel.model.service.core/src/org/fusesource/ide/camel/model/service/core/catalog/CamelModelFactory.java
+++ core/plugins/org.fusesource.ide.camel.model.service.core/src/org/fusesource/ide/camel/model/service/core/catalog/CamelModelFactory.java
@@ -50,12 +50,13 @@ public class CamelModelFactory {
 		camelVersionToFuseBOMMapping.put("2.15.1.redhat-621084", "6.2.1.redhat-084");
 		camelVersionToFuseBOMMapping.put("2.15.1.redhat-621117", "6.2.1.redhat-117");
 		camelVersionToFuseBOMMapping.put("2.17.0.redhat-630187", "6.3.0.redhat-187");
-		camelVersionToFuseBOMMapping.put("2.17.3",               "6.3.0.redhat-187");
+		camelVersionToFuseBOMMapping.put("2.17.0.redhat-630224", "6.3.0.redhat-224");
+		camelVersionToFuseBOMMapping.put("2.17.3",               "6.3.0.redhat-224");
 	}
-	private static final String LATEST_BOM_VERSION = "6.3.0.redhat-187";
 	
 	private static final Set<String> pureFisVersions = Stream.of("2.18.1.redhat-000007").collect(Collectors.toSet());
 	
+	private static final String LATEST_BOM_VERSION = "6.3.0.redhat-224";
 	
 	private static HashMap<String, Map<String, CamelModel>> supportedCamelModels;
 	
