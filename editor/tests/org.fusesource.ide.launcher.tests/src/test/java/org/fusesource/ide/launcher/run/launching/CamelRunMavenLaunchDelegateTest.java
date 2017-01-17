e16f713bd5cb3976323d9d87c515449dab7712b1
Aurelien Pupier <apupier@redhat.com>
2017-01-17 17:31:27 +0100

    FUSETOOLS-2234 - add unit test for springboot goals

--- editor/tests/org.fusesource.ide.launcher.tests/src/test/java/org/fusesource/ide/launcher/run/launching/CamelRunMavenLaunchDelegateTest.java
+++ editor/tests/org.fusesource.ide.launcher.tests/src/test/java/org/fusesource/ide/launcher/run/launching/CamelRunMavenLaunchDelegateTest.java
@@ -10,10 +10,14 @@
  ******************************************************************************/
 package org.fusesource.ide.launcher.run.launching;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.fusesource.ide.launcher.run.util.CamelContextLaunchConfigConstants;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -26,17 +30,29 @@ public class CamelRunMavenLaunchDelegateTest {
 	@Mock
 	private ILaunchConfiguration launchConfig;
 
-	private CamelRunMavenLaunchDelegate camelRunMavenLaunchDelegate = Mockito.spy(new CamelRunMavenLaunchDelegate());
+	private CamelRunMavenLaunchDelegate camelRunMavenLaunchDelegate = spy(new CamelRunMavenLaunchDelegate());
+	
+	@Before
+	public void setup() throws Exception {
+		doReturn("file:C:\\my%20path%20with%20space").when(launchConfig).getAttribute(CamelContextLaunchConfigConstants.ATTR_FILE, (String) null);
+		doReturn(null).when(camelRunMavenLaunchDelegate).getFileInWorkspace(Mockito.anyString());
+		doReturn(false).when(camelRunMavenLaunchDelegate).isWarPackaging(Mockito.any(IFile.class));
+	}
 
 	@Test
 	public void testGetGoals() throws Exception {
-		Mockito.doReturn("file:C:\\my%20path%20with%20space").when(launchConfig).getAttribute(CamelContextLaunchConfigConstants.ATTR_FILE, (String) null);
-		Mockito.doReturn(null).when(camelRunMavenLaunchDelegate).getFileInWorkspace(Mockito.anyString());
-		Mockito.doReturn(false).when(camelRunMavenLaunchDelegate).isWarPackaging(Mockito.any(IFile.class));
-		Mockito.doReturn(false).when(camelRunMavenLaunchDelegate).isSpringBoot(Mockito.any(IFile.class));
+		doReturn(false).when(camelRunMavenLaunchDelegate).isSpringBoot(Mockito.any(IFile.class));
 		
-		Assertions.assertThat(camelRunMavenLaunchDelegate.getGoals(launchConfig))
+		assertThat(camelRunMavenLaunchDelegate.getGoals(launchConfig))
 				.isEqualTo("clean package org.apache.camel:camel-maven-plugin:run -Dcamel.fileApplicationContextUri=\"file:C:\\my path with space\"");
+	}
+	
+	@Test
+	public void testGetGoalsForSpringBoot() throws Exception {
+		doReturn(true).when(camelRunMavenLaunchDelegate).isSpringBoot(Mockito.any(IFile.class));
+		
+		assertThat(camelRunMavenLaunchDelegate.getGoals(launchConfig))
+				.isEqualTo("clean package org.springframework.boot:spring-boot-maven-plugin:run -Dcamel.fileApplicationContextUri=\"file:C:\\my path with space\"");
 	}
 
 }
