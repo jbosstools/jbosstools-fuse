d0a5c0a6dd8ab7f8a081839d94b32b61214c296c
Aurelien Pupier <apupier@redhat.com>
2017-03-28 11:30:38 +0200

    use StringBuilder to concatenate String in Loop

--- tools/ide-catalog-generator/src/main/java/org/fusesource/ide/catalog/generator/model/component/UriParam.java
+++ tools/ide-catalog-generator/src/main/java/org/fusesource/ide/catalog/generator/model/component/UriParam.java
@@ -243,13 +243,17 @@ public class UriParam {
 	}
 	
 	public String getChoiceString() {
-		if (this.choice == null || this.choice.length<1) return null;
-		String retVal = "";
-		for (String c : this.choice) {
-			if (retVal.length()>0) retVal += ","; 
-			retVal += c;
+		if (this.choice == null || this.choice.length<1) {
+			return null;
 		}
-		return retVal;
+		StringBuilder retVal = new StringBuilder();
+		for (String c : this.choice) {
+			if (retVal.length()>0) {
+				retVal.append(","); 
+			}
+			retVal.append(c);
+		}
+		return retVal.toString();
 	}
 
 	public String getSecret() {
