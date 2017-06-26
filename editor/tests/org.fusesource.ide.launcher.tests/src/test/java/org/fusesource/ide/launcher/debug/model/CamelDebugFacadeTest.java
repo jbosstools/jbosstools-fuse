1fcddc55e5af8f8841df8a358968e69c8329dc4d
Aurélien Pupier <apupier@redhat.com>
2017-06-26 10:58:34 +0200

    FUSETOOLS-2430 - Support "Edit Routes" in JMX view for multi context
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

---
+++ editor/tests/org.fusesource.ide.launcher.tests/src/test/java/org/fusesource/ide/launcher/debug/model/CamelDebugFacadeTest.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.fusesource.ide.launcher.debug.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anySetOf;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doCallRealMethod;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.management.ObjectInstance;
+import javax.management.ObjectName;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CamelDebugFacadeTest {
+	
+	@Mock
+	private CamelDebugFacade cdf;
+	
+	@Before
+	public void setup() {
+		doCallRealMethod().when(cdf).findDebugger(anyString(), anySetOf(ObjectInstance.class));
+	}
+
+	@Test
+	public void testRetrieveDebuggerWith2CamelContexts() throws Exception {
+		Set<ObjectInstance> mbeans = new HashSet<>();
+		ObjectInstance o1 = new ObjectInstance("org.apache.camel:context=cbr-example-context,type=tracer,name=BacklogDebugger", null);
+		mbeans.add(o1);
+		ObjectInstance o2 = new ObjectInstance("org.apache.camel:context=cbr-example-context-2,type=tracer,name=BacklogDebugger", null);
+		mbeans.add(o2);
+		ObjectName found = cdf.findDebugger("cbr-example-context-2", mbeans);
+		assertThat(found.getKeyProperty(CamelDebugFacade.KEY_PROPERTY_CONTEXT_FOR_DEBUGGER_MBEAN)).isEqualTo("cbr-example-context-2");
+	}
+	
+	@Test
+	public void testRetrieveDebuggerWith2CamelContextsFromKaraf() throws Exception {
+		Set<ObjectInstance> mbeans = new HashSet<>();
+		ObjectInstance o1 = new ObjectInstance("org.apache.camel:context=com.mycompany.camel-cbr-example-context,type=tracer,name=BacklogDebugger", null);
+		mbeans.add(o1);
+		ObjectInstance o2 = new ObjectInstance("org.apache.camel:context=com.mycompany.camel-cbr-example-context-2,type=tracer,name=BacklogDebugger", null);
+		mbeans.add(o2);
+		ObjectName found = cdf.findDebugger("cbr-example-context-2", mbeans);
+		assertThat(found.getKeyProperty(CamelDebugFacade.KEY_PROPERTY_CONTEXT_FOR_DEBUGGER_MBEAN)).contains("cbr-example-context-2");
+	}
+	
+}
