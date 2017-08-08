de0f6c9ce97216929dc197cdbd35ceed84b113fc
Aurélien Pupier <apupier@redhat.com>
2017-08-08 09:21:57 +0200

    FUSETOOLS-2299 - Fix model for choice/enum parameters
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

---
+++ core/tests/org.fusesource.ide.camel.model.service.core.tests/src/test/java/org/fusesource/ide/camel/model/service/core/catalog/eips/EipParameterTest.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.fusesource.ide.camel.model.service.core.catalog.eips;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.fusesource.ide.camel.model.service.core.catalog.Parameter;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EipParameterTest {
+
+	private Eip eip;
+	
+	@Before
+	public void onStart() throws IOException {
+		File jsonFile = new File("testdata/to.json");
+		this.eip = Eip.getJSONFactoryInstance(new FileInputStream(jsonFile));
+	}
+	
+	
+	@Test
+	public void testGetParameterWithEnum() throws Exception {
+		Parameter patternParam = eip.getParameter("pattern");
+		assertThat(patternParam.getChoice()).contains("InOnly", "InOptionalOut", "InOut", "OutIn", "OutOnly", "OutOptionalIn", "RobustInOnly", "RobustOutOnly");
+	}
+	
+}
