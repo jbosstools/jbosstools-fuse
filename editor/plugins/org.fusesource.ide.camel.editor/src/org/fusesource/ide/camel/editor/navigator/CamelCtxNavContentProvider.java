17eda2e626e5909106ff40236c43676a78a46b76
Aurélien Pupier <apupier@redhat.com>
2017-08-09 11:52:59 +0200

    FUSETOOLS-2363 - avoid duplicated entries for routes in Common navigator
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- editor/plugins/org.fusesource.ide.camel.editor/src/org/fusesource/ide/camel/editor/navigator/CamelCtxNavContentProvider.java
+++ editor/plugins/org.fusesource.ide.camel.editor/src/org/fusesource/ide/camel/editor/navigator/CamelCtxNavContentProvider.java
@@ -10,25 +10,21 @@
  ******************************************************************************/
 package org.fusesource.ide.camel.editor.navigator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.jface.viewers.AbstractTreeViewer;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IMemento;
 import org.eclipse.ui.navigator.ICommonContentExtensionSite;
 import org.eclipse.ui.navigator.ICommonContentProvider;
+import org.fusesource.ide.camel.editor.internal.UIMessages;
 import org.fusesource.ide.camel.model.service.core.io.CamelIOHandler;
-import org.fusesource.ide.camel.model.service.core.model.AbstractCamelModelElement;
 import org.fusesource.ide.camel.model.service.core.model.CamelFile;
 import org.fusesource.ide.camel.model.service.core.model.CamelRouteElement;
 import org.fusesource.ide.foundation.ui.util.Widgets;
@@ -39,113 +35,37 @@ import org.fusesource.ide.foundation.ui.util.Widgets;
 public class CamelCtxNavContentProvider implements ICommonContentProvider {
 	
 	private AbstractTreeViewer mViewer;
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getChildren(java.lang.Object)
-	 */
+	private Job job;
+	public static final Object JOB_FAMILY = new Object();
+	
 	@Override
 	public Object[] getChildren(Object parent) { 
 		if(parent instanceof IFile){	
-			return getRoutes((IFile)parent,true);
-		} 
-//		else if(parent instanceof CamelCtxNavRouteNode){
-//			List<CamelModelElement> rootNodes = ((CamelCtxNavRouteNode)parent).getCamelRoute().getChildElements();
-//			//show root nodes to differentiate routes with no id
-//			return rootNodes.toArray(new CamelModelElement[rootNodes.size()]); 
-//		} 
+			return getRoutes((IFile)parent);
+		}
 		return new Object[0];
 	}
 	
-	private Object[] getRoutes(IFile camelFile,boolean deferred){
-		if(deferred){
-			//deferred since load time for context files is unknown
-			LoadingPlaceHolder placeHolder = new LoadingPlaceHolder();
-			doDeferredLoad(camelFile, placeHolder);
-			return new Object[] { placeHolder};
-		} else {
-			return getRoutes(camelFile);
-		}		
-	}
-	
-	private CamelCtxNavRouteNode[] getRoutes(IFile camelFile) {
-		CamelIOHandler ioHandler = new CamelIOHandler();
-		final CamelFile rc = ioHandler.loadCamelModel(camelFile, new NullProgressMonitor());
-		if (rc != null && rc.getRouteContainer() != null) {
-			List<CamelCtxNavRouteNode> routes = new ArrayList<CamelCtxNavRouteNode>();
-			for(AbstractCamelModelElement node : rc.getRouteContainer().getChildElements()) {
-				if(node instanceof CamelRouteElement) {
-					routes.add(new CamelCtxNavRouteNode((CamelRouteElement)node, camelFile));
-				}
-			}
-			return routes.toArray(new CamelCtxNavRouteNode[routes.size()]);
-		}
-		return new CamelCtxNavRouteNode[0];
+	private Object[] getRoutes(IFile camelFile){
+		//deferred since load time for context files is unknown
+		LoadingPlaceHolder placeHolder = new LoadingPlaceHolder();
+		doDeferredLoad(camelFile, placeHolder);
+		return new Object[] { placeHolder};
 	}
 	
 	private void doDeferredLoad(final IFile camelFile,final LoadingPlaceHolder placeHolder){
 		//instead of DeferredTreeContentManager
-		Job job = new Job("Loading " + camelFile.getName()) {
-			/*
-			 * (non-Javadoc)
-			 * @see org.eclipse.core.runtime.jobs.Job#run(org.eclipse.core.runtime.IProgressMonitor)
-			 */
-			@Override
-			protected IStatus run(IProgressMonitor monitor) {
-				final CamelCtxNavRouteNode[] routes = getRoutes(camelFile);
-				if (routes!=null) {
-					Display.getDefault().asyncExec(new Runnable() {
-						/*
-						 * (non-Javadoc)
-						 * @see java.lang.Runnable#run()
-						 */
-						@Override
-						public void run() {
-							if (!Widgets.isDisposed(mViewer)) {
-								mViewer.add(camelFile,routes);
-							}
-						}
-					});
-				}
-				return Status.OK_STATUS;
-			}
-		};
-		
-		job.addJobChangeListener(new JobChangeAdapter() {
-	        /*
-	         * (non-Javadoc)
-	         * @see org.eclipse.core.runtime.jobs.JobChangeAdapter#done(org.eclipse.core.runtime.jobs.IJobChangeEvent)
-	         */
-			@Override
-			public void done(IJobChangeEvent event) {
-	        	Display.getDefault().asyncExec(new Runnable() {
-					@Override
-					public void run() {
-						if (!Widgets.isDisposed(mViewer)) {
-							mViewer.remove(camelFile, new Object[] { placeHolder});
-						}
-					}
-				});
-	        }
-	     });
+		job = new LoadingCamelRoutesForNavigatorViewerJob(NLS.bind(UIMessages.loadingCamelFile, camelFile.getName(), camelFile.getProject().getName()), camelFile, placeHolder);
 		
 		job.schedule();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
-	 */
 	@Override
 	public boolean hasChildren(Object element) {
 		return element instanceof IFile ||
 			   (element instanceof CamelCtxNavRouteNode && getChildren(element).length>0);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
-	 */
 	@Override
 	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
 		if (viewer instanceof AbstractTreeViewer) {
@@ -153,69 +73,92 @@ public class CamelCtxNavContentProvider implements ICommonContentProvider {
 		}
 	}
 	
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
-	 */
 	@Override
 	public void dispose() {
+		if(mViewer != null && mViewer.getControl() != null) {
+			mViewer.getControl().dispose();
+		}
 		mViewer = null;
+		if(job != null){
+			job.cancel();
+		}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getElements(java.lang.Object)
-	 */
 	@Override
 	public Object[] getElements(Object inputElement) {
 		return getChildren(inputElement);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
-	 */
 	@Override
 	public Object getParent(Object element) {
 		return null;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.navigator.IMementoAware#restoreState(org.eclipse.ui.IMemento)
-	 */
 	@Override
-	public void restoreState(IMemento aMemento) {
+	public void restoreState(IMemento aMemento) { /*Not supported*/	}
+	@Override
+	public void saveState(IMemento aMemento)  { /*Not supported*/ }
+	@Override
+	public void init(ICommonContentExtensionSite aConfig) { /*Not supported*/ }
+	
+	private final class LoadingCamelRoutesForNavigatorViewerJob extends Job {
+		private final IFile camelFile;
+		private LoadingPlaceHolder placeHolder;
+
+		private LoadingCamelRoutesForNavigatorViewerJob(String name, IFile camelFile, LoadingPlaceHolder placeHolder) {
+			super(name);
+			this.camelFile = camelFile;
+			this.placeHolder = placeHolder;
+		}
+
+		@Override
+		protected IStatus run(IProgressMonitor monitor) {
+			final CamelCtxNavRouteNode[] routes = getRoutes(camelFile, monitor);
+			if (routes!=null) {
+				Display.getDefault().asyncExec(() -> {
+					if (!Widgets.isDisposed(mViewer)) {
+						clearPreviouslyComputedData();
+						mViewer.add(camelFile, routes);
+						mViewer.getControl().setData(camelFile.getLocationURI().toString(), routes);
+					}
+				});
+			}
+			return Status.OK_STATUS;
+		}
+
+		protected void clearPreviouslyComputedData() {
+			Object[] storedData = (Object[])mViewer.getControl().getData(camelFile.getLocationURI().toString());
+			if(storedData != null) {
+				mViewer.remove(camelFile, storedData);
+			}
+			mViewer.remove(camelFile, new Object[] { placeHolder});
+		}
+		
+		private CamelCtxNavRouteNode[] getRoutes(IFile camelFile, IProgressMonitor monitor) {
+			SubMonitor subMonitor = SubMonitor.convert(monitor, 1);
+			CamelIOHandler ioHandler = new CamelIOHandler();
+			final CamelFile rc = ioHandler.loadCamelModel(camelFile, subMonitor.newChild(1));
+			if (rc != null && rc.getRouteContainer() != null) {
+				return rc.getRouteContainer().getChildElements()
+						.stream()
+						.filter(node -> node instanceof CamelRouteElement)
+						.map(route -> new CamelCtxNavRouteNode((CamelRouteElement)route, camelFile))
+						.toArray(CamelCtxNavRouteNode[]::new);
+			}
+			return new CamelCtxNavRouteNode[0];
+		}
+		
+		@Override
+		public boolean belongsTo(Object family) {
+			return JOB_FAMILY.equals(family) || super.belongsTo(family);
+		}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.navigator.IMementoAware#saveState(org.eclipse.ui.IMemento)
-	 */
-	@Override
-	public void saveState(IMemento aMemento) {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.navigator.ICommonContentProvider#init(org.eclipse.ui.navigator.ICommonContentExtensionSite)
-	 */
-	@Override
-	public void init(ICommonContentExtensionSite aConfig) {
-	}
-	
-	
 	private static class LoadingPlaceHolder {
 		
-		private final String loadingMsg = "Pending ...";
-		
-		/*
-		 * (non-Javadoc)
-		 * @see java.lang.Object#toString()
-		 */
 		@Override
 		public String toString() {
-			return loadingMsg;
+			return UIMessages.pending;
 		}		
 	}
 }
---
+++ editor/tests/org.fusesource.ide.camel.editor.tests.integration/src/main/java/org/fusesource/ide/camel/editor/navigator/CamelCtxNavContentProviderIT.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.fusesource.ide.camel.editor.navigator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.ui.PlatformUI;
+import org.fusesource.ide.camel.model.service.core.model.CamelFile;
+import org.fusesource.ide.camel.model.service.core.tests.integration.core.io.FuseProject;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class CamelCtxNavContentProviderIT {
+
+	@Rule
+	public FuseProject fuseProject = new FuseProject(getClass().getSimpleName());
+	
+	@Test
+	public void testRouteLoadedWithOneRoute() throws Exception {
+		Display display = PlatformUI.getWorkbench().getDisplay();
+		TreeViewer treeViewer = new TreeViewer(display.getActiveShell());
+		treeViewer.setContentProvider(new CamelCtxNavContentProvider());
+		treeViewer.setLabelProvider(new CamelCtxNavLabelProvider());
+		CamelFile camelFileWithOneRoute = fuseProject.createEmptyCamelFile();
+		treeViewer.setInput(camelFileWithOneRoute.getResource());
+		treeViewer.refresh();
+		Job.getJobManager().join(CamelCtxNavContentProvider.JOB_FAMILY, new NullProgressMonitor());
+		while(display.readAndDispatch()) { }
+		TreeItem[] items = treeViewer.getTree().getItems();
+		assertThat(items).hasSize(1);
+		assertThat(items[0].getText()).isEqualTo("Route route3");
+	}
+	
+}
