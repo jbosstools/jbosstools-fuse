19e4838a135c9f68b68d24da3b613e7cb2901907
Brian Fitzpatrick <bfitzpat@redhat.com>
2017-10-04 13:46:50 -0600

    FUSESTOOLS-2564 - Initial stab at wrapping wsdl2rest code
    * Initial Wizard contribution
    * Next round of updates.
    * Can successfully run the wsdl2rest code from the wizard and tests now
    * Added ability to provide more detail via the wizard page
    * A few more updates
    * Refactoring one-page into two-page wizard with validation
    * Cleanup and externalization of strings
    * Correcting i10n issues
    * Updates to make Eclipse paths work with java.io paths and utility
    * Adding auto-config of paths based on project selection
    * Code cleanup and documentation pass.
    * Adjustments based on suggestions from @apupier
    * Pulling in the wsdl2rest code from maven instead of duplicating code
    * More cleanup of names and versions
    * Beginning of support for reading service address from wsdl
    * Added code to grab the target address URL from the selected WSDL
    * Adding wizard icon (using camel project one for now)
    * Implementing suggestions from Aurelien
    * Updates for latest wsdl2rest changes upstream
    * Adds required Rest library to dependencies and refreshes project
    * Updates to readme, new externalized string, and copyright dates
    * Creates the java path for source if it doesn't exist
    * Updates for blueprint support and absolute paths
    * Addition of gitignore
    * Addressing first wave of feedback from Aurelien
    
    Signed-off-by: Brian Fitzpatrick <bfitzpat@redhat.com>

---
+++ editor/plugins/org.fusesource.ide.wsdl2rest.ui/src/org/fusesource/ide/wsdl2rest/ui/wizard/pages/PathValidator.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.fusesource.ide.wsdl2rest.ui.wizard.pages;
+
+import org.eclipse.core.databinding.validation.IValidator;
+import org.eclipse.core.databinding.validation.ValidationStatus;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.fusesource.ide.foundation.core.util.Strings;
+import org.fusesource.ide.wsdl2rest.ui.internal.UIMessages;
+
+/**
+ * Validates that a given path exists in the selected project.
+ * @author brianf
+ */
+public class PathValidator implements IValidator {
+
+	/**
+	 * Verify that the path exists in the selected project.
+	 * @param path
+	 * @return
+	 */
+	private boolean isPathAccessible(String path) {
+		Path testPath = new Path(path);
+		IResource container = 
+				ResourcesPlugin.getWorkspace().getRoot().findMember(testPath);
+		while (container == null && !testPath.isEmpty()) {
+			testPath = (Path) testPath.removeLastSegments(1);
+			container = ResourcesPlugin.getWorkspace().getRoot().findMember(testPath);
+			if (container != null && container.exists()) {
+				break;
+			}
+		}
+		return container != null && container.exists();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.databinding.validation.IValidator#validate(java.lang.Object)
+	 */
+	@Override
+	public IStatus validate(Object value) {
+		if (value instanceof String) {
+			String stringValue = (String)value;
+			if(stringValue.isEmpty()){
+				return ValidationStatus.error(UIMessages.wsdl2RestWizardSecondPageValidatorPathRequired);
+			}
+			if (!isPathAccessible(stringValue)) {
+				return ValidationStatus.error(UIMessages.wsdl2RestWizardSecondPageValidatorPathMustBeAccessible);
+			}
+		}
+		return ValidationStatus.ok();   		
+	}
+}
---
+++ editor/plugins/org.fusesource.ide.wsdl2rest.ui/src/org/fusesource/ide/wsdl2rest/ui/wizard/pages/Wsdl2RestWizardSecondPage.java
@@ -0,0 +1,123 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.fusesource.ide.wsdl2rest.ui.wizard.pages;
+
+import org.eclipse.core.databinding.Binding;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jface.databinding.fieldassist.ControlDecorationSupport;
+import org.eclipse.jface.databinding.wizard.WizardPageSupport;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Text;
+import org.fusesource.ide.wsdl2rest.ui.internal.UIMessages;
+
+/**
+ * Second page of the wsdl2rest wizard that collects secondary details.
+ * @author brianf
+ */
+public class Wsdl2RestWizardSecondPage extends Wsdl2RestWizardBasePage {
+
+	/**
+	 * Constructor
+	 * @param pageName
+	 */
+	public Wsdl2RestWizardSecondPage(String pageName) {
+		this(pageName, null, null);
+	}
+
+	/**
+	 * Constructor
+	 * @param pageName
+	 * @param title
+	 * @param titleImage
+	 */
+	public Wsdl2RestWizardSecondPage(String pageName, String title, ImageDescriptor titleImage) {
+		super(pageName, title, titleImage);
+		setMessage(UIMessages.wsdl2RestWizardSecondPagePageTwoDescription);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	public void createControl(Composite parent) {
+		WizardPageSupport.create(this, dbc);
+		setDescription(UIMessages.wsdl2RestWizardSecondPagePageTwoDescription);
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(GridLayoutFactory.swtDefaults().numColumns(4).create());
+		composite.setLayoutData(GridDataFactory.fillDefaults().grab(false, false).create());
+
+		Text javaPathTextControl = createLabelAndText(composite, UIMessages.wsdl2RestWizardSecondPageJavaFolderLabel, 2);
+		Button javaPathBrowseBtn = createButton(composite, "..."); //$NON-NLS-1$
+		javaPathBrowseBtn.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// browse
+				IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(getOptionsFromWizard().getProjectName());
+				String path = selectFolder(project);
+				if (path != null) {
+					getOptionsFromWizard().setDestinationJava(path);
+					javaPathTextControl.notifyListeners(SWT.Modify, new Event());
+				}
+			}
+		});
+
+		Text camelPathTextControl = createLabelAndText(composite, UIMessages.wsdl2RestWizardSecondPageCamelFolderLabel, 2);
+		Button outPathBrowseButton = createButton(composite, "..."); //$NON-NLS-1$
+		outPathBrowseButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// browse
+				IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(getOptionsFromWizard().getProjectName());
+				String path = selectFolder(project);
+				if (path != null) {
+					getOptionsFromWizard().setDestinationCamel(path);
+					camelPathTextControl.notifyListeners(SWT.Modify, new Event());
+				}
+			}
+		});
+
+		Text targetAddressText = createLabelAndText(composite, UIMessages.wsdl2RestWizardSecondPageTargetServiceAddressLabel, 3);
+		Text targetRestAddressText = createLabelAndText(composite, UIMessages.wsdl2RestWizardSecondPageTargetRESTServiceAddressLabel, 3);
+
+		// define the data bindings
+		Binding javaPathBinding = createBinding(javaPathTextControl, "destinationJava", new PathValidator()); //$NON-NLS-1$
+		ControlDecorationSupport.create(javaPathBinding, SWT.LEFT | SWT.TOP);
+
+		Binding camelPathBinding = createBinding(camelPathTextControl, "destinationCamel", new PathValidator()); //$NON-NLS-1$
+		ControlDecorationSupport.create(camelPathBinding, SWT.LEFT | SWT.TOP);
+
+		Binding targetAddressBinding = createBinding(targetAddressText, "targetServiceAddress", new TargetURLValidator()); //$NON-NLS-1$
+		ControlDecorationSupport.create(targetAddressBinding, SWT.LEFT | SWT.TOP);
+
+		Binding targetRestAddressBinding = createBinding(targetRestAddressText, "targetRestServiceAddress", new TargetURLValidator()); //$NON-NLS-1$
+		ControlDecorationSupport.create(targetRestAddressBinding, SWT.LEFT | SWT.TOP);
+
+		// set initial values
+		initIfNotEmpty(javaPathTextControl, getOptionsFromWizard().getDestinationJava());
+		initIfNotEmpty(camelPathTextControl, getOptionsFromWizard().getDestinationCamel());
+		initIfNotEmpty(targetAddressText, getOptionsFromWizard().getTargetServiceAddress());
+		initIfNotEmpty(targetRestAddressText, getOptionsFromWizard().getTargetRestServiceAddress());
+
+		setControl(composite);
+		setPageComplete(isPageComplete());
+		setErrorMessage(null); // clear any error messages at first
+	}
+}
