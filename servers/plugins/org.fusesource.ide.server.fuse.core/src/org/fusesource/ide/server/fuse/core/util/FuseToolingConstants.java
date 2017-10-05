9cde0e45dd486728ff895bc57c1610761c5b4181
Aurélien Pupier <apupier@redhat.com>
2017-10-05 17:13:34 +0200

    FUSETOOLS-2531 - Avoid public static final for mutable objects
    - use immutable
    - remove unused constants
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- servers/plugins/org.fusesource.ide.server.fuse.core/src/org/fusesource/ide/server/fuse/core/util/FuseToolingConstants.java
+++ servers/plugins/org.fusesource.ide.server.fuse.core/src/org/fusesource/ide/server/fuse/core/util/FuseToolingConstants.java
@@ -10,6 +10,10 @@
  ******************************************************************************/
 package org.fusesource.ide.server.fuse.core.util;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Keep track of the server and runtime id's for the fuse servers
  */
@@ -24,10 +28,6 @@ public class FuseToolingConstants {
 	
     public static final String RUNTIME_FUSE_70 = "org.fusesource.ide.fuseesb.runtime.70";
     
-	public static final String[] ALL_FUSE_RUNTIME_TYPES = new String[]{
-		RUNTIME_FUSE_60, RUNTIME_FUSE_61, RUNTIME_FUSE_62, RUNTIME_FUSE_63, RUNTIME_FUSE_70
-	};	
-
 	public static final String SERVER_FUSE_60 = "org.fusesource.ide.fuseesb.server.60";
 	public static final String SERVER_FUSE_61 = "org.fusesource.ide.fuseesb.server.61";
     public static final String SERVER_FUSE_62 = "org.fusesource.ide.fuseesb.server.62";
@@ -35,9 +35,9 @@ public class FuseToolingConstants {
 	
     public static final String SERVER_FUSE_70 = "org.fusesource.ide.fuseesb.server.70";
     
-	public static final String[] ALL_FUSE_SERVER_TYPES = new String[]{
+	public static final List<String> ALL_FUSE_SERVER_TYPES = Collections.unmodifiableList(Arrays.asList(
 		SERVER_FUSE_60, SERVER_FUSE_61, SERVER_FUSE_62, SERVER_FUSE_63, SERVER_FUSE_70
-	};
+	));
 	
 	private FuseToolingConstants() {
 		// util class
