3178108b044b5943b3c305601fa0e689f63b4c03
Dominik Jelinek <djelinek@redhat.com>
2017-11-10 12:23:46 +0100

    FUSETOOLS-2613: Create structure for using different versions of Camel catalog in tests
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

---
+++ uitests/plugins/org.jboss.tools.fuse.reddeer/src/org/jboss/tools/fuse/reddeer/requirement/CamelCatalogConfiguration.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.fuse.reddeer.requirement;
+
+import org.eclipse.reddeer.junit.requirement.configuration.RequirementConfiguration;
+
+/**
+ * @author djelinek
+ */
+public class CamelCatalogConfiguration implements RequirementConfiguration {
+
+	private String name;
+	private String version;
+	private String home;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getHome() {
+		return home;
+	}
+
+	public void setHome(String root) {
+		this.home = root;
+	}
+
+	@Override
+	public String getId() {
+		return name;
+	}
+
+}
---
+++ uitests/plugins/org.jboss.tools.fuse.reddeer/src/org/jboss/tools/fuse/reddeer/view/PaletteViewExt.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.fuse.reddeer.view;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.eclipse.gef.GraphicalViewer;
+import org.eclipse.gef.palette.PaletteEntry;
+import org.eclipse.gef.ui.palette.PaletteViewer;
+import org.eclipse.reddeer.common.util.Display;
+import org.eclipse.reddeer.common.util.ResultRunnable;
+import org.eclipse.reddeer.gef.impl.palette.AbstractPalette;
+import org.eclipse.reddeer.gef.matcher.IsToolEntryWithParent;
+import org.eclipse.reddeer.workbench.core.lookup.EditorPartLookup;
+
+/**
+ * Extends RedDeer palette view implementation - allows getting tools only from specific palette view group
+ * 
+ * @author djelinek
+ */
+public class PaletteViewExt extends AbstractPalette {
+
+	public PaletteViewExt() {
+		super(Display.syncExec(new ResultRunnable<PaletteViewer>() {
+
+			@Override
+			public PaletteViewer run() {
+				return getGraphicalViewer().getEditDomain().getPaletteViewer();
+			}
+		}));
+	}
+
+	public List<String> getGroupTools(String group) {
+		List<PaletteEntry> entries = paletteHandler.getPaletteEntries(paletteViewer, new IsToolEntryWithParent(group));
+		return entries.stream().map(PaletteEntry::getLabel).collect(Collectors.toList());
+	}
+
+	private static GraphicalViewer getGraphicalViewer() {
+		return Display.syncExec(new ResultRunnable<GraphicalViewer>() {
+
+			@Override
+			public GraphicalViewer run() {
+				return (GraphicalViewer) EditorPartLookup.getInstance().getEditor().getAdapter(GraphicalViewer.class);
+			}
+		});
+	}
+}
