2dd9a3f6d601be26b08903bd6f0b74ede574a91a
Dominik Jelinek <djelinek@redhat.com>
2017-11-24 13:24:23 +0100

    FUSETOOLS-2671: Add test for collapse/expand feature in Camel Editor
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

---
+++ uitests/tests/org.jboss.tools.fuse.ui.bot.tests/src/org/jboss/tools/fuse/ui/bot/tests/utils/LogChecker.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.fuse.ui.bot.tests.utils;
+
+import static org.junit.Assert.assertTrue;
+
+import org.jboss.tools.fuse.reddeer.LogGrapper;
+
+/**
+ * Checks 'Fuse' errors in Error Log View
+ * 
+ * @author djelinek
+ */
+public class LogChecker {
+	
+	public static boolean noFuseError() {
+		return LogGrapper.getPluginErrors("fuse").isEmpty();
+	}
+	
+	public static void assertNoFuseError() {
+		assertTrue("Console contains 'fuse' errors", noFuseError());
+	}
+	
+}
