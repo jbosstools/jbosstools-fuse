29ea26256cdce98c26c2475ebe0d67297cd78745
Brian Fitzpatrick <bfitzpat@redhat.com>
2017-12-08 10:02:21 -0700

    Updates to make Eclipse paths work with java.io paths and utility
    Signed-off-by: Brian Fitzpatrick <bfitzpat@redhat.com>

--- editor/plugins/org.fusesource.ide.wsdl2rest.ui/src/org/fusesource/ide/wsdl2rest/ui/wizard/Wsdl2RestWizard.java
+++ editor/plugins/org.fusesource.ide.wsdl2rest.ui/src/org/fusesource/ide/wsdl2rest/ui/wizard/Wsdl2RestWizard.java
@@ -11,12 +11,17 @@
 package org.fusesource.ide.wsdl2rest.ui.wizard;
 
 import java.io.File;
+import java.net.URI;
 import java.net.URL;
 import java.nio.file.Path;
-
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
@@ -24,6 +29,7 @@ import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
+import org.fusesource.ide.foundation.core.util.Strings;
 import org.fusesource.ide.wsdl2rest.ui.internal.UIMessages;
 import org.fusesource.ide.wsdl2rest.ui.internal.Wsdl2RestUIActivator;
 import org.fusesource.ide.wsdl2rest.ui.wizard.pages.Wsdl2RestWizardFirstPage;
@@ -51,7 +57,7 @@ public class Wsdl2RestWizard extends Wizard implements INewWizard {
 	@Override
 	public boolean performFinish() {
 		try {
-			generate(new URL(options.getWsdlURL()), options.getDestinationCamel());
+			generate();
 		} catch (Exception e) {
 			Wsdl2RestUIActivator.pluginLog().logError(e);
 			return false;
@@ -59,7 +65,7 @@ public class Wsdl2RestWizard extends Wizard implements INewWizard {
 		return true;
 	}
 
-	protected IProject sampleGetSelectedProject() {
+	protected IProject getSelectedProjectFromSelectionService() {
 		// This is a hack for some prototyping to make sure we know what project to stick the stuff in.
 		ISelectionService ss = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService();
 		String projExpID = "org.eclipse.ui.navigator.ProjectExplorer"; //$NON-NLS-1$
@@ -81,7 +87,7 @@ public class Wsdl2RestWizard extends Wizard implements INewWizard {
 		super.addPages();
 		
 		// page one
-		IProject project = sampleGetSelectedProject();
+		IProject project = getSelectedProjectFromSelectionService();
 		Wsdl2RestWizardFirstPage pageOne = new Wsdl2RestWizardFirstPage("page1", UIMessages.wsdl2RestWizardPageOneTitle, null); //$NON-NLS-1$
 		if (project != null) {
 			options.setProjectName(project.getName());
@@ -93,12 +99,38 @@ public class Wsdl2RestWizard extends Wizard implements INewWizard {
 		addPage(pageTwo);
 	}
 	
-    private void generate(final URL wsdlLocation, final String outputPath) throws Exception {
-        Path outpath = new File(outputPath).toPath();
-        Path contextpath = new File(outputPath + File.pathSeparator + "rest-camel-context.xml").toPath(); //$NON-NLS-1$
-        Wsdl2Rest tool = new Wsdl2Rest(wsdlLocation, outpath);
-        tool.setTargetContext(contextpath);
-		tool.process();
+	private void generate() throws Exception {
+		URL wsdlLocation = new URL(options.getWsdlURL());
+		IPath path = new org.eclipse.core.runtime.Path(options.getDestinationJava());
+		IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(path);
+		File javaFile = null;
+		if (resource instanceof IFolder) {
+			IFolder destFolder = (IFolder) resource;
+			// gets URI for EFS.
+			URI uri = destFolder.getLocationURI();
+
+			// what if file is a link, resolve it.
+			if(destFolder.isLinked()){
+			   uri = destFolder.getRawLocationURI();
+			}
+
+			// Gets native File using EFS
+			javaFile = EFS.getStore(uri).toLocalFile(0, new NullProgressMonitor());			
+		}
+		if (javaFile != null) {
+	        Path outpath = javaFile.toPath();
+	        Path contextpath = new File(javaFile.getAbsolutePath() + File.separator + "rest-camel-context.xml").toPath(); //$NON-NLS-1$
+	        Wsdl2Rest tool = new Wsdl2Rest(wsdlLocation, outpath);
+	        tool.setTargetContext(contextpath);
+	        if (!Strings.isEmpty(options.getBeanClassName())) {
+	        	tool.setTargetBean(options.getBeanClassName());
+	        }
+	        if (!Strings.isEmpty(options.getTargetServiceAddress())) {
+	    		URL targetAddressURL = new URL(options.getTargetServiceAddress());
+	        	tool.setTargetAddress(targetAddressURL);
+	        }
+			tool.process();
+		}
     }
 
     public Wsdl2RestOptions getOptions() {
