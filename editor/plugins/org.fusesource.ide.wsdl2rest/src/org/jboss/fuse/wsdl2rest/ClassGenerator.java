d89835999c0d3bc03ebc4ee8a52c347c50337c78
Brian Fitzpatrick <bfitzpat@redhat.com>
2017-12-11 14:57:00 -0700

    Pulling in the wsdl2rest code from maven instead of duplicating code
    Signed-off-by: Brian Fitzpatrick <bfitzpat@redhat.com>

--- editor/plugins/org.fusesource.ide.wsdl2rest/src/org/jboss/fuse/wsdl2rest/ClassGenerator.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.fuse.wsdl2rest;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface ClassGenerator {
-
-    void generateClasses(List<EndpointInfo> clazzDef) throws IOException;
-}
