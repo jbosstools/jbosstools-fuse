48e17f9b4a4c54ce4ac413f2fd59ccdaf56570c0
Lars Heinemann <lhein.smx@gmail.com>
2017-12-15 11:39:11 +0100

    FUSETOOLS-2635 - removed nature and facet, replaced icons, cleanup
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- syndesis/plugins/org.fusesource.ide.syndesis.extension.ui/src/org/fusesource/ide/syndesis/extensions/ui/maven/SyndesisExtensionProjectConfigurator.java
+++ syndesis/plugins/org.fusesource.ide.syndesis.extension.ui/src/org/fusesource/ide/syndesis/extensions/ui/maven/SyndesisExtensionProjectConfigurator.java
@@ -10,19 +10,13 @@
  ******************************************************************************/
 package org.fusesource.ide.syndesis.extensions.ui.maven;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Set;
-
 import org.apache.maven.project.MavenProject;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jst.common.project.facet.WtpUtils;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.MavenProjectChangedEvent;
 import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
@@ -33,17 +27,13 @@ import org.eclipse.wst.common.project.facet.core.IFacetedProjectWorkingCopy;
 import org.eclipse.wst.common.project.facet.core.IProjectFacet;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
 import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
-import org.eclipse.wst.common.project.facet.core.internal.FacetedProjectNature;
 import org.fusesource.ide.projecttemplates.maven.CamelProjectConfigurator;
-import org.fusesource.ide.syndesis.extensions.ui.SyndesisExtensionProjectNature;
 import org.fusesource.ide.syndesis.extensions.ui.internal.Messages;
 
 /**
  * @author lheinema
  */
 public class SyndesisExtensionProjectConfigurator extends AbstractProjectConfigurator {
-
-	public static final IProjectFacet syndesisExtensionFacet = ProjectFacetsManager.getProjectFacet("jst.syndesis.extension"); //$NON-NLS-1$
 	
 	/* (non-Javadoc)
 	 * @see org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator#mavenProjectChanged(org.eclipse.m2e.core.project.MavenProjectChangedEvent, org.eclipse.core.runtime.IProgressMonitor)
@@ -60,68 +50,6 @@ public class SyndesisExtensionProjectConfigurator extends AbstractProjectConfigu
 			}
 		}
 		super.mavenProjectChanged(event, monitor);
-	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator#configure(org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest, org.eclipse.core.runtime.IProgressMonitor)
-	 */
-	@Override
-	public void configure(ProjectConfigurationRequest request, IProgressMonitor monitor) throws CoreException {
-		if (checkSyndesisExtensionsMetaDataExist(request.getProject())) {
-			if (!isSyndesisExtensionsFacetEnabled(request)) {
-				// if we have a camel context but no facade set we do set it
-				configureFacet(request.getMavenProject(), request.getProject(), monitor);
-			}
-			if (!isSyndesisExtensionsNatureEnabled(request.getProject())) {
-				// enable the camel nature
-				configureNature(request.getProject(), monitor);
-			}
-		}
-	}
-
-	private boolean isSyndesisExtensionsFacetEnabled(ProjectConfigurationRequest request) throws CoreException {
-		IProject project = request.getProject();
-		IFacetedProject fproj = ProjectFacetsManager.create(project);
-		if (fproj != null) {
-			Set<IProjectFacetVersion> facets = fproj.getProjectFacets();
-			Iterator<IProjectFacetVersion> itFacet = facets.iterator();
-			while (itFacet.hasNext()) {
-				IProjectFacetVersion f = itFacet.next();
-				if (syndesisExtensionFacet.getId().equals(f.getProjectFacet().getId())) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
-	private boolean isSyndesisExtensionsNatureEnabled(IProject project) throws CoreException {
-		IProjectDescription projectDescription = project.getDescription();
-		String[] ids = projectDescription.getNatureIds();
-		return Arrays.stream(ids).anyMatch(SyndesisExtensionProjectNature.NATURE_ID::equals);
-	}
-	
-	private void configureFacet(MavenProject mavenProject, IProject project, IProgressMonitor monitor)
-			throws CoreException {
-		SubMonitor subMonitor = SubMonitor.convert(monitor, Messages.configuringFacets, 4);
-		if (!checkSyndesisExtensionsMetaDataExist(project)) {
-			subMonitor.setWorkRemaining(0);
-			return;
-		}
-		IFacetedProject fproj = ProjectFacetsManager.create(project, true, subMonitor.split(1));
-
-		if (fproj == null) {
-			// Add the modulecore nature
-			WtpUtils.addNatures(project);
-			addNature(project, FacetedProjectNature.NATURE_ID, subMonitor.split(1));
-			fproj = ProjectFacetsManager.create(project);
-		}
-		subMonitor.setWorkRemaining(1);
-
-		if (fproj != null) {
-			installDefaultFacets(project, mavenProject, fproj, subMonitor.split(1));
-		}
-		subMonitor.setWorkRemaining(0);
 	}
 	
 	private boolean checkSyndesisExtensionsMetaDataExist(IProject project) {
@@ -146,10 +74,9 @@ public class SyndesisExtensionProjectConfigurator extends AbstractProjectConfigu
 				installFacet(fproj, fpwc, CamelProjectConfigurator.utilFacet, CamelProjectConfigurator.utilFacet.getLatestVersion());
 			}
 		}
-		installSyndesisExtensionFacet(fproj, fpwc, subMonitor.split(1));
 		fpwc.commitChanges(subMonitor.split(1));
-		configureNature(project, subMonitor.split(1));
 		updateMavenProject(project);
+		subMonitor.setWorkRemaining(0);
 	}
 
 	private void updateMavenProject(final IProject project) {
@@ -173,17 +100,12 @@ public class SyndesisExtensionProjectConfigurator extends AbstractProjectConfigu
 			}
 		}
 	}
-	
-	private void configureNature(IProject project, IProgressMonitor monitor)
-			throws CoreException {
-		addNature(project, SyndesisExtensionProjectNature.NATURE_ID, monitor);
-	}
-	
-	private void installSyndesisExtensionFacet(IFacetedProject fproj, IFacetedProjectWorkingCopy fpwc, IProgressMonitor monitor) throws CoreException {
-		installFacet(fproj, fpwc, syndesisExtensionFacet, syndesisExtensionFacet.getLatestVersion());
-		// we need to switch dependency versions
-		SyndesisFacetVersionChangeDelegate del = new SyndesisFacetVersionChangeDelegate();
-		del.execute(fproj.getProject(), syndesisExtensionFacet.getLatestVersion(), null, monitor);
-	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator#configure(org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	@Override
+	public void configure(ProjectConfigurationRequest request, IProgressMonitor monitor) throws CoreException {
+		// nothing to do		
+	}
 }
