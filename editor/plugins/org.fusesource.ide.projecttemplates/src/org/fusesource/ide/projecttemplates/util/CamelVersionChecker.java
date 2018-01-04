jbosstools/jbosstools-fuse

commit bf39873f2c44a50e4ab8f59f8401d78235caf477

FUSETOOLS-2635 - fixed sonar issues and code cleanup
Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

editor/plugins/org.fusesource.ide.projecttemplates/src/org/fusesource/ide/projecttemplates/util/CamelVersionChecker.java
@@ -8,14 +8,16 @@
  * Contributors:
  *     Red Hat, Inc. - initial API and implementation
  ******************************************************************************/
-
 package org.fusesource.ide.projecttemplates.util;
 
 import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.osgi.util.NLS;
 import org.fusesource.ide.camel.model.service.core.CamelServiceManagerUtil;
+import org.fusesource.ide.projecttemplates.actions.ui.UnknownTimeMonitorUpdater;
 import org.fusesource.ide.projecttemplates.internal.Messages;
 
 /**
@@ -25,10 +27,9 @@ import org.fusesource.ide.projecttemplates.internal.Messages;
 public class CamelVersionChecker implements IRunnableWithProgress {
 
 	private String camelVersionToValidate;
-	private Thread thread;
-	private IProgressMonitor monitor;
 	private boolean valid;
 	private boolean done;
+	private UnknownTimeMonitorUpdater unknownTimeMonitorUpdater;
 	
 	public CamelVersionChecker(String camelVersionToValidate) {
 		this.camelVersionToValidate = camelVersionToValidate;
@@ -39,29 +40,20 @@ public class CamelVersionChecker implements IRunnableWithProgress {
 	 */
 	@Override
 	public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-		this.monitor = monitor;
-		monitor.beginTask(Messages.newProjectWizardRuntimePageResolveDependencyStatus, IProgressMonitor.UNKNOWN);
-		this.thread = createThread();
-		this.thread.start();
-		while (this.thread.isAlive() && !this.thread.isInterrupted() && !monitor.isCanceled()) {
-			// wait
-			Thread.sleep(100);
+		SubMonitor subMonitor = SubMonitor.convert(monitor, NLS.bind(Messages.validatingCamelVersionMessage, camelVersionToValidate), 1);
+		unknownTimeMonitorUpdater = new UnknownTimeMonitorUpdater(subMonitor);
+		try {
+			new Thread(unknownTimeMonitorUpdater).start();
+			valid = isCamelVersionValid(camelVersionToValidate);
+			subMonitor.setWorkRemaining(0);
+		} finally {
+			unknownTimeMonitorUpdater.finish();
 		}
 		done = true;
 	}
 	
 	public void cancel() {
-		this.thread.interrupt();
-		monitor.setCanceled(true);
-	}
-	
-	private Thread createThread() {
-		return new Thread( () -> {
-				valid = isCamelVersionValid(camelVersionToValidate);
-				if (!monitor.isCanceled()) {
-					monitor.done();
-				}
-		});
+		this.unknownTimeMonitorUpdater.cancel();
 	}
 	
 	public boolean isDone() {

editor/plugins/org.fusesource.ide.projecttemplates/META-INF/MANIFEST.MF
@@ -62,12 +62,8 @@ Export-Package: org.fusesource.ide.projecttemplates.actions;x-friends:="org.fuse
  org.fusesource.ide.projecttemplates.impl.simple,
  org.fusesource.ide.projecttemplates.internal;x-friends:="org.fusesource.ide.projecttemplates.tests.integration",
  org.fusesource.ide.projecttemplates.maven,
- org.fusesource.ide.projecttemplates.util;
-  x-friends:="org.fusesource.ide.projecttemplates.tests.integration,
-   org.fusesource.ide.camel.validation.tests.integration,
-   org.fusesource.ide.launcher.tests.integration,
-   org.fusesource.ide.syndesis.extension.ui",
- org.fusesource.ide.projecttemplates.util.camel;x-friends:="org.fusesource.ide.projecttemplates.tests.integration,org.fusesource.ide.syndesis.extension.ui",
- org.fusesource.ide.projecttemplates.wizards;x-friends:="org.fusesource.ide.projecttemplates.tests.integration,org.fusesource.ide.syndesis.extension.ui",
+ org.fusesource.ide.projecttemplates.util,
+ org.fusesource.ide.projecttemplates.util.camel,
+ org.fusesource.ide.projecttemplates.wizards,
  org.fusesource.ide.projecttemplates.wizards.pages;x-friends:="org.fusesource.ide.projecttemplates.tests.integration",
  org.fusesource.ide.projecttemplates.wizards.pages.model;x-friends:="org.fusesource.ide.projecttemplates.tests.integration"

editor/plugins/org.fusesource.ide.projecttemplates/src/org/fusesource/ide/projecttemplates/actions/ui/UnknownTimeMonitorUpdater.java
@@ -13,26 +13,33 @@ package org.fusesource.ide.projecttemplates.actions.ui;
 import org.eclipse.core.runtime.SubMonitor;
 import org.fusesource.ide.projecttemplates.internal.ProjectTemplatesActivator;
 
-final class UnknownTimeMonitorUpdater implements Runnable {
+public final class UnknownTimeMonitorUpdater implements Runnable {
 	
 	private final SubMonitor subMonitor;
 	private boolean finished = false;
+	private boolean canceled = false;
 
-	UnknownTimeMonitorUpdater(SubMonitor monitor) {
+	public UnknownTimeMonitorUpdater(SubMonitor monitor) {
 		this.subMonitor = monitor;
 	}
 
 	@Override
 	public void run() {
-		while(!finished) {
+		while(!finished && !canceled) {
+			subMonitor.setWorkRemaining(100).split(1);
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				ProjectTemplatesActivator.pluginLog().logError(e);
 				Thread.currentThread().interrupt();
-			}
-			subMonitor.setWorkRemaining(100).split(1);
+			}			
 		}
+		subMonitor.done();
+	}
+	
+	public void cancel() {
+		canceled = true;
+		subMonitor.setCanceled(true);
 	}
 
 	public void finish() {

syndesis/plugins/org.fusesource.ide.syndesis.extension.core/src/org/fusesource/ide/syndesis/extensions/core/internal/SyndesisExtensionsCoreActivator.java
@@ -8,7 +8,6 @@
  * Contributors:
  *     Red Hat, Inc. - initial API and implementation
  ******************************************************************************/
-
 package org.fusesource.ide.syndesis.extensions.core.internal;
 
 import org.eclipse.core.runtime.Platform;
@@ -16,7 +15,6 @@ import org.jboss.tools.foundation.core.plugin.BaseCorePlugin;
 import org.jboss.tools.foundation.core.plugin.log.IPluginLog;
 import org.jboss.tools.foundation.core.plugin.log.StatusFactory;
 import org.osgi.framework.BundleContext;
-
 
 /**
  * @author lhein
@@ -41,32 +39,33 @@ public class SyndesisExtensionsCoreActivator extends BaseCorePlugin {
 	public static SyndesisExtensionsCoreActivator getDefault() {
 		return instance;
 	}
-	
+
 	public static BundleContext getBundleContext() {
-	    return instance.getBundle().getBundleContext();
+		return instance.getBundle().getBundleContext();
 	}
 
-    @Override
+	@Override
 	public void start(BundleContext context) throws Exception {
-    	super.start(context);
-    	registerDebugOptionsListener(PLUGIN_ID, new Trace(this), context);
+		super.start(context);
+		registerDebugOptionsListener(PLUGIN_ID, new Trace(this), context);
 	}
-	
+
 	/**
 	 * Gets message from plugin.properties
+	 * 
 	 * @param key
 	 * @return
 	 */
-	public static String getMessage(String key)	{
+	public static String getMessage(String key) {
 		return Platform.getResourceString(instance.getBundle(), key);
 	}
 
 	/**
-	 * Get the IPluginLog for this plugin. This method 
-	 * helps to make logging easier, for example:
+	 * Get the IPluginLog for this plugin. This method helps to make logging easier,
+	 * for example:
 	 * 
-	 *     FoundationCorePlugin.pluginLog().logError(etc)
-	 *  
+	 * FoundationCorePlugin.pluginLog().logError(etc)
+	 * 
 	 * @return IPluginLog object
 	 */
 	public static IPluginLog pluginLog() {
@@ -75,10 +74,10 @@ public class SyndesisExtensionsCoreActivator extends BaseCorePlugin {
 
 	/**
 	 * Get a status factory for this plugin
+	 * 
 	 * @return status factory
 	 */
 	public static StatusFactory statusFactory() {
 		return getDefault().statusFactoryInternal();
 	}
-	
 }

syndesis/plugins/org.fusesource.ide.syndesis.extension.core/src/org/fusesource/ide/syndesis/extensions/core/model/SyndesisActionDescriptor.java
@@ -24,10 +24,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 public class SyndesisActionDescriptor {
 	@JsonProperty("kind")
-	private String kind;							// endpoint|bean|step
+	private String kind;					
 	
 	@JsonProperty("entrypoint")
-	private String entryPoint;						// direct:${groupId}/${artifactId}/${actionId}
+	private String entryPoint;
 	
 	@JsonProperty("inputDataShape")
 	private ActionDataShape inputDataShape;

syndesis/plugins/org.fusesource.ide.syndesis.extension.ui/plugin.xml
@@ -131,7 +131,7 @@
             	<and>
                		<requires
                     	facet="jst.java"
-                     	version="[1.7">
+                     	version="[1.8">
                		</requires>
                  <requires
                        facet="jboss.m2"

syndesis/plugins/org.fusesource.ide.syndesis.extension.ui/pom.xml
@@ -25,7 +25,7 @@
 			<!-- Generate the template binaries -->
 			<plugin>
 				<groupId>org.codehaus.mojo</groupId>
-        		<artifactId>truezip-maven-plugin</artifactId>
+				<artifactId>truezip-maven-plugin</artifactId>
         		<version>1.2</version>
         		<executions>
               		<!-- BLANK-BLUEPRINT TEMPLATE -->

syndesis/plugins/org.fusesource.ide.syndesis.extension.ui/src/org/fusesource/ide/syndesis/extensions/ui/internal/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 Red Hat, Inc.
+ * Copyright (c) 2017 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution,
@@ -8,7 +8,6 @@
  * Contributors:
  *     Red Hat, Inc. - initial API and implementation
  ******************************************************************************/
-
 package org.fusesource.ide.syndesis.extensions.ui.internal;
 
 import org.eclipse.osgi.util.NLS;

commit 3183dce6d4fb5e81f4ee09d0f7632ac99c464dd5

FUSETOOLS-2401 - Revert class introducing regression for cancel not working
regression introduced by:
https://github.com/jbosstools/jbosstools-fuse/commit/bf39873f2c44a50e4ab8f59f8401d78235caf477

Signed-off-by: Aurélien Pupier <apupier@redhat.com>

editor/plugins/org.fusesource.ide.projecttemplates/src/org/fusesource/ide/projecttemplates/util/CamelVersionChecker.java
@@ -13,11 +13,8 @@ package org.fusesource.ide.projecttemplates.util;
 import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.osgi.util.NLS;
 import org.fusesource.ide.camel.model.service.core.CamelServiceManagerUtil;
-import org.fusesource.ide.projecttemplates.actions.ui.UnknownTimeMonitorUpdater;
 import org.fusesource.ide.projecttemplates.internal.Messages;
 
 /**
@@ -27,43 +24,50 @@ import org.fusesource.ide.projecttemplates.internal.Messages;
 public class CamelVersionChecker implements IRunnableWithProgress {
 
 	private String camelVersionToValidate;
+	private Thread thread;
+	private IProgressMonitor monitor;
 	private boolean valid;
 	private boolean done;
-	private UnknownTimeMonitorUpdater unknownTimeMonitorUpdater;
-	
+
 	public CamelVersionChecker(String camelVersionToValidate) {
 		this.camelVersionToValidate = camelVersionToValidate;
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.operation.IRunnableWithProgress#run(org.eclipse.core.runtime.IProgressMonitor)
-	 */
+
 	@Override
 	public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-		SubMonitor subMonitor = SubMonitor.convert(monitor, NLS.bind(Messages.validatingCamelVersionMessage, camelVersionToValidate), 1);
-		unknownTimeMonitorUpdater = new UnknownTimeMonitorUpdater(subMonitor);
-		try {
-			new Thread(unknownTimeMonitorUpdater).start();
-			valid = isCamelVersionValid(camelVersionToValidate);
-			subMonitor.setWorkRemaining(0);
-		} finally {
-			unknownTimeMonitorUpdater.finish();
+		this.monitor = monitor;
+		monitor.beginTask(Messages.newProjectWizardRuntimePageResolveDependencyStatus, IProgressMonitor.UNKNOWN);
+		this.thread = createThread();
+		this.thread.start();
+		while (this.thread.isAlive() && !this.thread.isInterrupted() && !monitor.isCanceled()) {
+			// wait
+			Thread.sleep(100);
 		}
 		done = true;
 	}
-	
+
 	public void cancel() {
-		this.unknownTimeMonitorUpdater.cancel();
+		this.thread.interrupt();
+		monitor.setCanceled(true);
 	}
-	
+
+	private Thread createThread() {
+		return new Thread( () -> {
+			valid = isCamelVersionValid(camelVersionToValidate);
+			if (!monitor.isCanceled()) {
+				monitor.done();
+			}
+		});
+	}
+
 	public boolean isDone() {
 		return done;
 	}
-	
+
 	public boolean isValid() {
 		return valid;
 	}
-	
+
 	public synchronized boolean isCamelVersionValid(String camelVersion) {
 		boolean versionValid = false;
 		if (camelVersion != null) {