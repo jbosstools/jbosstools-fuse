90103ad6164838b153277cb089d12a81f0818a3e
Aurélien Pupier <apupier@redhat.com>
2018-01-08 15:35:27 +0100

    FUSETOOLS-2742 - Use parameterized test to improve readability of cases tested
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

---
+++ syndesis/tests/org.fusesource.ide.syndesis.extension.core.tests/src/test/java/org/fusesource/ide/syndesis/extensions/core/model/SyndesisExtensionUtilInvalidVersionTest.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.fusesource.ide.syndesis.extensions.core.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.fusesource.ide.syndesis.extensions.core.util.SyndesisExtensionsUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class SyndesisExtensionUtilInvalidVersionTest {
+
+	@Parameter
+	public String invalidVersion;
+	
+	@Parameters(name = "{0}")
+	public static String[] invalidVersions() {
+		return new String[] {
+				"1",
+				"1-SNAPSHOT",
+				"1.1.SNAPSHOT",
+				"1.1-SNAPSHOT",
+				"1.1.0-SNAPSHOT",
+				"1.1.v20180108",
+				"1.b",
+				"1.1.",
+				".1.1",
+				".1."
+				};
+	}
+	
+	@Test
+	public void testIsValidSyndesisExtensionVersion() {
+		assertThat(SyndesisExtensionsUtil.isValidSyndesisExtensionVersion(invalidVersion)).isFalse();
+	}
+	
+}
--- syndesis/tests/org.fusesource.ide.syndesis.extension.core.tests/src/test/java/org/fusesource/ide/syndesis/extensions/core/model/SyndesisExtensionUtilValidVersionTest.java
+++ syndesis/tests/org.fusesource.ide.syndesis.extension.core.tests/src/test/java/org/fusesource/ide/syndesis/extensions/core/model/SyndesisExtensionUtilValidVersionTest.java
@@ -14,42 +14,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.fusesource.ide.syndesis.extensions.core.util.SyndesisExtensionsUtil;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  * @author lheinema
  */
-public class SyndesisExtensionUtilTest {
-
-	private String[] validVersions = new String[] {
-			"1.0",
-			"1.0.0",
-			"1.1.20180108"
-	};
+@RunWith(Parameterized.class)
+public class SyndesisExtensionUtilValidVersionTest {
 	
-	private String[] invalidVersions = new String[] {
-			"1",
-			"1-SNAPSHOT",
-			"1.1.SNAPSHOT",
-			"1.1-SNAPSHOT",
-			"1.1.0-SNAPSHOT",
-			"1.1.v20180108",
-			"1.b",
-			"1.1.",
-			".1.1",
-			".1."
-	};
+	@Parameter
+	public String validVersion;
 	
-	@Test
-	public void testIsValidSyndesisExtensionVersion() {
-		for (String version : validVersions) {
-			assertThat(SyndesisExtensionsUtil.isValidSyndesisExtensionVersion(version)).as("Check value: " + version).isTrue();
-		}
+	@Parameters(name = "{0}")
+	public static String[] validVersions() {
+		return new String[] {
+				"1.0",
+				"1.0.0",
+		"1.1.20180108"};
 	}
 	
 	@Test
-	public void testIsInvalidSyndesisExtensionVersion() {
-		for (String version : invalidVersions) {
-			assertThat(SyndesisExtensionsUtil.isValidSyndesisExtensionVersion(version)).as("Check value: " + version).isFalse();
-		}
+	public void testIsValidSyndesisExtensionVersion() {
+		assertThat(SyndesisExtensionsUtil.isValidSyndesisExtensionVersion(validVersion)).isTrue();
 	}
 }
