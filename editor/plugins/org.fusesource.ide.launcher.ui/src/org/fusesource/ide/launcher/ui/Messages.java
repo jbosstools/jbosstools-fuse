646c4fe11294f5ab83b12c1328a3c0d2bb8269a9
Aurélien Pupier <apupier@redhat.com>
2018-01-18 10:50:50 +0100

    FUSETOOLS-2725 - removed no more used messages
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- editor/plugins/org.fusesource.ide.launcher.ui/src/org/fusesource/ide/launcher/ui/Messages.java
+++ editor/plugins/org.fusesource.ide.launcher.ui/src/org/fusesource/ide/launcher/ui/Messages.java
@@ -19,11 +19,6 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.fusesource.ide.launcher.ui.l10n.messages";
 
-	public static String pomGroup;
-	public static String browseWorkspace;
-	public static String choosePomDir;
-	public static String browseFs;
-	public static String browseVariables;
 	public static String ExecutePomActionSupport_NoCamelXMLFileFoundMessage;
 	public static String ExecutePomActionSupport_NoCamelXMLFileFoundTitle;
 	public static String ExecutePomActionSupport_SelectConfigurationDialogTitle;
@@ -31,18 +26,6 @@ public class Messages extends NLS {
 	public static String ExecutePomActionSupport_SelectRunConfigurationmessage;
 	public static String ExecutePomActionSupport_UnableToLaunchMessage;
 	public static String ExecutePomActionSupport_UnableToLaunchTitle;
-	public static String goalsLabel;
-	public static String goals;
-	public static String profilesLabel;
-	public static String propName;
-	public static String propValue;
-	public static String propAddButton;
-	public static String propEditButton;
-	public static String propRemoveButton;
-	public static String propertyDialog_browseVariables;
-	public static String mainTabName;
-	public static String pomDirectoryEmpty;
-	public static String pomDirectoryDoesntExist;
 	public static String xmlSelectionDialogOnRunAndDebugTitle;
 	public static String disconnected;
 	public static String suspended;
