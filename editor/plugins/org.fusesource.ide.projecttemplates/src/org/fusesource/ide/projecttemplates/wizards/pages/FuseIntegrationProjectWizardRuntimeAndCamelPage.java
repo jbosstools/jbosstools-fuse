54516030b7e5607ad8065ef8d6b08d8fed892bca
Aurélien Pupier <apupier@redhat.com>
2018-03-12 14:57:05 +0100

    FUSETOOLS-2684 - Update UI to choose targeted environment for new project
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- editor/plugins/org.fusesource.ide.projecttemplates/src/org/fusesource/ide/projecttemplates/wizards/pages/FuseIntegrationProjectWizardRuntimeAndCamelPage.java
+++ editor/plugins/org.fusesource.ide.projecttemplates/src/org/fusesource/ide/projecttemplates/wizards/pages/FuseIntegrationProjectWizardRuntimeAndCamelPage.java
@@ -20,6 +20,14 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.beans.BeanProperties;
+import org.eclipse.core.databinding.observable.sideeffect.ISideEffect;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.core.databinding.observable.value.SelectObservableValue;
+import org.eclipse.jface.databinding.swt.ISWTObservableValue;
+import org.eclipse.jface.databinding.swt.WidgetProperties;
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -40,7 +48,6 @@ import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
 import org.eclipse.wst.common.project.facet.core.runtime.RuntimeManager;
 import org.eclipse.wst.server.core.IRuntime;
@@ -54,6 +61,9 @@ import org.fusesource.ide.foundation.ui.util.Widgets;
 import org.fusesource.ide.projecttemplates.internal.Messages;
 import org.fusesource.ide.projecttemplates.internal.ProjectTemplatesActivator;
 import org.fusesource.ide.projecttemplates.util.CamelVersionChecker;
+import org.fusesource.ide.projecttemplates.wizards.pages.model.EnvironmentData;
+import org.fusesource.ide.projecttemplates.wizards.pages.model.FuseDeploymentPlatform;
+import org.fusesource.ide.projecttemplates.wizards.pages.model.FuseRuntimeKind;
 
 
 /**
@@ -65,10 +75,16 @@ public class FuseIntegrationProjectWizardRuntimeAndCamelPage extends WizardPage 
 	private static final Pattern MAVEN_VERSION_PATTERN = Pattern.compile("^(\\d+){1}(\\.\\d+){1}(\\.\\d+){1}?((\\.|\\-).*)?$");
 
 	private boolean activeCamelVersionValidation;
-	private ComboViewer runtimeComboViewer;
+	private ComboViewer runtimeKarafComboViewer;
+	private ComboViewer runtimeWildflyComboViewer;
 	private Map<String, IRuntime> serverRuntimes;
 	private String lastSelectedRuntime;
 	private ComboViewer camelVersionComboViewer;
+	private EnvironmentData environment = new EnvironmentData(CamelCatalogUtils.getLatestCamelVersion(), FuseDeploymentPlatform.OpenShift, FuseRuntimeKind.SpringBoot);
+	private DataBindingContext dbc = new DataBindingContext();
+	private ISWTObservableValue standAloneObservable;
+	private IObservableValue<FuseDeploymentPlatform> environmentPlatformObservable;
+	private ISideEffect sideEffect;
 	
 	public FuseIntegrationProjectWizardRuntimeAndCamelPage() {
 		super(Messages.newProjectWizardRuntimePageName);
@@ -82,70 +98,40 @@ public class FuseIntegrationProjectWizardRuntimeAndCamelPage extends WizardPage 
 	public void createControl(Composite parent) {
 		Composite container = new Composite(parent, SWT.NULL);
 		container.setLayout(new GridLayout(3, false));
+		
+		createPlatformChoiceGroup(container);
+		createRuntimeChoiceGroup(container);
+		createCamelVersionSelectionGroup(container);
+				
+		setControl(container);
+		
+		configureRuntimeCombo();
 
-		Group runtimeGrp = new Group(container, SWT.NONE);
-		GridData runtimeGrpData = new GridData(SWT.FILL, SWT.CENTER, true, false, 3, 1);
-		runtimeGrp.setLayout(new GridLayout(3, false));
-		runtimeGrp.setLayoutData(runtimeGrpData);
-		runtimeGrp.setText(Messages.newProjectWizardRuntimePageRuntimeGroupLabel);
+		ServerCore.addRuntimeLifecycleListener(new FuseRuntimeLifecycleListener());
+		validate();
+		sideEffect = selectSpringBootForOpenShiftPlatform();
+	}
 
-		Label runtimeLabel = new Label(runtimeGrp, SWT.NONE);
-		GridData runtimeLabelData = new GridData(SWT.FILL, SWT.CENTER, true, false, 3, 1);
-		runtimeLabel.setLayoutData(runtimeLabelData);
-		runtimeLabel.setText(Messages.newProjectWizardRuntimePageRuntimeLabel);
-
-		runtimeComboViewer = new ComboViewer(runtimeGrp, SWT.NONE | SWT.READ_ONLY);
-		runtimeComboViewer.setComparator(new ViewerComparator((o1, o2) -> {
-			if (Messages.newProjectWizardRuntimePageNoRuntimeSelectedLabel.equals(o1)){
-				return -1;
+	private ISideEffect selectSpringBootForOpenShiftPlatform() {
+		return ISideEffect.create(() -> {
+			if (FuseDeploymentPlatform.OpenShift.equals(environmentPlatformObservable.getValue())) {
+				return FuseRuntimeKind.SpringBoot;
+			} else {
+				return environment.getFuseRuntime();
 			}
-			return o1.compareTo(o2);
-		}));
-
-		GridData runtimeComboData = new GridData(SWT.FILL, SWT.FILL, true, false, 2, 1);
-		runtimeComboViewer.getCombo().setLayoutData(runtimeComboData);
-		runtimeComboViewer.getCombo().setToolTipText(Messages.newProjectWizardRuntimePageRuntimeDescription);
-		runtimeComboViewer.setContentProvider(ArrayContentProvider.getInstance());
-		runtimeComboViewer.getCombo().addModifyListener(e -> {
-			lastSelectedRuntime = getSelectedRuntimeAsString();
-			preselectCamelVersionForRuntime(determineRuntimeCamelVersion(getSelectedRuntime()));
+		}, fuseRuntime -> {
+			environment.setFuseRuntime(fuseRuntime);
+			dbc.updateTargets();
 			validate();
 		});
+	}
 
-
-
-		Button runtimeNewButton = new Button(runtimeGrp, SWT.NONE);
-		GridData runtimeNewButtonData = new GridData(SWT.FILL, SWT.CENTER, false, false);
-		runtimeNewButton.setLayoutData(runtimeNewButtonData);
-		runtimeNewButton.setText(Messages.newProjectWizardRuntimePageRuntimeNewButtonLabel);
-		runtimeNewButton.setToolTipText(Messages.newProjectWizardRuntimePageRuntimeNewButtonDescription);
-		runtimeNewButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				String[] oldRuntimes = runtimeComboViewer.getCombo().getItems();
-				boolean created = ServerUIUtil.showNewRuntimeWizard(getShell(), null, null);
-				if (created) {
-					String[] newRuntimes = runtimeComboViewer.getCombo().getItems();
-					String newRuntime = getNewRuntime(oldRuntimes, newRuntimes);
-					if(newRuntime != null){
-						runtimeComboViewer.setSelection(new StructuredSelection(newRuntime));
-					}
-				}
-			}
-		});
-
-		new Label(runtimeGrp, SWT.None);
-
+	private void createCamelVersionSelectionGroup(Composite container) {
 		Group camelGrp = new Group(container, SWT.NONE);
 		GridData camelGrpData = new GridData(SWT.FILL, SWT.CENTER, true, false, 3, 1);
 		camelGrp.setLayout(new GridLayout(3, false));
 		camelGrp.setLayoutData(camelGrpData);
-		camelGrp.setText(Messages.newProjectWizardRuntimePageCamelGroupLabel);
-
-		Label camelVersionLabel = new Label(camelGrp, SWT.NONE);
-		GridData camelLabelData = new GridData(SWT.FILL, SWT.CENTER, true, false, 3, 1);
-		camelVersionLabel.setLayoutData(camelLabelData);
-		camelVersionLabel.setText(Messages.newProjectWizardRuntimePageCamelLabel);
+		camelGrp.setText(Messages.newProjectWizardRuntimePageCamelLabel);
 
 		Combo camelVersionCombo = new Combo(camelGrp, SWT.RIGHT | SWT.DROP_DOWN);
 		camelVersionComboViewer = new ComboViewer(camelVersionCombo);
@@ -184,18 +170,108 @@ public class FuseIntegrationProjectWizardRuntimeAndCamelPage extends WizardPage 
 		camelVersionValidationBtn.setText(Messages.newProjectWizardRuntimePageCamelVersionValidationLabel);
 		camelVersionValidationBtn.setToolTipText(Messages.newProjectWizardRuntimePageCamelVersionValidationDescription);
 		camelVersionValidationBtn.addSelectionListener(new VersionValidationHandler());
-				
-		new Label(camelGrp, SWT.None);
-				
-		setControl(container);
-		
-		configureRuntimeCombo();
+	}
 
-		ServerCore.addRuntimeLifecycleListener(new FuseRuntimeLifecycleListener());
-		validate();
+	private void createRuntimeChoiceGroup(Composite container) {
+		Group runtimeGrp = new Group(container, SWT.NONE);
+		runtimeGrp.setLayout(new GridLayout(3, false));
+		runtimeGrp.setLayoutData(GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).grab(true, false).span(3, 1).create());
+		runtimeGrp.setText(Messages.newProjectWizardRuntimePageWhichRuntime);
+
+		Button springBootRadio = new Button(runtimeGrp, SWT.RADIO);
+		springBootRadio.setText(Messages.newProjectWizardRuntimePageSpringBootChoice);
+		springBootRadio.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).span(3, 1).create());
+		springBootRadio.addSelectionListener(new ValidateTriggerListener());
+		
+		Button karafRadio = new Button(runtimeGrp, SWT.RADIO);
+		karafRadio.setText(Messages.newProjectWizardRuntimePageKarafChoice);
+		dbc.bindValue(WidgetProperties.enabled().observe(karafRadio), standAloneObservable);
+		runtimeKarafComboViewer = createRuntimeSelection(runtimeGrp, karafRadio);
+		
+		Button eapRadio = new Button(runtimeGrp, SWT.RADIO);
+		eapRadio.setText(Messages.newProjectWizardRuntimePageWildflyChoice);
+		dbc.bindValue(WidgetProperties.enabled().observe(eapRadio), standAloneObservable);
+		runtimeWildflyComboViewer = createRuntimeSelection(runtimeGrp, eapRadio);
+		
+		SelectObservableValue<FuseRuntimeKind> observableValue = new SelectObservableValue<>();
+		observableValue.addOption(FuseRuntimeKind.SpringBoot, WidgetProperties.selection().observe(springBootRadio));
+		observableValue.addOption(FuseRuntimeKind.Karaf, WidgetProperties.selection().observe(karafRadio));
+		observableValue.addOption(FuseRuntimeKind.WildFly, WidgetProperties.selection().observe(eapRadio));
+		dbc.bindValue(observableValue, BeanProperties.value(EnvironmentData.class, "fuseRuntime").observe(environment));
+	}
+
+	private ComboViewer createRuntimeSelection(Group runtimeGrp, Button relatedRadioButton) {
+		ComboViewer runtimeComboViewer = new ComboViewer(runtimeGrp, SWT.NONE | SWT.READ_ONLY);
+		runtimeComboViewer.setComparator(new ViewerComparator((o1, o2) -> {
+			if (Messages.newProjectWizardRuntimePageNoRuntimeSelectedLabel.equals(o1)){
+				return -1;
+			}
+			return o1.compareTo(o2);
+		}));
+
+		runtimeComboViewer.getCombo().setLayoutData(GridDataFactory.fillDefaults().grab(true, false).create());
+		runtimeComboViewer.getCombo().setToolTipText(Messages.newProjectWizardRuntimePageRuntimeDescription);
+		runtimeComboViewer.setContentProvider(ArrayContentProvider.getInstance());
+		runtimeComboViewer.getCombo().addModifyListener(e -> {
+			lastSelectedRuntime = getSelectedRuntimeAsString(runtimeComboViewer);
+			preselectCamelVersionForRuntime(determineRuntimeCamelVersion(getSelectedRuntime()));
+			validate();
+		});
+
+		Button runtimeNewButton = new Button(runtimeGrp, SWT.NONE);
+		GridData runtimeNewButtonData = new GridData(SWT.FILL, SWT.CENTER, false, false);
+		runtimeNewButton.setLayoutData(runtimeNewButtonData);
+		runtimeNewButton.setText(Messages.newProjectWizardRuntimePageRuntimeNewButtonLabel);
+		runtimeNewButton.setToolTipText(Messages.newProjectWizardRuntimePageRuntimeNewButtonDescription);
+		runtimeNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				String[] oldRuntimes = runtimeComboViewer.getCombo().getItems();
+				boolean created = ServerUIUtil.showNewRuntimeWizard(getShell(), null, null);
+				if (created) {
+					String[] newRuntimes = runtimeComboViewer.getCombo().getItems();
+					String newRuntime = getNewRuntime(oldRuntimes, newRuntimes);
+					if(newRuntime != null){
+						runtimeComboViewer.setSelection(new StructuredSelection(newRuntime));
+					}
+				}
+			}
+		});
+		
+		dbc.bindValue(WidgetProperties.enabled().observe(runtimeComboViewer.getCombo()), WidgetProperties.selection().observe(relatedRadioButton));
+		dbc.bindValue(WidgetProperties.enabled().observe(runtimeNewButton), WidgetProperties.selection().observe(relatedRadioButton));
+		
+		return runtimeComboViewer;
+	}
+
+	private void createPlatformChoiceGroup(Composite container) {
+		Group platformGrp = new Group(container, SWT.NONE);
+		platformGrp.setLayout(new GridLayout(1, false));
+		platformGrp.setLayoutData(GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).grab(true, false).create());
+		platformGrp.setText("Where are you deploying?");
+		
+		Button openShiftRadioButton = new Button(platformGrp, SWT.RADIO);
+		openShiftRadioButton.setText("Kubernetes/OpenShift");
+		
+		Button standAloneRadioButton = new Button(platformGrp, SWT.RADIO);
+		standAloneRadioButton.setText("Standalone");
+		
+		SelectObservableValue<FuseDeploymentPlatform> observableValue = new SelectObservableValue<>();
+		standAloneObservable = WidgetProperties.selection().observe(standAloneRadioButton);
+		observableValue.addOption(FuseDeploymentPlatform.OpenShift,  WidgetProperties.selection().observe(openShiftRadioButton));
+		observableValue.addOption(FuseDeploymentPlatform.Standalone, standAloneObservable);
+		environmentPlatformObservable = BeanProperties.value(EnvironmentData.class, "deploymentPlatform").observe(environment);
+		dbc.bindValue(observableValue, environmentPlatformObservable);
+		
+		openShiftRadioButton.addSelectionListener(new ValidateTriggerListener());
 	}	
 
 	private void configureRuntimeCombo() {
+		configureRuntimeCombo(runtimeKarafComboViewer);
+		configureRuntimeCombo(runtimeWildflyComboViewer);
+	}
+
+	private void configureRuntimeCombo(ComboViewer runtimeComboViewer) {
 		if (Widgets.isDisposed(runtimeComboViewer)) {
 			return;
 		}
@@ -301,8 +377,17 @@ public class FuseIntegrationProjectWizardRuntimeAndCamelPage extends WizardPage 
 	 * @return the selected runtime
 	 */
 	public IRuntime getSelectedRuntime() {
+		if (FuseRuntimeKind.Karaf.equals(environment.getFuseRuntime())) {
+			return getSelectedRuntime(runtimeKarafComboViewer);
+		} else if(FuseRuntimeKind.Karaf.equals(environment.getFuseRuntime())) {
+			return getSelectedRuntime(runtimeWildflyComboViewer);
+		}
+		return null;
+	}
+
+	private IRuntime getSelectedRuntime(ComboViewer runtimeComboViewer) {
 		if (!Widgets.isDisposed(runtimeComboViewer)) {
-			String runtimeId = getSelectedRuntimeAsString();
+			String runtimeId = getSelectedRuntimeAsString(runtimeComboViewer);
 			if (!Messages.newProjectWizardRuntimePageNoRuntimeSelectedLabel.equalsIgnoreCase(runtimeId)) {
 				return serverRuntimes.get(runtimeId);
 			}
@@ -310,7 +395,7 @@ public class FuseIntegrationProjectWizardRuntimeAndCamelPage extends WizardPage 
 		return null;
 	}
 
-	public String getSelectedRuntimeAsString(){
+	public String getSelectedRuntimeAsString(ComboViewer runtimeComboViewer){
 		IStructuredSelection structuredSelection = runtimeComboViewer.getStructuredSelection();
 		if(!structuredSelection.isEmpty()){
 			return (String) structuredSelection.getFirstElement();
@@ -344,17 +429,13 @@ public class FuseIntegrationProjectWizardRuntimeAndCamelPage extends WizardPage 
 		if (getSelectedRuntime() != null) {
 			validateForRuntimeCamelVersion(determineRuntimeCamelVersion(getSelectedRuntime()));
 		} else {
-			if (!Widgets.isDisposed(camelVersionComboViewer)){
+			if (!Widgets.isDisposed(camelVersionComboViewer)) {
 				camelVersionComboViewer.getCombo().setEnabled(true);
 			}			
 			String selectedCamelVersion = getSelectedCamelVersion();
 			if (!isValidCamelVersionSyntax(selectedCamelVersion)) {
 				setErrorMessage(NLS.bind(Messages.newProjectWizardRuntimePageCamelVersionInvalidSyntaxWarning, selectedCamelVersion)); //$NON-NLS-1$
 			} 		
-		}
-
-		if (Strings.isBlank(getSelectedRuntimeAsString())) {
-			setErrorMessage(Messages.newProjectWizardRuntimePageNoRuntimeSelectedLabel);			
 		}
 		
 		if (Strings.isBlank(getSelectedCamelVersion())) {
@@ -389,22 +470,24 @@ public class FuseIntegrationProjectWizardRuntimeAndCamelPage extends WizardPage 
 		return false;
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.wizard.WizardPage#canFlipToNextPage()
-	 */
 	@Override
 	public boolean canFlipToNextPage() {
 		return super.canFlipToNextPage() && !activeCamelVersionValidation && isPageComplete();
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.wizard.WizardPage#isPageComplete()
-	 */
 	@Override
 	public boolean isPageComplete() {
 		return super.isPageComplete() && getErrorMessage() == null;
 	}
 	
+	private final class ValidateTriggerListener extends SelectionAdapter {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			super.widgetSelected(e);
+			validate();
+		}
+	}
+
 	class FuseRuntimeLifecycleListener implements IRuntimeLifecycleListener {
 		@Override
 		public void runtimeRemoved(IRuntime runtime) {
@@ -428,9 +511,6 @@ public class FuseIntegrationProjectWizardRuntimeAndCamelPage extends WizardPage 
 	
 	class VersionValidationHandler extends SelectionAdapter {
 		
-		/* (non-Javadoc)
-		 * @see org.eclipse.swt.events.SelectionAdapter#widgetSelected(org.eclipse.swt.events.SelectionEvent)
-		 */
 		@Override
 		public void widgetSelected(SelectionEvent e) {
 			String camelVersion = getSelectedCamelVersion();
@@ -466,4 +546,11 @@ public class FuseIntegrationProjectWizardRuntimeAndCamelPage extends WizardPage 
 			setPageComplete(valid);
 		}
 	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
+		sideEffect.dispose();
+	}
+	
 }
---
+++ editor/plugins/org.fusesource.ide.projecttemplates/src/org/fusesource/ide/projecttemplates/wizards/pages/model/EnvironmentData.java
@@ -0,0 +1,51 @@
+/******************************************************************************* 
+ * Copyright (c) 2018 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+package org.fusesource.ide.projecttemplates.wizards.pages.model;
+
+import java.beans.Beans;
+
+public class EnvironmentData extends Beans {
+	
+	private String camelVersion;
+	private FuseDeploymentPlatform deploymentPlatform;
+	private FuseRuntimeKind fuseRuntime;
+
+	public EnvironmentData(String camelVersion, FuseDeploymentPlatform deploymentPlatform, FuseRuntimeKind fuseRuntime) {
+		this.camelVersion = camelVersion;
+		this.deploymentPlatform = deploymentPlatform;
+		this.fuseRuntime = fuseRuntime;
+	}
+
+	public String getCamelVersion() {
+		return camelVersion;
+	}
+
+	public void setCamelVersion(String camelVersion) {
+		this.camelVersion = camelVersion;
+	}
+
+	public FuseDeploymentPlatform getDeploymentPlatform() {
+		return deploymentPlatform;
+	}
+
+	public void setDeploymentPlatform(FuseDeploymentPlatform deploymentPlatform) {
+		this.deploymentPlatform = deploymentPlatform;
+	}
+
+	public FuseRuntimeKind getFuseRuntime() {
+		return fuseRuntime;
+	}
+
+	public void setFuseRuntime(FuseRuntimeKind fuseRuntime) {
+		this.fuseRuntime = fuseRuntime;
+	}
+
+}
