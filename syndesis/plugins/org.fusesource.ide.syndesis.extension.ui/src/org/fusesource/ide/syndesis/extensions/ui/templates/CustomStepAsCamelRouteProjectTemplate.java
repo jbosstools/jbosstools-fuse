d4ea3f6d074add8eaea699a40773d3f0baaaddcc
Lars Heinemann <lhein.smx@gmail.com>
2018-03-13 11:16:04 +0100

    FUSETOOLS-2834 - fixed wrong headers and removed obsolete comments
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- syndesis/plugins/org.fusesource.ide.syndesis.extension.ui/src/org/fusesource/ide/syndesis/extensions/ui/templates/CustomStepAsCamelRouteProjectTemplate.java
+++ syndesis/plugins/org.fusesource.ide.syndesis.extension.ui/src/org/fusesource/ide/syndesis/extensions/ui/templates/CustomStepAsCamelRouteProjectTemplate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Red Hat, Inc.
+ * Copyright (c) 2018 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution,
@@ -8,7 +8,6 @@
  * Contributors:
  *     Red Hat, Inc. - initial API and implementation
  ******************************************************************************/
-
 package org.fusesource.ide.syndesis.extensions.ui.templates;
 
 import java.io.IOException;
