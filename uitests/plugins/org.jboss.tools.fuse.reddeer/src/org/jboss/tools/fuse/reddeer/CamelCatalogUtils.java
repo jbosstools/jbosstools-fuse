41236f834b51fdfc788859db2e8b337049f0b2fe
Dominik Jelinek <djelinek@redhat.com>
2018-03-14 08:39:24 +0100

    FUSETOOLS-2806: UI Tests - Extend 'Camel catalog' testing util structure
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- uitests/plugins/org.jboss.tools.fuse.reddeer/src/org/jboss/tools/fuse/reddeer/CamelCatalogUtils.java
+++ uitests/plugins/org.jboss.tools.fuse.reddeer/src/org/jboss/tools/fuse/reddeer/CamelCatalogUtils.java
@@ -10,13 +10,19 @@
  ******************************************************************************/
 package org.jboss.tools.fuse.reddeer;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.eclipse.reddeer.common.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
- * Structure for using different versions of Camel catalog - implemented only for 'components'
+ * Structure for using different versions of Camel catalog
  * 
  * @author djelinek
  */
@@ -26,13 +32,30 @@ public class CamelCatalogUtils {
 
 	private static final String ROOT_PATH = "/org/apache/camel/catalog/";
 
-	private static String COMPONENTS = ROOT_PATH + "components";
+	private static String COMPONENTS_PATH = ROOT_PATH + "components";
 
-	private static String COMPONENTS_LIST = ROOT_PATH + "components.properties";
+	private static String COMPONENTS_LIST_PATH = ROOT_PATH + "components.properties";
+
+	private static String DATAFORMATS_PATH = ROOT_PATH + "dataformats";
+
+	private static String DATAFORMATS_LIST_PATH = ROOT_PATH + "dataformats.properties";
+
+	private static String LANGUAGES_PATH = ROOT_PATH + "languages";
+
+	private static String LANGUAGES_LIST_PATH = ROOT_PATH + "languages.properties";
+
+	private static String MODELS_PATH = ROOT_PATH + "models";
+
+	private static String MODELS_LIST_PATH = ROOT_PATH + "models.properties";
+
+	private static final String JSON_SUFFIX = ".json";
 
 	public enum CatalogType {
 
-		COMPONENT("component", COMPONENTS, COMPONENTS_LIST);
+		COMPONENT("component", COMPONENTS_PATH, COMPONENTS_LIST_PATH),
+		DATAFORMAT("dataformat", DATAFORMATS_PATH, DATAFORMATS_LIST_PATH),
+		LANGUAGE("language", LANGUAGES_PATH, LANGUAGES_LIST_PATH),
+		MODEL("model", MODELS_PATH, MODELS_LIST_PATH);
 
 		private String type;
 		private String path;
@@ -71,8 +94,14 @@ public class CamelCatalogUtils {
 	}
 
 	public CamelCatalogUtils(String path) {
-		COMPONENTS = path + COMPONENTS;
-		COMPONENTS_LIST = path + COMPONENTS_LIST;
+		COMPONENTS_PATH = path + COMPONENTS_PATH;
+		COMPONENTS_LIST_PATH = path + COMPONENTS_LIST_PATH;
+		DATAFORMATS_PATH = path + DATAFORMATS_PATH;
+		DATAFORMATS_LIST_PATH = path + DATAFORMATS_LIST_PATH;
+		LANGUAGES_PATH = path + LANGUAGES_PATH;
+		LANGUAGES_LIST_PATH = path + LANGUAGES_LIST_PATH;
+		MODELS_PATH = path + MODELS_PATH;
+		MODELS_LIST_PATH = path + MODELS_LIST_PATH;
 	}
 
 	/**
@@ -82,6 +111,33 @@ public class CamelCatalogUtils {
 	 */
 	public String Components() {
 		return getCatalogComponentsList(CatalogType.COMPONENT);
+	}
+
+	/**
+	 * Returns all available dataformats
+	 *
+	 * @return String
+	 */
+	public String DataFormats() {
+		return getCatalogComponentsList(CatalogType.DATAFORMAT);
+	}
+
+	/**
+	 * Returns all available languages
+	 *
+	 * @return String
+	 */
+	public String Languages() {
+		return getCatalogComponentsList(CatalogType.LANGUAGE);
+	}
+
+	/**
+	 * Returns all available models
+	 *
+	 * @return String
+	 */
+	public String Models() {
+		return getCatalogComponentsList(CatalogType.MODEL);
 	}
 
 	/**
@@ -95,6 +151,103 @@ public class CamelCatalogUtils {
 		return catalogNamePattern(name).matcher(Components()).find();
 	}
 
+	/**
+	 * Checks if dataformat is in CamelCatalog
+	 *
+	 * @param name
+	 *            (name of Component)
+	 * @return true/false
+	 */
+	public boolean isExistDataFormat(String name) {
+		return catalogNamePattern(name).matcher(DataFormats()).find();
+	}
+
+	/**
+	 * Checks if language is in CamelCatalog
+	 *
+	 * @param name
+	 *            (name of Component)
+	 * @return true/false
+	 */
+	public boolean isExistLanguage(String name) {
+		return catalogNamePattern(name).matcher(Languages()).find();
+	}
+
+	/**
+	 * Checks if model is in CamelCatalog
+	 *
+	 * @param name
+	 *            (name of Component)
+	 * @return true/false
+	 */
+	public boolean isExistModel(String name) {
+		return catalogNamePattern(name).matcher(Models()).find();
+	}
+
+	/**
+	 * Returns property value for given component name
+	 * 
+	 * @return String
+	 */
+	public String getPropertytValue(CatalogType type, String name, String property) {
+		try {
+			return new JSONObject(getJsonFileContent(type, name)).getJSONObject(type.type).getString(property);
+		} catch (Exception e) {
+			log.error("Trying to get missing property '" + property + "'");
+		}
+		return "";
+	}
+
+	/**
+	 * Returns list of available component properties names
+	 * 
+	 * @param type
+	 *            CatalogType (COMPONENT, DATAFORMAT, LANGUAGE, MODEL)
+	 * @param name
+	 *            Name of component
+	 * @return List<String>
+	 */
+	public List<String> getPropertiesNamesList(CatalogType type, String name) {
+		JSONObject obj = null;
+		JSONArray arr = null;
+		List<String> properties = new ArrayList<>();
+		try {
+			obj = new JSONObject(getJsonFileContent(type, name)).getJSONObject("properties");
+			arr = obj.names();
+			for (int i = 0; i < arr.length(); i++) {
+				properties.add(arr.get(i).toString());
+			}
+		} catch (Exception e) {
+			log.error("Trying to get non-existing 'properties' JSON object for '" + type + "' - '" + name + "'");
+		}
+		if (type == CatalogType.COMPONENT) {
+			try {
+				obj = new JSONObject(getJsonFileContent(type, name)).getJSONObject("componentProperties");
+				arr = obj.names();
+				if (arr != null) {
+					for (int i = 0; i < arr.length(); i++) {
+						properties.add(arr.get(i).toString());
+					}
+				}
+			} catch (Exception e) {
+				log.error("Trying to get non-existing 'componentProperties' JSON object for '" + type + "' - '" + name + "'");
+			}
+		}
+		return properties;
+	}
+
+	/**
+	 * @return JSON object
+	 */
+	public JSONObject getComponentJSONObject(CatalogType type, String name) {
+		try {
+			return new JSONObject(getJsonFileContent(type, name));
+		} catch (Exception e) {
+			log.error("Trying to get JSONobject for component '" + name + "'");
+		}
+		return null;
+	}
+
 	private Pattern catalogNamePattern(String name) {
 		return Pattern.compile(name + "|" + name.split("\\s+", 2)[0] + "|" + name.replaceAll("\\s+", "") + "|"
 				+ name.replaceAll("\\s+", "-") + "|" + name.replaceAll("-", ""), Pattern.CASE_INSENSITIVE);
@@ -105,6 +258,41 @@ public class CamelCatalogUtils {
 			return FileUtils.getFileContent(type.listPath);
 		} catch (IOException e) {
 			log.error("Resource missing: can't find a failing test case to copy (" + type.listPath + ")!");
+		}
+		return "";
+	}
+
+	/**
+	 * Returns path if file with given 'name'.json exists
+	 * 
+	 * @param type
+	 *            CatalogType (COMPONENT, DATAFORMAT, LANGUAGE, MODEL)
+	 * @param name
+	 *            File name
+	 * @return String path
+	 */
+	private String findJSONFile(CatalogType type, String name) {
+		Matcher m = catalogNamePattern("(" + name + ")").matcher(getCatalogComponentsList(type));
+		while (m.find()) {
+			String path = type.path + "/" + m.group(1) + JSON_SUFFIX;
+			if (new File(path).exists())
+				return path;
+		}
+		return "";
+	}
+
+	/**
+	 * Returns the component information from JSon as String format.
+	 *
+	 * @param name
+	 *            (the component name)
+	 * @return component details in String format
+	 */
+	private String getJsonFileContent(CatalogType type, String name) {
+		try {
+			return FileUtils.getFileContent(findJSONFile(type, name));
+		} catch (IOException e) {
+			log.error("Resource missing: can't find a failing test case to copy (" + name + ")!");
 		}
 		return "";
 	}
