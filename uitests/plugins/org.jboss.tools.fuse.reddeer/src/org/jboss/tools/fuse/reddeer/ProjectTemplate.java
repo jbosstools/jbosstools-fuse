31070bcca8808053f0a7ac1b4f10bc30a050981c
Aurélien Pupier <apupier@redhat.com>
2018-03-21 14:32:17 +0100

    FUSETOOLS-2881 - update UI tests
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- uitests/plugins/org.jboss.tools.fuse.reddeer/src/org/jboss/tools/fuse/reddeer/ProjectTemplate.java
+++ uitests/plugins/org.jboss.tools.fuse.reddeer/src/org/jboss/tools/fuse/reddeer/ProjectTemplate.java
@@ -12,16 +12,16 @@ package org.jboss.tools.fuse.reddeer;
 
 public class ProjectTemplate {
 
-	public static final String[] SPRINGBOOT = {"Fuse on OpenShift", "SpringBoot on OpenShift - Spring DSL"};
+	public static final String[] SPRINGBOOT = {"Simple log using Spring Boot - Spring DSL"};
 	public static final String[] EMPTY_BLUEPRINT = {"Empty", "Empty - Blueprint DSL"};
 	public static final String[] EMPTY_SPRING = {"Empty", "Empty - Spring DSL"};
 	public static final String[] EMPTY_JAVA = {"Empty", "Empty - JAVA DSL"};
-	public static final String[] CBR_BLUEPRINT = {"Red Hat Fuse", "Beginner", "Content Based Router - Blueprint DSL"};
-	public static final String[] CBR_SPRING = {"Red Hat Fuse", "Beginner", "Content Based Router - Spring DSL"};
-	public static final String[] CBR_JAVA = {"Red Hat Fuse", "Beginner", "Content Based Router - JAVA DSL"};
-	public static final String[] AMQ_BLUEPRINT = {"Red Hat Fuse", "Beginner", "ActiveMQ - Blueprint DSL"};
-	public static final String[] AMQ_SPRING = {"Red Hat Fuse", "Beginner", "ActiveMQ - Spring DSL"};
-	public static final String[] CXF_BLUEPRINT = {"Red Hat Fuse", "Advanced", "CXF code first - Blueprint DSL"};
-	public static final String[] CXF_SPRING = {"Red Hat Fuse", "Advanced", "CXF code first - Spring DSL"};
-	public static final String[] EAP_SPRING = {"Fuse on EAP", "Medium", "Spring on EAP - Spring DSL"};
+	public static final String[] CBR_BLUEPRINT = {"Beginner", "Content Based Router - Blueprint DSL"};
+	public static final String[] CBR_SPRING = {"Beginner", "Content Based Router - Spring DSL"};
+	public static final String[] CBR_JAVA = {"Beginner", "Content Based Router - JAVA DSL"};
+	public static final String[] AMQ_BLUEPRINT = {"Beginner", "ActiveMQ - Blueprint DSL"};
+	public static final String[] AMQ_SPRING = {"Beginner", "ActiveMQ - Spring DSL"};
+	public static final String[] CXF_BLUEPRINT = {"Advanced", "CXF code first - Blueprint DSL"};
+	public static final String[] CXF_SPRING = {"Advanced", "CXF code first - Spring DSL"};
+	public static final String[] EAP_SPRING = {"Spring Bean - Spring DSL"};
 }
