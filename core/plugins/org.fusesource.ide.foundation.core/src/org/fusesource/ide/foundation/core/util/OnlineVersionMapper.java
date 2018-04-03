d44dd5479c9bf9b53c2f5d64717a6eeeb7513fab
Aurélien Pupier <apupier@redhat.com>
2018-04-03 16:26:43 +0200

    FUSETOOLS-2899 - set back config to master branch
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- core/plugins/org.fusesource.ide.foundation.core/src/org/fusesource/ide/foundation/core/util/OnlineVersionMapper.java
+++ core/plugins/org.fusesource.ide.foundation.core/src/org/fusesource/ide/foundation/core/util/OnlineVersionMapper.java
@@ -20,8 +20,7 @@ import org.fusesource.ide.foundation.core.internal.FoundationCoreActivator;
 
 public abstract class OnlineVersionMapper {
 	
-	//TODO: change after merge of PR
-	protected static final String BASE_REPO_CONFIG_URI = "https://raw.githubusercontent.com/apupier/jbosstools-fuse/FUSETOOLS-2899-AllowRemoteConfigForDefaultCamelVersionToUse/configuration/";
+	protected static final String BASE_REPO_CONFIG_URI = "https://raw.githubusercontent.com/jbosstools/jbosstools-fuse/master/configuration/";
 	
 	private String mappingProperty;
 	private String defaultUrl;
