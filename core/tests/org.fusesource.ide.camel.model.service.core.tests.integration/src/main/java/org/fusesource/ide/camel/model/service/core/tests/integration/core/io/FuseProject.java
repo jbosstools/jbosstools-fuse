d1dba0c39fc51bb4019bddae564401b5d7090b82
Brian Fitzpatrick <bfitzpat@redhat.com>
2018-04-26 07:59:01 -0600

    FUSETOOLS-2564 - a few improvements and new tests
    * removed duplicated "find resource" method and streamlined
    * added new tests to ensure Blueprint path is working
    * improved FuseProject class with better Blueprint support
    
    Signed-off-by: Brian Fitzpatrick <bfitzpat@redhat.com>

--- core/tests/org.fusesource.ide.camel.model.service.core.tests.integration/src/main/java/org/fusesource/ide/camel/model/service/core/tests/integration/core/io/FuseProject.java
+++ core/tests/org.fusesource.ide.camel.model.service.core.tests.integration/src/main/java/org/fusesource/ide/camel/model/service/core/tests/integration/core/io/FuseProject.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Red Hat, Inc.
+ * Copyright (c) 2018 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution,
@@ -73,17 +73,73 @@ public class FuseProject extends ExternalResource {
 			+ "  </dependencies>\n"
 			+ "</project>";
 
+	private static final String DUMMY_BLUEPRINT_POM_CONTENT = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+			+ "<project xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\" xmlns=\"http://maven.apache.org/POM/4.0.0\"\n"
+			+ "    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n"
+			+ "  <modelVersion>4.0.0</modelVersion>\n"
+			+ "  <groupId>com.mycompany</groupId>\n"
+			+ "  <artifactId>testproject.blueprint</artifactId>\n"
+			+ "  <version>1.0.0-SNAPSHOT</version>\n"
+			+ "  <packaging>bundle</packaging>\n"
+			+ "  <name>Some Dummy Blueprint Project</name>\n"
+			+ "  <properties>\n"
+			+ "     <camel.version>%s</camel.version>\n"
+			+ "  </properties>\n"
+			+ "  <build>\n"
+			+ "    <defaultGoal>install</defaultGoal>\n"
+			+ "    <plugins>\n"
+			+ "      <plugin>\n"
+			+ "        <artifactId>maven-compiler-plugin</artifactId>\n"
+			+ "        <version>3.5.1</version>\n"
+			+ "        <configuration>\n"
+			+ "          <source>1.7</source>\n"
+			+ "          <target>1.7</target>\n"
+			+ "        </configuration>\n"
+			+ "      </plugin>\n"
+			+ "      <plugin>\n"
+			+ "        <artifactId>maven-resources-plugin</artifactId>\n"
+			+ "        <version>2.6</version>\n"
+			+ "        <configuration>\n"
+			+ "          <encoding>UTF-8</encoding>\n"
+			+ "        </configuration>\n"
+			+ "      </plugin>\n"
+			+ "    </plugins>\n"
+			+ "  </build>\n"
+			+ "  <dependencies>\n"
+			+ "    <dependency>\n"
+			+ "      <groupId>org.apache.camel</groupId>\n"
+			+ "      <artifactId>camel-core</artifactId>\n"
+			+ "      <version>${camel.version}</version>\n"
+			+ "    </dependency>\n"
+			+ "    <dependency>\n"
+			+ "      <groupId>org.apache.camel</groupId>\n"
+			+ "      <artifactId>camel-blueprint</artifactId>\n"
+			+ "      <version>${camel.version}</version>\n"
+			+ "    </dependency>\n"
+			+ "  </dependencies>\n"
+			+ "</project>";
+
 	private IProject project = null;
 	private String projectName;
 	private String camelVersion;
+	private boolean isBlueprint = false;
 
 	public FuseProject(String projectName) {
 		this(projectName, CamelCatalogUtils.getLatestCamelVersion());
 	}
 	
+	public FuseProject(String projectName, boolean isBlueprint) {
+		this(projectName, CamelCatalogUtils.getLatestCamelVersion(), isBlueprint);
+	}
+
 	public FuseProject(String projectName, String camelVersion) {
 		this.projectName = projectName;
 		this.camelVersion = camelVersion;
+	}
+
+	public FuseProject(String projectName, String camelVersion, boolean isBlueprint) {
+		this(projectName, camelVersion);
+		this.isBlueprint = isBlueprint;
 	}
 
 	@Override
@@ -99,7 +155,11 @@ public class FuseProject extends ExternalResource {
 		}
 		// Create a fake pom.xml
 		IFile pom = project.getFile(IMavenConstants.POM_FILE_NAME);
-		pom.create(new ByteArrayInputStream(String.format(DUMMY_POM_CONTENT, this.camelVersion).getBytes(StandardCharsets.UTF_8)), true, new NullProgressMonitor());
+		String pomContent = DUMMY_POM_CONTENT;
+		if (isBlueprint) {
+			pomContent = DUMMY_BLUEPRINT_POM_CONTENT;
+		}
+		pom.create(new ByteArrayInputStream(String.format(pomContent, this.camelVersion).getBytes(StandardCharsets.UTF_8)), true, new NullProgressMonitor());
 		IFolder srcFolder = project.getFolder("src");
 		srcFolder.create(IResource.FORCE, true, new NullProgressMonitor());
 		IFolder srcMainFolder = srcFolder.getFolder("main");
@@ -130,6 +190,11 @@ public class FuseProject extends ExternalResource {
 
 	public CamelFile createEmptyCamelFile(IFile file) throws CoreException, IOException {
 		return createFileFromTemplate(file, "/empty-CamelFile.xml");
+	}
+
+	public CamelFile createEmptyBlueprintCamelFile() throws CoreException, IOException {
+		IFile file = project.getFile("blueprint.xml");
+		return createFileFromTemplate(file, "/empty-BlueprintCamelFile.xml");
 	}
 
 	public CamelFile createEmptyBlueprintCamelFile(IFile file) throws CoreException, IOException {
