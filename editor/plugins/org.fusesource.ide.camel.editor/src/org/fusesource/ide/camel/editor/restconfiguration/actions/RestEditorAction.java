dd88e4b2d519818a1d91f414e4048435c958b364
Lars Heinemann <lhein.smx@gmail.com>
2018-07-03 13:46:09 +0200

    FUSETOOLS-3027 - fixing insert position for new rest configs and several other small improvements
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

---
+++ editor/plugins/org.fusesource.ide.camel.editor/src/org/fusesource/ide/camel/editor/restconfiguration/actions/RestEditorAction.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.fusesource.ide.camel.editor.restconfiguration.actions;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.fusesource.ide.camel.editor.restconfiguration.RestConfigEditor;
+
+/**
+ * @author lheinema
+ */
+public class RestEditorAction extends Action {
+	
+	protected RestConfigEditor parent;
+	protected ImageRegistry mImageRegistry;
+	
+	public RestEditorAction(RestConfigEditor parent, ImageRegistry mImageRegistry) {
+		super(null, IAction.AS_PUSH_BUTTON);
+		this.parent = parent;
+		this.mImageRegistry = mImageRegistry;
+	}
+}
