8b2f6f64effa194d798810bd11e9eb34849628d2
Aurélien Pupier <apupier@redhat.com>
2018-07-24 14:25:54 +0200

    FUSETOOLS-3050 - Transacted and Policy should not have outputs
    see
    https://issues.apache.org/jira/browse/CAMEL-12558
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- core/plugins/org.fusesource.ide.camel.validation/src/org/fusesource/ide/camel/validation/model/EIPMandatoryChildValidator.java
+++ core/plugins/org.fusesource.ide.camel.validation/src/org/fusesource/ide/camel/validation/model/EIPMandatoryChildValidator.java
@@ -41,12 +41,10 @@ public class EIPMandatoryChildValidator implements IValidator {
 			"multicast",
 			"onCompletion",
 			"pipeline",
-			"policy",
 			"sample",
 			"split",
 			"threads",
 			"throttle",
-			"transacted",
 			"resequence"));
 	
 	@Override
