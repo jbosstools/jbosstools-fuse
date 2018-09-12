a5d5bfeccd9243e30d46fabad16eb3f5e05f271e
Aurélien Pupier <apupier@redhat.com>
2018-09-12 20:32:39 +0200

    FUSETOOLS-3074 - switch back config to master branch
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- core/plugins/org.fusesource.ide.foundation.core/src/org/fusesource/ide/foundation/core/util/OnlineVersionMapper.java
+++ core/plugins/org.fusesource.ide.foundation.core/src/org/fusesource/ide/foundation/core/util/OnlineVersionMapper.java
@@ -20,8 +20,7 @@ import org.fusesource.ide.foundation.core.internal.FoundationCoreActivator;
 
 public abstract class OnlineVersionMapper {
 	
-	//TODO: switch back to master after merge
-	protected static final String BASE_REPO_CONFIG_URI = "https://raw.githubusercontent.com/apupier/jbosstools-fuse/FUSETOOLS-3074-ProvideFuse71VersionForCreatingTemplates/configuration/";
+	protected static final String BASE_REPO_CONFIG_URI = "https://raw.githubusercontent.com/jbosstools/jbosstools-fuse/master/configuration/";
 	
 	private String mappingProperty;
 	private String defaultUrl;
