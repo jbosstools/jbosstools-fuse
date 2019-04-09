3aebd601ae769c980a1543c8d7658d0ee43af6d2
Aurélien Pupier <apupier@redhat.com>
2019-04-09 14:34:32 +0200

    FUSETOOLS-3054 - avoid false detection of Camel nature
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

---
+++ syndesis/tests/org.fusesource.ide.syndesis.extension.ui.tests.integration/src/main/java/org/fusesource/ide/syndesis/extensions/tests/integration/SyndesisProjectConfiguratorIT.java
@@ -0,0 +1,21 @@
+/******************************************************************************* 
+ * Copyright (c) 2019 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+package org.fusesource.ide.syndesis.extensions.tests.integration;
+
+import org.fusesource.ide.projecttemplates.tests.integration.wizards.CamelProjectConfiguratorIT;
+
+/**
+ * just extend to play the same tests but with SyndesisCamelprojectConfigurator active
+ *
+ */
+public class SyndesisProjectConfiguratorIT extends CamelProjectConfiguratorIT {
+	
+}
