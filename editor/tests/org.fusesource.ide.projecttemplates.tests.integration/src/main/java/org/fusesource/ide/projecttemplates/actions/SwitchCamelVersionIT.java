a75cfb8f4fefad240ecd9222412e4fd62037f575
Aurélien Pupier <apupier@redhat.com>
2019-06-27 09:41:00 +0200

    FUSETOOLS-3252 - fix typos introducing in previous PR
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- editor/tests/org.fusesource.ide.projecttemplates.tests.integration/src/main/java/org/fusesource/ide/projecttemplates/actions/SwitchCamelVersionIT.java
+++ editor/tests/org.fusesource.ide.projecttemplates.tests.integration/src/main/java/org/fusesource/ide/projecttemplates/actions/SwitchCamelVersionIT.java
@@ -50,7 +50,7 @@ public class SwitchCamelVersionIT {
 		job.schedule();
 		job.join();
 		
-		//FIXME: the wait of buidl job should be preferably handled in ChangeCamelversionJob or CamelMavenUtils but as it is not a regression, wait in test fo rnow
+		//FIXME: the wait of build job should be preferably handled in ChangeCamelversionJob or CamelMavenUtils but as it is not a regression, wait in test fo rnow
 		new BuildAndRefreshJobWaiterUtil().waitJob(new NullProgressMonitor());
 		
 		String newCamelVersion = utils.getCamelVersionFromMaven(project, false);
