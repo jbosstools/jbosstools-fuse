3fa90dc47870f106077e4af5824d285c244b7caf
Aurélien Pupier <apupier@redhat.com>
2021-02-10 10:32:47 +0100

    FUSETOOLS2-978 - remove syncExec block to avoid potential jobs deadlock
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- core/plugins/org.fusesource.ide.preferences/src/org/fusesource/ide/preferences/initializer/PreferencesInitializer.java
+++ core/plugins/org.fusesource.ide.preferences/src/org/fusesource/ide/preferences/initializer/PreferencesInitializer.java
@@ -42,9 +42,7 @@ public class PreferencesInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(PreferencesConstants.EDITOR_FIGURE_FG_COLOR, "128,128,128");
 		store.setDefault(PreferencesConstants.EDITOR_TEXT_COLOR, "0,0,0");
 
-		Display.getDefault().syncExec( () -> {
-			Color c = Display.getDefault().getSystemColor(SWT.COLOR_WIDGET_LIGHT_SHADOW);
-			store.setDefault(PreferencesConstants.EDITOR_TABLE_CHART_BG_COLOR, String.format("%d,%d,%d", c.getRed(), c.getGreen(), c.getBlue()));				
-		});
+		Color c = Display.getDefault().getSystemColor(SWT.COLOR_WIDGET_LIGHT_SHADOW);
+		store.setDefault(PreferencesConstants.EDITOR_TABLE_CHART_BG_COLOR, String.format("%d,%d,%d", c.getRed(), c.getGreen(), c.getBlue()));				
 	}
 }
