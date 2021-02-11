391bef156f3e8b96c8b6d922bce3ba0bfb71327a
Aurélien Pupier <apupier@redhat.com>
2021-02-11 21:16:52 +0100

    FUSETOOLS2-991 - use first available shell if no active available
    randomly on CI, there is sometimes no active shell.
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- editor/tests/org.fusesource.ide.camel.editor.tests.integration/src/main/java/org/fusesource/ide/camel/editor/navigator/CamelCtxNavContentProviderIT.java
+++ editor/tests/org.fusesource.ide.camel.editor.tests.integration/src/main/java/org/fusesource/ide/camel/editor/navigator/CamelCtxNavContentProviderIT.java
@@ -11,14 +11,17 @@
 package org.fusesource.ide.camel.editor.navigator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.PlatformUI;
+import org.fusesource.ide.camel.editor.tests.integration.Activator;
 import org.fusesource.ide.camel.model.service.core.model.CamelFile;
 import org.fusesource.ide.camel.model.service.core.tests.integration.core.io.FuseProject;
 import org.fusesource.ide.camel.tests.util.CommonTestUtils;
@@ -48,7 +51,8 @@ public class CamelCtxNavContentProviderIT {
 	@Test
 	public void testRouteLoadedWithOneRoute() throws Exception {
 		Display display = PlatformUI.getWorkbench().getDisplay();
-		TreeViewer treeViewer = new TreeViewer(display.getActiveShell());
+		Shell shell = findShell(display);
+		TreeViewer treeViewer = new TreeViewer(shell);
 		treeViewer.setContentProvider(new CamelCtxNavContentProvider());
 		treeViewer.setLabelProvider(new CamelCtxNavLabelProvider());
 		CamelFile camelFileWithOneRoute = fuseProject.createEmptyCamelFile();
@@ -59,6 +63,38 @@ public class CamelCtxNavContentProviderIT {
 		TreeItem[] items = treeViewer.getTree().getItems();
 		assertThat(items).hasSize(1);
 		assertThat(items[0].getText()).isEqualTo("Route route3");
+	}
+
+	private Shell findShell(Display display) {
+		Shell shell = display.getActiveShell();
+		if(shell == null) {
+			logWarning("No active shell. Searching for another shell and make it active.");
+			Shell[] shells = display.getShells();
+			if(shells.length == 0) {
+				logWarning("No shell found.");
+				fail("No Shell found");
+			} else {
+				shell = shells[0];
+				logInfo("Randomly picking the first shell available named "+ shell.getText());
+			}
+		}
+		return shell;
+	}
+
+	private void logInfo(String message) {
+		if(Activator.getDefault() != null) {
+			Activator.pluginLog().logInfo(message);
+		} else {
+			System.out.println(message);
+		}
+	}
+
+	private void logWarning(String message) {
+		if(Activator.getDefault() != null) {
+			Activator.pluginLog().logWarning(message);
+		} else {
+			System.out.println(message);
+		}
 	}
 	
 }
