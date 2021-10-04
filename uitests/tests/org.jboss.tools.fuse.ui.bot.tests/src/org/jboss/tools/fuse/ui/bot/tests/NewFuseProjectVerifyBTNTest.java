931d8f45198b63af594354429ca807d2f37faa92
Filip Pospisil <fpospisi@redhat.com>
2021-10-04 10:13:08 +0200

    FUSETOOLS-3531: Fix and convert fixed UI Test 'NewFuseProjectVerifyBTNTest' into master branch
    Signed-off-by: Filip Pospisil <fpospisi@redhat.com>

--- uitests/tests/org.jboss.tools.fuse.ui.bot.tests/src/org/jboss/tools/fuse/ui/bot/tests/NewFuseProjectVerifyBTNTest.java
+++ uitests/tests/org.jboss.tools.fuse.ui.bot.tests/src/org/jboss/tools/fuse/ui/bot/tests/NewFuseProjectVerifyBTNTest.java
@@ -24,9 +24,10 @@ import org.eclipse.reddeer.requirements.cleanworkspace.CleanWorkspaceRequirement
 import org.eclipse.reddeer.requirements.openperspective.OpenPerspectiveRequirement.OpenPerspective;
 import org.eclipse.reddeer.swt.condition.ControlIsEnabled;
 import org.eclipse.reddeer.swt.impl.button.CancelButton;
-import org.eclipse.reddeer.swt.impl.button.NextButton;
+import org.eclipse.reddeer.swt.impl.button.FinishButton;
 import org.eclipse.reddeer.swt.impl.label.DefaultLabel;
 import org.eclipse.reddeer.swt.impl.toolbar.DefaultToolItem;
+import org.eclipse.reddeer.workbench.core.condition.JobIsRunning;
 import org.eclipse.reddeer.workbench.handler.EditorHandler;
 import org.eclipse.reddeer.workbench.handler.WorkbenchShellHandler;
 import org.eclipse.reddeer.workbench.impl.shell.WorkbenchShell;
@@ -45,7 +46,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-	
+
 /**
  * Verifies functionality of 'Verify' button on New Fuse Integration Project wizard
  * 
@@ -58,24 +59,24 @@ import org.junit.runner.RunWith;
 public class NewFuseProjectVerifyBTNTest {
 
 	protected Logger log = Logger.getLogger(NewFuseProjectVerifyBTNTest.class);
-	
+
 	public static final String PROJECT_NAME = "test";
-	
+
 	public static final String EXISTING_CAMEL_VERSION = SupportedCamelVersions.CAMEL_2_17_0_REDHAT_630187;
 	public static final String NON_EXISTING_CAMEL_VERSION = "0.0.0";
 	public static final String INTERRUPT_CAMEL_VERSION = "0.0.1";
 	public static final String COMMUNITY_CAMEL_VERSION = "2.20.2";
 	public static final String DISPLAYED_NAME_CAMEL_VERSION = "2.21.0.fuse-000077-redhat-1 (Fuse 7.0.0 GA)";
-	
+
 	private NewFuseIntegrationProjectWizard wizard;
-	
+
 	@BeforeClass
 	public static void initSetup() {
 		new WorkbenchShell().maximize();
 		ConsolePreferenceUtil.setConsoleOpenOnError(false);
 		ConsolePreferenceUtil.setConsoleOpenOnOutput(false);
 	}
-	
+
 	@Before
 	public void prepareTestEnvironment() {
 		wizard = new NewFuseIntegrationProjectWizard();
@@ -83,18 +84,18 @@ public class NewFuseProjectVerifyBTNTest {
 		NewFuseIntegrationProjectWizardFirstPage firstPage = new NewFuseIntegrationProjectWizardFirstPage(wizard);
 		firstPage.setProjectName(PROJECT_NAME);
 		wizard.next();
-	} 
-	
+	}
+
 	@After
 	public void cleanSetup() {
-		LogView view = new LogView(); 
+		LogView view = new LogView();
 		view.open();
 		view.deleteLog();
 		WorkbenchShellHandler.getInstance().closeAllNonWorbenchShells();
 		EditorHandler.getInstance().closeAll(true);
 		ProjectFactory.deleteAllProjects();
 	}
-	
+
 	/**
 	 * Test tries to verify existing supported Camel version
 	 */
@@ -103,12 +104,12 @@ public class NewFuseProjectVerifyBTNTest {
 		NewFuseIntegrationProjectWizardRuntimePage secondPage = new NewFuseIntegrationProjectWizardRuntimePage(wizard);
 		secondPage.typeCamelVersion(EXISTING_CAMEL_VERSION);
 		secondPage.clickVerifyCamelVersionButton();
-		
+
 		waiting();
 		finish();
 		LogChecker.assertNoFuseError();
 	}
-	
+
 	/**
 	 * Test tries to verify non-existing Camel version
 	 */
@@ -117,52 +118,52 @@ public class NewFuseProjectVerifyBTNTest {
 		NewFuseIntegrationProjectWizardRuntimePage secondPage = new NewFuseIntegrationProjectWizardRuntimePage(wizard);
 		secondPage.typeCamelVersion(NON_EXISTING_CAMEL_VERSION);
 		secondPage.clickVerifyCamelVersionButton();
-		
+
 		waiting();
 		assertButtons();
 		cancel();
 		LogChecker.assertNoFuseError();
 	}
-	
+
 	/**
 	 * Test tries to verify community Camel version
 	 */
 	@Test
 	public void testCommunityCamelVersion() {
-		
+
 		NewFuseIntegrationProjectWizardRuntimePage secondPage = new NewFuseIntegrationProjectWizardRuntimePage(wizard);
 		secondPage.typeCamelVersion(COMMUNITY_CAMEL_VERSION);
 		secondPage.clickVerifyCamelVersionButton();
-		
+
 		waiting();
 		finish();
 		LogChecker.assertNoFuseError();
 	}
-	
+
 	/**
 	 * Test tries to verify when typing a displayed name
 	 */
 	@Test
 	public void testTypingDisplayedNameCamelVersion() {
-		
+
 		NewFuseIntegrationProjectWizardRuntimePage secondPage = new NewFuseIntegrationProjectWizardRuntimePage(wizard);
 		secondPage.typeCamelVersion(DISPLAYED_NAME_CAMEL_VERSION);
 		secondPage.clickVerifyCamelVersionButton();
-		
+
 		waiting();
 		finish();
 		LogChecker.assertNoFuseError();
 	}
-	
+
 	/**
-	 * Test tries to interrupt verifying of project Camel version 
+	 * Test tries to interrupt verifying of project Camel version
 	 */
 	@Test
 	public void testInterruptVerifying() {
 		NewFuseIntegrationProjectWizardRuntimePage secondPage = new NewFuseIntegrationProjectWizardRuntimePage(wizard);
 		secondPage.typeCamelVersion(INTERRUPT_CAMEL_VERSION);
 		secondPage.clickVerifyCamelVersionButton();
-		
+
 		AbstractWait.sleep(TimePeriod.MEDIUM);
 		new DefaultToolItem(wizard, 0).click(); // click on 'Cancel Operation'
 		waiting();
@@ -171,41 +172,47 @@ public class NewFuseProjectVerifyBTNTest {
 		cancel();
 		LogChecker.assertNoFuseError();
 	}
-	
+
 	/**
-	 * Verifies 'Finish' and 'Next' buttons availability  
+	 * Verifies 'Finish' and 'Next' buttons availability
 	 */
 	private void assertButtons() {
+		new WaitUntil(new JobIsRunning(), TimePeriod.MEDIUM, false);
 		assertFalse("Button 'Next' should be disabled", wizard.isNextEnabled());
 		assertFalse("Button 'Finish' should be disabled", wizard.isFinishEnabled());
 	}
-	
+
 	/**
 	 * Wait until 'Finish' button is enabled and click
 	 */
 	private void finish() {
-		new WaitUntil(new ControlIsEnabled(new NextButton(wizard)), TimePeriod.MEDIUM);
+		new WaitUntil(new JobIsRunning(), TimePeriod.MEDIUM, false);
+		new WaitUntil(new ControlIsEnabled(new FinishButton(wizard)), TimePeriod.DEFAULT, false);
+		new CancelButton(wizard).click();
 	}
-	
+
 	/**
 	 * Wait until 'Cancel' button is enabled and click
 	 */
 	private void cancel() {
-		new WaitUntil(new ControlIsEnabled(new CancelButton(wizard)), TimePeriod.MEDIUM);
+		new WaitUntil(new JobIsRunning(), TimePeriod.MEDIUM, false);
+		new WaitUntil(new ControlIsEnabled(new CancelButton(wizard)), TimePeriod.DEFAULT, false);
+		new CancelButton(wizard).click();
 	}
-	
+
 	private void waiting() {
-		String label = "Checking availability for Camel version " + new NewFuseIntegrationProjectWizardRuntimePage(wizard).getSelectedCamelVersion() + ". Please wait...";
+		String label = "Checking availability for Camel version "
+				+ new NewFuseIntegrationProjectWizardRuntimePage(wizard).getSelectedCamelVersion() + ". Please wait...";
 		new WaitWhile(new LabelContainsText(new DefaultLabel(wizard, 5), label), TimePeriod.VERY_LONG, false);
 	}
-	
+
 	/**
 	 * https://issues.jboss.org/browse/FUSETOOLS-2840
 	 */
 	private void testIssue2840() {
-		if(wizard.isNextEnabled() || wizard.isFinishEnabled())
+		if (wizard.isNextEnabled() || wizard.isFinishEnabled())
 			throw new JiraIssue("FUSETOOLS-2840");
-		else 
+		else
 			return;
 	}
 }
